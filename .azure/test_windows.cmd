e7cc51925dcac5d8ca872f967f94ff02b179a58d
Nick Banks <nibanks@microsoft.com>
2020-01-21 10:18:32 -0800

    Fix azure build

--- .azure/test_windows.cmd
+++ .azure/test_windows.cmd
@@ -5,8 +5,8 @@ start bld\procdump\procdump64.exe -ma -e -b -accepteula -w msquictest.exe artifa
 
 :: Import our ETW manifest.
 wevtutil im manifest\MsQuicEtw.man ^
-    /rf:%cd%\artifacts\bin\debug\msquic.dll ^
-    /mf:%cd%\artifacts\bin\debug\msquic.dll
+    /rf:%cd%\artifacts\windows\bin\debug\msquic.dll ^
+    /mf:%cd%\artifacts\windows\bin\debug\msquic.dll
 
 :: Start log collection.
 netsh trace start sessionname=quic ^
@@ -15,7 +15,7 @@ netsh trace start sessionname=quic ^
     provider=Microsoft-Quic level=0x5 keywords=0xE0000100
 
 :: Run the tests.
-artifacts\bin\debug\msquictest.exe ^
+artifacts\windows\bin\debug\msquictest.exe ^
     --gtest_filter=%1 ^
     --gtest_output=xml:artifacts\logs\windows-test-results.xml
 
