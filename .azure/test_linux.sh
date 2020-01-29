3374e2ff9ae0e5125b3bd8e6512773f43e5a65cc
Nick Banks <nibanks@microsoft.com>
2020-01-29 13:41:59 -0800

    Another try

--- .azure/test_linux.sh
+++ .azure/test_linux.sh
@@ -6,12 +6,23 @@ echo "Enabling core dumps..."
 ulimit -c unlimited
 
 # Run the tests.
-pwsh -NoLogo -NoProfile -NonInteractive \
-    ./test.ps1 \
-        -Config Debug \
-        -Filter $1 \
-        -Batch \
-        -SaveXmlResults \
-        -LogProfile Basic.Light \
-        -ConvertLogs \
-        -KeepLogsOnSuccess
+if [ $# -eq 0 ]
+    pwsh -NoLogo -NoProfile -NonInteractive \
+        ./test.ps1 \
+            -Config Debug \
+            -Batch \
+            -SaveXmlResults \
+            -LogProfile Basic.Light \
+            -ConvertLogs \
+            -KeepLogsOnSuccess
+else
+    pwsh -NoLogo -NoProfile -NonInteractive \
+        ./test.ps1 \
+            -Config Debug \
+            -Filter $1 \
+            -Batch \
+            -SaveXmlResults \
+            -LogProfile Basic.Light \
+            -ConvertLogs \
+            -KeepLogsOnSuccess
+fi
