79fc0e2f23b7f4ff3a798a34a26a4c4b64610894
Nick Banks <nibanks@microsoft.com>
2020-01-29 12:24:30 -0800

    Test with PowerShell

--- .azure/test_linux.sh
+++ .azure/test_linux.sh
@@ -1,26 +1,8 @@
 #!/bin/bash
 
-# Setup logs.
-echo "Clearing syslog file..."
-sudo truncate -s 0 /var/log/syslog
-
 # Enable core dumps.
 echo "Enabling core dumps..."
 ulimit -c unlimited
-mkdir artifacts/dumps
-cd artifacts/dumps
 
 # Run the tests.
-../linux/bin/msquictest \
-    --gtest_filter=$1 \
-    --gtest_output=xml:../logs/linux-test-results.xml
-
-# Print any core files that might be generated.
-echo "Available core dumps:"
-ls
-
-# Copy logs to log folder (with correct permsissions).
-echo "Copying logs..."
-cd ../logs
-sudo cp /var/log/syslog quic.log
-sudo chmod -R 0777 .
+pwsh -NoLogo -NoProfile -NonInteractive -ExecutionPolicy Unrestricted ./test.ps1 -Batch -SaveXmlResults -LogProfile Full.Light -ConvertLogs -Filter $1
--- .azure/test_windows.cmd
+++ .azure/test_windows.cmd
@@ -4,26 +4,5 @@ wevtutil im manifest\MsQuicEtw.man ^
     /rf:%cd%\artifacts\windows\bin\debug\msquic.dll ^
     /mf:%cd%\artifacts\windows\bin\debug\msquic.dll
 
-:: Start log collection.
-mkdir artifacts\logs
-netsh trace start sessionname=quic ^
-    overwrite=yes report=dis correlation=dis maxSize=256 ^
-    traceFile=artifacts\logs\quic.etl ^
-    provider=Microsoft-Quic level=0x5 keywords=0xE0000100
-
 :: Run the tests.
-mkdir artifacts\dumps
-bld\windows\procdump\procdump64.exe ^
-    -ma -e -b -l -accepteula -x artifacts\dumps ^
-    artifacts\windows\bin\debug\msquictest.exe ^
-        --gtest_filter=%1 ^
-        --gtest_output=xml:artifacts\logs\windows-test-results.xml
-
-:: Stop log collection.
-netsh trace stop sessionname=quic
-
-:: Convert ETW logs to text.
-netsh trace convert ^
-    artifacts\logs\quic.etl ^
-    output=artifacts\logs\quic.log ^
-    overwrite=yes
+pwsh -NoLogo -NoProfile -NonInteractive -ExecutionPolicy Unrestricted .\test.ps1 -Batch -SaveXmlResults -LogProfile Full.Light -ConvertLogs -Filter %1
