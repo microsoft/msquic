95cd1e3fd2b4f700093c9089aaafbffb8dfb145c
Nick Banks <nibanks@microsoft.com>
2020-02-12 14:10:22 -0800

    Fix test script bug

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -131,9 +131,9 @@ $RunTest = Join-Path $RootDir ".azure/scripts/run-gtest.ps1"
 # Path to the msquictest exectuable.
 $MsQuicTest = $null
 if ($IsWindows) {
-    $MsQuicTest = Join-Path $PSScriptRoot "\artifacts\windows\$($Arch)_$($Config)_$($Tls)\msquictest.exe"
+    $MsQuicTest = Join-Path $RootDir "\artifacts\windows\$($Arch)_$($Config)_$($Tls)\msquictest.exe"
 } else {
-    $MsQuicTest = Join-Path $PSScriptRoot "/artifacts/linux/$($Arch)_$($Config)_$($Tls)/msquictest"
+    $MsQuicTest = Join-Path $RootDir "/artifacts/linux/$($Arch)_$($Config)_$($Tls)/msquictest"
 }
 
 # Build up all the arguments to pass to the Powershell script.
