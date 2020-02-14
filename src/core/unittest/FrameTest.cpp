bedc60236d25e0e2fb5109a278ff8629daaad9de
Anthony Rossi <anrossi@microsoft.com>
2020-02-13 20:00:04 -0800

    Refactor TAEF core unittests into gtest.

--- src/core/unittest/FrameTest.cpp
+++ src/core/unittest/FrameTest.cpp
@@ -9,83 +9,72 @@ Abstract:
 
 --*/
 
-#include "precomp.h"
-#define LOG_ONLY_FAILURES
-#define INLINE_TEST_METHOD_MARKUP
-#include <wextestclass.h>
-#include <logcontroller.h>
-
+#include "main.h"
 #ifdef QUIC_LOGS_WPP
 #include "frametest.tmh"
 #endif
 
-using namespace WEX::Logging;
-using namespace WEX::Common;
-
-#define VERIFY_QUIC_SUCCESS(result, ...) \
-    VERIFY_ARE_EQUAL(QUIC_STATUS_SUCCESS, result, __VA_ARGS__)
-
-struct FrameTest : public WEX::TestClass<FrameTest>
+uint64_t Encode(uint64_t Value)
 {
-    BEGIN_TEST_CLASS(FrameTest)
-    END_TEST_CLASS()
+    uint64_t Encoded = 0;
+    TEST_TRUE(QuicVarIntEncode(Value, (uint8_t*)&Encoded) != NULL);
+    return Encoded;
+}
 
-    uint64_t Encode(uint64_t Value)
-    {
-        uint64_t Encoded = 0;
-        VERIFY_IS_NOT_NULL(QuicVarIntEncode(Value, (uint8_t*)&Encoded));
-        return Encoded;
+uint64_t Decode(uint64_t Encoded)
+{
+    uint64_t Decoded;
+    UINT16 Offset = 0;
+    TEST_TRUE(QuicVarIntDecode(sizeof(Encoded), (uint8_t*)&Encoded, &Offset, &Decoded));
+    return Decoded;
+}
+
+void
+FrameTestWellKnownEncode(
+    )
+{
+    TEST_EQUAL(Encode(0), 0);
+    TEST_EQUAL(Encode(0x3F), 0x3F);
+    TEST_EQUAL(Encode(0x40), 0x4040);
+    TEST_EQUAL(Encode(0x3FFF), 0xFF7F);
+    TEST_EQUAL(Encode(0x4000), 0x400080);
+    TEST_EQUAL(Encode(0x3FFFFFFF), 0xFFFFFFBF);
+    TEST_EQUAL(Encode(0x40000000), 0x40000000C0ULL);
+    TEST_EQUAL(Encode(0x3FFFFFFFFFFFFFFF), 0xFFFFFFFFFFFFFFFF);
+}
+
+void
+FrameTestWellKnownDecode(
+    )
+{
+    TEST_EQUAL(Decode(0), 0);
+    TEST_EQUAL(Decode(0x3F), 0x3F);
+    TEST_EQUAL(Decode(0x4040), 0x40);
+    TEST_EQUAL(Decode(0xFF7F), 0x3FFF);
+    TEST_EQUAL(Decode(0x400080), 0x4000);
+    TEST_EQUAL(Decode(0xFFFFFFBF), 0x3FFFFFFF);
+    TEST_EQUAL(Decode(0x40000000C0ULL), 0x40000000);
+    TEST_EQUAL(Decode(0xFFFFFFFFFFFFFFFF), 0x3FFFFFFFFFFFFFFFULL);
+}
+
+void
+FrameTestRandomEncodeDecode(
+    )
+{
+    for (uint32_t i = 0; i < 1000; i++) {
+
+        //
+        // Generate a random value and make sure the top 2 bits aren't set.
+        //
+        uint64_t Value;
+        TEST_QUIC_SUCCEEDED(QuicRandom(sizeof(Value), &Value));
+        Value &= ~(3ULL << 62);
+
+        //
+        // Encode the value, decode the result and compare to the original value.
+        //
+        uint64_t Encoded = Encode(Value);
+        uint64_t Decoded = Decode(Encoded);
+        TEST_EQUAL(Value, Decoded);
     }
-
-    uint64_t Decode(uint64_t Encoded)
-    {
-        uint64_t Decoded;
-        UINT16 Offset = 0;
-        VERIFY_IS_TRUE(QuicVarIntDecode(sizeof(Encoded), (uint8_t*)&Encoded, &Offset, &Decoded));
-        return Decoded;
-    }
-
-    TEST_METHOD(WellKnownEncode)
-    {
-        VERIFY_ARE_EQUAL(Encode(0), 0);
-        VERIFY_ARE_EQUAL(Encode(0x3F), 0x3F);
-        VERIFY_ARE_EQUAL(Encode(0x40), 0x4040);
-        VERIFY_ARE_EQUAL(Encode(0x3FFF), 0xFF7F);
-        VERIFY_ARE_EQUAL(Encode(0x4000), 0x400080);
-        VERIFY_ARE_EQUAL(Encode(0x3FFFFFFF), 0xFFFFFFBF);
-        VERIFY_ARE_EQUAL(Encode(0x40000000), 0x40000000C0ULL);
-        VERIFY_ARE_EQUAL(Encode(0x3FFFFFFFFFFFFFFF), 0xFFFFFFFFFFFFFFFF);
-    }
-
-    TEST_METHOD(WellKnownDecode)
-    {
-        VERIFY_ARE_EQUAL(Decode(0), 0);
-        VERIFY_ARE_EQUAL(Decode(0x3F), 0x3F);
-        VERIFY_ARE_EQUAL(Decode(0x4040), 0x40);
-        VERIFY_ARE_EQUAL(Decode(0xFF7F), 0x3FFF);
-        VERIFY_ARE_EQUAL(Decode(0x400080), 0x4000);
-        VERIFY_ARE_EQUAL(Decode(0xFFFFFFBF), 0x3FFFFFFF);
-        VERIFY_ARE_EQUAL(Decode(0x40000000C0ULL), 0x40000000);
-        VERIFY_ARE_EQUAL(Decode(0xFFFFFFFFFFFFFFFF), 0x3FFFFFFFFFFFFFFFULL);
-    }
-
-    TEST_METHOD(RandomEncodeDecode)
-    {
-        for (uint32_t i = 0; i < 1000; i++) {
-
-            //
-            // Generate a random value and make sure the top 2 bits aren't set.
-            //
-            uint64_t Value;
-            VERIFY_QUIC_SUCCESS(QuicRandom(sizeof(Value), &Value));
-            Value &= ~(3ULL << 62);
-
-            //
-            // Encode the value, decode the result and compare to the original value.
-            //
-            uint64_t Encoded = Encode(Value);
-            uint64_t Decoded = Decode(Encoded);
-            VERIFY_ARE_EQUAL(Value, Decoded);
-        }
-    }
-};
+}
--- src/core/unittest/main.cpp
+++ src/core/unittest/main.cpp
@@ -5,39 +5,303 @@
 
 --*/
 
-#include "precomp.h"
-#define LOG_ONLY_FAILURES
-#define INLINE_TEST_METHOD_MARKUP
-#include <wextestclass.h>
-#include <logcontroller.h>
+#include "main.h"
 
 #ifdef QUIC_LOGS_WPP
 #include "main.tmh"
 #endif
 
-using namespace WEX::Common;
+extern "C" _IRQL_requires_max_(PASSIVE_LEVEL) void QuicTraceRundown(void) { }
 
-BEGIN_MODULE()
-    MODULE_PROPERTY(L"BinaryUnderTest", L"core.lib")
-    MODULE_PROPERTY(L"Description", L"Tests the QUIC core user-mode library")
-    MODULE_PROPERTY(L"Owner", L"nibanks")
-END_MODULE()
-
-extern "C" void QuicTraceRundown(void) { }
-
-MODULE_SETUP(GlobalTestSetup)
-{
-    QuicPlatformSystemLoad();
-    if (QUIC_FAILED(QuicPlatformInitialize())) {
-        QuicPlatformSystemUnload();
-        return false;
+class QuicCoreTestEnvironment : public ::testing::Environment {
+public:
+    void SetUp() override {
+        QuicPlatformSystemLoad();
+        ASSERT_TRUE(QUIC_SUCCEEDED(QuicPlatformInitialize()));
     }
-    return true;
+    void TearDown() override {
+        QuicPlatformUninitialize();
+        QuicPlatformSystemUnload();
+    }
+};
+
+void
+LogTestFailure(
+    _In_z_ const char* File,
+    _In_z_ const char* Function,
+    int Line,
+    _Printf_format_string_ const char* Format,
+    ...
+    )
+{
+    UNREFERENCED_PARAMETER(Function);
+    char Buffer[128];
+    va_list Args;
+    va_start(Args, Format);
+    (void)_vsnprintf_s(Buffer, sizeof(Buffer), _TRUNCATE, Format, Args);
+    va_end(Args);
+    QuicTraceLogError("[test] FAILURE - %s:%d - %s", File, Line, Buffer);
+    GTEST_MESSAGE_AT_(File, Line, Buffer, ::testing::TestPartResult::kFatalFailure);
 }
 
-MODULE_CLEANUP(GlobalTestCleanup)
-{
-    QuicPlatformUninitialize();
-    QuicPlatformSystemUnload();
-    return true;
+struct TestLogger {
+    const char* TestName;
+    TestLogger(const char* Name) : TestName(Name) {
+        QuicTraceLogInfo("[test] START %s", TestName);
+    }
+    ~TestLogger() {
+        QuicTraceLogInfo("[test] END %s", TestName);
+    }
+};
+
+template<class T>
+struct TestLoggerT {
+    const char* TestName;
+    TestLoggerT(const char* Name, const T& Params) : TestName(Name) {
+        std::ostringstream stream; stream << Params;
+        QuicTraceLogInfo("[test] START %s, %s", TestName, stream.str().c_str());
+    }
+    ~TestLoggerT() {
+        QuicTraceLogInfo("[test] END %s", TestName);
+    }
+};
+
+//
+// Frame tests
+//
+TEST(FrameTest, WellKnownEncode) {
+    TestLogger Logger("FrameTestWellKnownEncode");
+    FrameTestWellKnownEncode();
+}
+
+TEST(FrameTest, WellKnownDecode) {
+    TestLogger Logger("FrameTestWellKnownDecode");
+    FrameTestWellKnownDecode();
+}
+
+TEST(FrameTest, RandomEncodeDecode) {
+    TestLogger Logger("FrameTestRandomEncodeDecode");
+    FrameTestRandomEncodeDecode();
+}
+
+//
+// Packet number tests
+//
+TEST(PacketNumberTest, WellKnownDecompress) {
+    TestLogger Logger("FrameTestRandomEncodeDecode");
+    PacketNumberTestWellKnownDecompress();
+}
+
+//
+// Range test
+//
+TEST(RangeTest, AddSingle) {
+    TestLogger Logger("RangeTestAddSingle");
+    RangeTestAddSingle();
+}
+
+TEST(RangeTest, AddTwoAdjacentBefore) {
+    TestLogger Logger("RangeTestAddTwoAdjacentBefore");
+    RangeTestAddTwoAdjacentBefore();
+}
+
+TEST(RangeTest, AddTwoAdjacentAfter) {
+    TestLogger Logger("RangeTestAddTwoAdjacentAfter");
+    RangeTestAddTwoAdjacentAfter();
+}
+
+TEST(RangeTest, AddTwoSeparateBefore) {
+    TestLogger Logger("RangeTestAddTwoSeparateBefore");
+    RangeTestAddTwoSeparateBefore();
+}
+
+TEST(RangeTest, AddTwoSeparateAfter) {
+    TestLogger Logger("RangeTestAddTwoSeparateAfter");
+    RangeTestAddTwoSeparateAfter();
+}
+
+TEST(RangeTest, AddThreeMerge) {
+    TestLogger Logger("RangeTestAddThreeMerge");
+    RangeTestAddThreeMerge();
+}
+
+TEST(RangeTest, AddBetween) {
+    TestLogger Logger("RangeTestAddBetween");
+    RangeTestAddBetween();
+}
+
+TEST(RangeTest, AddRangeSingle) {
+    TestLogger Logger("RangeTestAddRangeSingle");
+    RangeTestAddRangeSingle();
+}
+
+TEST(RangeTest, AddRangeBetween) {
+    TestLogger Logger("RangeTestAddRangeBetween");
+    RangeTestAddRangeBetween();
+}
+
+TEST(RangeTest, AddRangeTwoAdjacentBefore) {
+    TestLogger Logger("RangeTestAddRangeTwoAdjacentBefore");
+    RangeTestAddRangeTwoAdjacentBefore();
+}
+
+TEST(RangeTest, AddRangeTwoAdjacentAfter) {
+    TestLogger Logger("RangeTestAddRangeTwoAdjacentAfter");
+    RangeTestAddRangeTwoAdjacentAfter();
+}
+
+TEST(RangeTest, AddRangeTwoSeparateBefore) {
+    TestLogger Logger("RangeTestAddRangeTwoSeparateBefore");
+    RangeTestAddRangeTwoSeparateBefore();
+}
+
+TEST(RangeTest, AddRangeTwoSeparateAfter) {
+    TestLogger Logger("RangeTestAddRangeTwoSeparateAfter");
+    RangeTestAddRangeTwoSeparateAfter();
+}
+
+TEST(RangeTest, AddRangeTwoOverlapBefore1) {
+    TestLogger Logger("RangeTestAddRangeTwoOverlapBefore1");
+    RangeTestAddRangeTwoOverlapBefore1();
+}
+
+TEST(RangeTest, AddRangeTwoOverlapBefore2) {
+    TestLogger Logger("RangeTestAddRangeTwoOverlapBefore2");
+    RangeTestAddRangeTwoOverlapBefore2();
+}
+
+TEST(RangeTest, AddRangeTwoOverlapBefore3) {
+    TestLogger Logger("RangeTestAddRangeTwoOverlapBefore3");
+    RangeTestAddRangeTwoOverlapBefore3();
+}
+
+TEST(RangeTest, AddRangeTwoOverlapAfter1) {
+    TestLogger Logger("RangeTestAddRangeTwoOverlapAfter1");
+    RangeTestAddRangeTwoOverlapAfter1();
+}
+
+TEST(RangeTest, AddRangeTwoOverlapAfter2) {
+    TestLogger Logger("RangeTestAddRangeTwoOverlapAfter2");
+    RangeTestAddRangeTwoOverlapAfter2();
+}
+
+TEST(RangeTest, AddRangeThreeMerge) {
+    TestLogger Logger("RangeTestAddRangeThreeMerge");
+    RangeTestAddRangeThreeMerge();
+}
+
+TEST(RangeTest, AddRangeThreeOverlapAndAdjacentAfter1) {
+    TestLogger Logger("RangeTestAddRangeThreeOverlapAndAdjacentAfter1");
+    RangeTestAddRangeThreeOverlapAndAdjacentAfter1();
+}
+
+TEST(RangeTest, AddRangeThreeOverlapAndAdjacentAfter2) {
+    TestLogger Logger("RangeTestAddRangeThreeOverlapAndAdjacentAfter2");
+    RangeTestAddRangeThreeOverlapAndAdjacentAfter2();
+}
+
+TEST(RangeTest, AddRangeThreeOverlapAndAdjacentAfter3) {
+    TestLogger Logger("RangeTestAddRangeThreeOverlapAndAdjacentAfter3");
+    RangeTestAddRangeThreeOverlapAndAdjacentAfter3();
+}
+
+TEST(RangeTest, AddRangeThreeOverlapAndAdjacentAfter4) {
+    TestLogger Logger("RangeTestAddRangeThreeOverlapAndAdjacentAfter4");
+    RangeTestAddRangeThreeOverlapAndAdjacentAfter4();
+}
+
+TEST(RangeTest, RemoveRangeBefore) {
+    TestLogger Logger("RangeTestRemoveRangeBefore");
+    RangeTestRemoveRangeBefore();
+}
+
+TEST(RangeTest, RemoveRangeAfter) {
+    TestLogger Logger("RangeTestRemoveRangeAfter");
+    RangeTestRemoveRangeAfter();
+}
+
+TEST(RangeTest, RemoveRangeFront) {
+    TestLogger Logger("RangeTestRemoveRangeFront");
+    RangeTestRemoveRangeFront();
+}
+
+TEST(RangeTest, RemoveRangeBack) {
+    TestLogger Logger("RangeTestRemoveRangeBack");
+    RangeTestRemoveRangeBack();
+}
+
+TEST(RangeTest, RemoveRangeAll) {
+    TestLogger Logger("RangeTestRemoveRangeAll");
+    RangeTestRemoveRangeAll();
+}
+
+TEST(RangeTest, ExampleAckTest) {
+    TestLogger Logger("RangeTestExampleAckTest");
+    RangeTestExampleAckTest();
+}
+
+TEST(RangeTest, ExampleAckWithLossTest) {
+    TestLogger Logger("RangeTestExampleAckWithLossTest");
+    RangeTestExampleAckWithLossTest();
+}
+
+TEST(RangeTest, AddLots) {
+    TestLogger Logger("RangeTestAddLots");
+    RangeTestAddLots();
+}
+
+TEST(RangeTest, HitMax) {
+    TestLogger Logger("RangeTestHitMax");
+    RangeTestHitMax();
+}
+
+TEST(RangeTest, SearchZero) {
+    TestLogger Logger("RangeTestSearchZero");
+    RangeTestSearchZero();
+}
+
+TEST(RangeTest, SearchOne) {
+    TestLogger Logger("RangeTestSearchOne");
+    RangeTestSearchOne();
+}
+
+TEST(RangeTest, SearchTwo) {
+    TestLogger Logger("RangeTestSearchTwo");
+    RangeTestSearchTwo();
+}
+
+TEST(RangeTest, SearchThree) {
+    TestLogger Logger("RangeTestSearchThree");
+    RangeTestSearchThree();
+}
+
+TEST(RangeTest, SearchFour) {
+    TestLogger Logger("RangeTestSearchFour");
+    RangeTestSearchFour();
+}
+
+TEST(RangeTest, SearchRangeZero) {
+    TestLogger Logger("RangeTestSearchRangeZero");
+    RangeTestSearchRangeZero();
+}
+
+TEST(RangeTest, SearchRangeOne) {
+    TestLogger Logger("RangeTestSearchRangeOne");
+    RangeTestSearchRangeOne();
+}
+
+TEST(RangeTest, SearchRangeTwo) {
+    TestLogger Logger("RangeTestSearchRangeTwo");
+    RangeTestSearchRangeTwo();
+}
+
+TEST(RangeTest, SearchRangeThree) {
+    TestLogger Logger("RangeTestSearchRangeThree");
+    RangeTestSearchRangeThree();
+}
+
+int main(int argc, char** argv) {
+    ::testing::AddGlobalTestEnvironment(new QuicCoreTestEnvironment);
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
