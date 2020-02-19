4116e40ee21c7dcad6a48eb7823b4a69eeb7f44a
Max <628527+Maximus-@users.noreply.github.com>
2020-02-19 15:05:34 -0800

    Enable abort_on_error for ASAN;
    Abort on error ensures that ASAN calls abort(), which is signals SIGABRT, which, when uncaught, creates a core dump.

--- .azure/scripts/run-executable.ps1
+++ .azure/scripts/run-executable.ps1
@@ -145,7 +145,7 @@ function Start-Executable {
             }
         } else {
             $pinfo.FileName = "bash"
-            $pinfo.Arguments = "-c `"ulimit -c unlimited && ASAN_OPTIONS=disable_coredump=0 $($Path) $($Arguments) && echo Done`""
+            $pinfo.Arguments = "-c `"ulimit -c unlimited && ASAN_OPTIONS=disable_coredump=0:abort_on_error=1 $($Path) $($Arguments) && echo Done`""
             $pinfo.WorkingDirectory = $LogDir
         }
     }
