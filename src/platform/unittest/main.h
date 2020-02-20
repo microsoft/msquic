fd512d171f5bc19c62c4c48d5c61717e31c7b102
Anthony Rossi <anrossi@microsoft.com>
2020-02-19 16:08:36 -0800

    Add platform unit tests and get them building.

---
+++ src/platform/unittest/main.h
@@ -0,0 +1,19 @@
+/*++
+
+    Copyright (c) Microsoft Corporation.
+    Licensed under the MIT License.
+
+--*/
+
+#undef min // gtest headers conflict with previous definitions of min/max.
+#undef max
+#include "gtest/gtest.h"
+
+#include "quic_platform.h"
+
+#include "quic_trace.h"
+
+#define VERIFY_QUIC_SUCCESS(result) ASSERT_EQ(QUIC_STATUS_SUCCESS, result)
+
+#define GTEST_SKIP_NO_RETURN_(message) \
+  GTEST_MESSAGE_(message, ::testing::TestPartResult::kSkip)
