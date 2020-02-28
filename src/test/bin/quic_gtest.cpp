2505b8dcd0320091f66e99c5ed47018334e3b6dd
Nick Banks <nibanks@microsoft.com>
2020-02-28 13:01:41 -0800

    Fix Kernel Mode Flag String Compare (#196)

--- src/test/bin/quic_gtest.cpp
+++ src/test/bin/quic_gtest.cpp
@@ -833,7 +833,7 @@ INSTANTIATE_TEST_CASE_P(
 
 int main(int argc, char** argv) {
     for (int i = 0; i < argc; ++i) {
-        if (strcmp("--kernel", argv[i])) {
+        if (strcmp("--kernel", argv[i]) == 0) {
             TestingKernelMode = true;
             break;
         }
