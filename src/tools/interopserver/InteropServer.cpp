85978962e287a079cfcc2e003b69747c866d3fcc
Nick Banks <nibanks@microsoft.com>
2020-03-01 11:49:24 -0800

    Add Interop Server Code

---
+++ src/tools/interopserver/InteropServer.cpp
@@ -0,0 +1,300 @@
+/*++
+
+    Copyright (c) Microsoft Corporation.
+    Licensed under the MIT License.
+
+Abstract:
+
+    Very Simple QUIC HTTP 1.1 GET server.
+
+--*/
+
+#include "InteropServer.h"
+
+QUIC_API_V1* MsQuic;
+QUIC_SEC_CONFIG* SecurityConfig;
+const char* RootFolderPath;
+
+const char* SupportedALPNs[] = {
+    ALPN_HTTP_OVER_QUIC,
+};
+
+void
+PrintUsage()
+{
+    printf("interopserver is simple http 0.9/1.1 server.\n\n");
+
+    printf("Usage:\n");
+    printf("  interopserver.exe -listen:<addr or *> -root:<path>"
+           " [-thumbprint:<cert_thumbprint>] [-name:<cert_name>]"
+           " [-file:<cert_filepath> AND -key:<cert_key_filepath>]"
+           " [-port:<####> (def:%u)]  [-retry:<0/1> (def:%u)]\n\n",
+           DEFAULT_QUIC_HTTP_SERVER_PORT, DEFAULT_QUIC_HTTP_SERVER_RETRY);
+
+    printf("Examples:\n");
+    printf("  interopserver.exe -listen:127.0.0.1 -name:localhost -port:443 -root:c:\\temp\n");
+    printf("  interopserver.exe -listen:* -retry:1 -thumbprint:175342733b39d81c997817296c9b691172ca6b6e -root:c:\\temp\n");
+}
+
+int
+QUIC_MAIN_EXPORT
+main(
+    _In_ int argc,
+    _In_reads_(argc) _Null_terminated_ char* argv[]
+    )
+{
+    if (argc < 2 ||
+        GetValue(argc, argv, "help") ||
+        GetValue(argc, argv, "?")) {
+        PrintUsage();
+        return -1;
+    }
+
+    HQUIC Registration = nullptr;
+    EXIT_ON_FAILURE(MsQuicOpenV1(&MsQuic));
+    EXIT_ON_FAILURE(MsQuic->RegistrationOpen("interopserver", &Registration));
+
+    //
+    // Optional parameters.
+    //
+    uint16_t LocalPort = DEFAULT_QUIC_HTTP_SERVER_PORT;
+    BOOLEAN Retry = DEFAULT_QUIC_HTTP_SERVER_RETRY;
+    TryGetValue(argc, argv, "port", &LocalPort);
+    TryGetValue(argc, argv, "retry", &Retry);
+    if (Retry) {
+        EXIT_ON_FAILURE(
+            QuicForceRetry(
+                MsQuic,
+                Registration,
+                true));
+        printf("Enabling forced RETRY on server.\n");
+    }
+
+    //
+    // Required parameters.
+    //
+    const char* ListenAddrStr = nullptr;
+    SOCKADDR_INET ListenAddr = { 0 };
+    if (!TryGetValue(argc, argv, "listen", &ListenAddrStr) ||
+        !ConvertArgToAddress(ListenAddrStr, LocalPort, &ListenAddr)) {
+        printf("Missing or invalid '-listen' arg!\n");
+        return -1;
+    }
+    if (!TryGetValue(argc, argv, "root", &RootFolderPath)) {
+        printf("Missing '-root' arg!\n");
+        return -1;
+    }
+
+    const char* CertThumbprint = nullptr;
+    const char* CertName = nullptr;
+    const char* CertFile = nullptr;
+    const char* CertKeyFile = nullptr;
+    if (TryGetValue(argc, argv, "thumbprint", &CertThumbprint)) {
+        SecurityConfig = GetSecConfigForThumbprint(MsQuic, Registration, CertThumbprint);
+        if (SecurityConfig == nullptr) {
+            printf("Failed to find certificate from thumbprint:'%s'.\n", CertThumbprint);
+            return -1;
+        }
+    } else if (TryGetValue(argc, argv, "name", &CertName)) {
+        SecurityConfig = GetSecConfigForSNI(MsQuic, Registration, CertName);
+        if (SecurityConfig == nullptr) {
+            printf("Failed to find certificate from name:'%s'.\n", CertName);
+            return -1;
+        }
+    } else if (TryGetValue(argc, argv, "file", &CertFile) &&
+        TryGetValue(argc, argv, "key", &CertKeyFile)) {
+        SecurityConfig = GetSecConfigForFile(MsQuic, Registration, CertKeyFile, CertFile);
+        if (SecurityConfig == nullptr) {
+            printf("Failed to find certificate from file:'%s'.\n", CertFile);
+            return -1;
+        }
+    } else {
+        printf("Missing arg loading server certificate!\n");
+        return -1;
+    }
+
+    HttpSession* Sessions[ARRAYSIZE(SupportedALPNs)] = {0};
+    for (uint32_t i = 0; i < ARRAYSIZE(Sessions); ++i) {
+        Sessions[i] = new HttpSession(Registration, SupportedALPNs[i], &ListenAddr);
+    }
+
+    printf("Press Enter to exit.\n\n");
+    getchar();
+
+    for (uint32_t i = 0; i < ARRAYSIZE(Sessions); ++i) {
+        delete Sessions[i];
+    }
+
+    MsQuic->SecConfigDelete(SecurityConfig);
+    MsQuic->RegistrationClose(Registration);
+    MsQuicClose(MsQuic);
+
+    return 0;
+}
+
+//
+// HttpRequest
+//
+
+HttpRequest::HttpRequest(HttpConnection *connection, HQUIC stream) :
+    Connection(connection), QuicStream(stream), File(nullptr),
+    Shutdown(false), WriteHttp11Header(false)
+{
+    MsQuic->SetCallbackHandler(QuicStream, QuicCallbackHandler, this);
+    Connection->AddRef();
+}
+
+HttpRequest::~HttpRequest()
+{
+    if (File) {
+        fclose(File);
+    }
+    MsQuic->StreamClose(QuicStream);
+    Connection->Release();
+}
+
+void
+HttpRequest::Process()
+{
+    if (Shutdown) {
+        return;
+    }
+
+    QUIC_BUFFER* QuicBuffer = &Buffer.QuicBuffer;
+
+    if (QuicBuffer->Length < 5 ||
+        _strnicmp((const char*)QuicBuffer->Buffer, "get ", 4) != 0) {
+        Abort();
+        return;
+    }
+
+    char* PathStart = (char*)QuicBuffer->Buffer + 4;
+
+    char* end = strpbrk(PathStart, " \r\n");
+    if (end != NULL) {
+        if (*end == ' ') {
+            WriteHttp11Header = true;
+        }
+        *end = '\0';
+    }
+
+    if (strstr(PathStart, "..") == nullptr) {
+        Abort(); // Don't allow requests with ../ in them.
+        return;
+    }
+
+    char fullFilePath[256];
+    strcpy(fullFilePath, RootFolderPath);
+    if (strcmp("/", PathStart) == 0) {
+        strcat(fullFilePath, "/index.html");
+    } else {
+        strcat(fullFilePath, PathStart);
+    }
+
+    printf("GET '%s'\n", PathStart);
+    File = fopen(fullFilePath, "rb"); // In case of failure, SendData still works.
+
+    SendData();
+}
+
+void
+HttpRequest::SendData()
+{
+    if (Shutdown) {
+        return;
+    }
+
+    Buffer.Reset();
+
+    if (File) {
+        if (WriteHttp11Header) {
+            const char Http11ResponseHeaders[] = "HTTP/1.1 200 OK\r\nConnection: Close\r\n\r\n";
+            Buffer.Write(Http11ResponseHeaders, sizeof(Http11ResponseHeaders));
+            WriteHttp11Header = false;
+        }
+
+        while (!Buffer.IsFull()) {
+            size_t BytesAvail = IO_SIZE - Buffer.QuicBuffer.Length;
+            size_t BytesRead =
+                fread(
+                    Buffer.RawBuffer + Buffer.QuicBuffer.Length,
+                    BytesAvail,
+                    1,
+                    File);
+            Buffer.QuicBuffer.Length += (uint32_t)BytesRead;
+            if (BytesAvail != BytesRead) {
+                Buffer.Flags |= QUIC_SEND_FLAG_FIN;
+                Shutdown = true;
+                break;
+            }
+        }
+
+    } else {
+        const char BadRequestBuffer11[] = "HTTP/1.1 400 BAD REQUEST\r\nConnection: Close\r\n\r\n";
+        const char BadRequestBuffer[] = "BAD REQUEST";
+        const char *ResponseBuffer = BadRequestBuffer;
+        uint32_t ResponseBufferSize = sizeof(BadRequestBuffer) - 1;
+
+        if (WriteHttp11Header) {
+            ResponseBuffer = BadRequestBuffer11;
+            ResponseBufferSize = sizeof(BadRequestBuffer11) - 1;
+            WriteHttp11Header = false;
+        }
+
+        Buffer.Write(ResponseBuffer, ResponseBufferSize);
+        Buffer.Flags |= QUIC_SEND_FLAG_FIN;
+        Shutdown = true;
+    }
+
+    if (QUIC_FAILED(
+        MsQuic->StreamSend(
+            QuicStream,
+            &Buffer.QuicBuffer,
+            1,
+            Buffer.Flags,
+            this))) {
+        Abort();
+    }
+}
+
+QUIC_STATUS
+QUIC_API
+HttpRequest::QuicCallbackHandler(
+    _In_ HQUIC Stream,
+    _In_opt_ void* Context,
+    _Inout_ QUIC_STREAM_EVENT* Event
+    )
+{
+    auto pThis = (HttpRequest*)Context;
+    auto Buffer = &pThis->Buffer;
+
+    switch (Event->Type) {
+    case QUIC_STREAM_EVENT_RECEIVE:
+        if (!Buffer->HasRoom(Event->RECEIVE.TotalBufferLength)) {
+            pThis->Abort();
+        } else {
+            for (uint32_t i = 0; i < Event->RECEIVE.BufferCount; ++i) {
+                Buffer->Write(
+                    Event->RECEIVE.Buffers[i].Buffer,
+                    Event->RECEIVE.Buffers[i].Length);
+            }
+            Buffer->QuicBuffer.Buffer[Buffer->QuicBuffer.Length] = 0;
+        }
+        break;
+    case QUIC_STREAM_EVENT_SEND_COMPLETE:
+        pThis->SendData();
+        break;
+    case QUIC_STREAM_EVENT_PEER_SEND_SHUTDOWN:
+        pThis->Process();
+        break;
+    case QUIC_STREAM_EVENT_PEER_SEND_ABORTED:
+    case QUIC_STREAM_EVENT_PEER_RECEIVE_ABORTED:
+        pThis->Abort();
+        break;
+    case QUIC_STREAM_EVENT_SHUTDOWN_COMPLETE:
+        delete pThis;
+        break;
+    }
+
+    return QUIC_STATUS_SUCCESS;
+}
---
+++ src/tools/interopserver/InteropServer.h
@@ -0,0 +1,244 @@
+/*++
+
+    Copyright (c) Microsoft Corporation.
+    Licensed under the MIT License.
+
+--*/
+
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <msquichelper.h>
+
+extern QUIC_API_V1* MsQuic;
+extern QUIC_SEC_CONFIG* SecurityConfig;
+
+//
+// The default port used for connecting with QuicHttpServer.
+//
+#define DEFAULT_QUIC_HTTP_SERVER_PORT 4433
+
+//
+// The default retry option for QuicHttpServer.
+//
+#define DEFAULT_QUIC_HTTP_SERVER_RETRY FALSE
+
+//
+// The maximum requests the server accepts per connection.
+//
+#define MAX_HTTP_REQUESTS_PER_CONNECTION 100
+
+//
+// The send IO size to use.
+//
+#define IO_SIZE 64 * 1024
+
+//
+// Exits if there is a failure.
+//
+#define EXIT_ON_FAILURE(x) do { \
+    auto _Status = x; \
+    if (QUIC_FAILED(_Status)) { \
+       printf("%s:%d %s failed!\n", __FILE__, __LINE__, #x); \
+       exit(1); \
+    } \
+} while (0);
+
+struct HttpSendBuffer {
+    QUIC_SEND_FLAGS Flags;
+    QUIC_BUFFER QuicBuffer;
+    uint8_t RawBuffer[IO_SIZE];
+    HttpSendBuffer() {
+        Flags = QUIC_SEND_FLAG_NONE;
+        QuicBuffer.Buffer = RawBuffer;
+        QuicBuffer.Length = 0;
+    }
+    bool IsFull() const { return QuicBuffer.Length == IO_SIZE; }
+    bool HasRoom(uint64_t Length) const { return Length + QuicBuffer.Length < IO_SIZE; }
+    void Write(const void* Buffer, uint32_t Length) {
+        memcpy(QuicBuffer.Buffer + QuicBuffer.Length, Buffer, Length);
+        QuicBuffer.Length += Length;
+    }
+    void Reset() {
+        QuicBuffer.Length = 0;
+    }
+};
+
+struct HttpConnection;
+
+struct HttpRequest {
+    HttpRequest(HttpConnection *connection, HQUIC stream);
+private:
+    HttpConnection *Connection;
+    HQUIC QuicStream;
+    FILE* File;
+    HttpSendBuffer Buffer;
+    bool Shutdown;
+    bool WriteHttp11Header;
+private:
+    ~HttpRequest();
+    void Abort(QUIC_UINT62 ErrorCode = 0) {
+        Shutdown = true;
+        MsQuic->StreamShutdown(
+            QuicStream,
+            QUIC_STREAM_SHUTDOWN_FLAG_ABORT,
+            ErrorCode);
+    }
+    void Process();
+    void SendData();
+    static
+    QUIC_STATUS
+    QUIC_API
+    QuicCallbackHandler(
+        _In_ HQUIC Stream,
+        _In_opt_ void* Context,
+        _Inout_ QUIC_STREAM_EVENT* Event
+        );
+};
+
+struct HttpConnection {
+    HttpConnection(HQUIC connection) :
+        QuicConnection(connection), RefCount(1) {
+        MsQuic->SetCallbackHandler(QuicConnection, QuicCallbackHandler, this);
+        printf("[%p] From %s\n", QuicConnection, GetRemoteAddr(MsQuic, QuicConnection).Address);
+    }
+    ~HttpConnection() {
+        MsQuic->ConnectionClose(QuicConnection);
+    }
+    void AddRef() {
+        InterlockedIncrement(&RefCount);
+    }
+    void Release() {
+        if (InterlockedDecrement(&RefCount) == 0) {
+            delete this;
+        }
+    }
+private:
+    HQUIC QuicConnection;
+    long RefCount;
+private:
+    static
+    QUIC_STATUS
+    QUIC_API
+    QuicCallbackHandler(
+        _In_ HQUIC Connection,
+        _In_opt_ void* Context,
+        _Inout_ QUIC_CONNECTION_EVENT* Event
+        ) {
+        HttpConnection *pThis = (HttpConnection*)Context;
+        switch (Event->Type) {
+        case QUIC_CONNECTION_EVENT_PEER_STREAM_STARTED:
+            if (Event->PEER_STREAM_STARTED.Flags & QUIC_STREAM_OPEN_FLAG_UNIDIRECTIONAL) {
+                MsQuic->SetCallbackHandler(
+                    Event->PEER_STREAM_STARTED.Stream,
+                    HttpUnidirectionalStreamCallback,
+                    nullptr);
+            } else {
+                new HttpRequest(pThis, Event->PEER_STREAM_STARTED.Stream);
+            }
+            break;
+        case QUIC_CONNECTION_EVENT_SHUTDOWN_COMPLETE:
+            pThis->Release();
+            break;
+        }
+        return QUIC_STATUS_SUCCESS;
+    }
+    static
+    _Function_class_(QUIC_STREAM_CALLBACK)
+    QUIC_STATUS
+    HttpUnidirectionalStreamCallback(
+        _In_ HQUIC Stream,
+        _In_opt_ void* /* Context */,
+        _Inout_ QUIC_STREAM_EVENT* Event
+        )
+    {
+        if (Event->Type == QUIC_STREAM_EVENT_SHUTDOWN_COMPLETE) {
+            //
+            // Don't care about anything else on the stream except closing it in
+            // resposne to shutdown complete.
+            //
+            MsQuic->StreamClose(Stream);
+        }
+        return QUIC_STATUS_SUCCESS;
+    }
+};
+
+struct HttpServer {
+    HttpServer(HQUIC Session, const QUIC_ADDR* LocalAddress) {
+        EXIT_ON_FAILURE(
+            MsQuic->ListenerOpen(
+                Session,
+                QuicCallbackHandler,
+                this,
+                &QuicListener));
+        EXIT_ON_FAILURE(
+            MsQuic->ListenerStart(
+                QuicListener,
+                LocalAddress));
+    }
+    ~HttpServer() {
+        MsQuic->ListenerClose(QuicListener);
+    }
+private:
+    HQUIC QuicListener;
+private:
+    static
+    QUIC_STATUS
+    QUIC_API
+    QuicCallbackHandler(
+        _In_ HQUIC,
+        _In_opt_ void*,
+        _Inout_ QUIC_LISTENER_EVENT* Event
+        ) {
+        if (Event->Type == QUIC_LISTENER_EVENT_NEW_CONNECTION) {
+            Event->NEW_CONNECTION.SecurityConfig = SecurityConfig;
+            new HttpConnection(Event->NEW_CONNECTION.Connection);
+            return QUIC_STATUS_SUCCESS;
+        }
+        return QUIC_STATUS_NOT_SUPPORTED;
+    }
+};
+
+struct HttpSession {
+    HttpSession(
+        HQUIC Registration,
+        const char* Alpn,
+        const QUIC_ADDR* LocalAddress) {
+        EXIT_ON_FAILURE(
+            MsQuic->SessionOpen(
+                Registration,
+                Alpn,
+                nullptr,
+                &Session));
+
+        uint16_t PeerBidiStreamCount = MAX_HTTP_REQUESTS_PER_CONNECTION;
+        uint16_t PeerUnidiStreamCount = 1; // We allow 1 unidirectional stream, just for interop tests.
+
+        EXIT_ON_FAILURE(
+            MsQuic->SetParam(
+                Session,
+                QUIC_PARAM_LEVEL_SESSION,
+                QUIC_PARAM_SESSION_PEER_BIDI_STREAM_COUNT,
+                sizeof(PeerBidiStreamCount),
+                &PeerBidiStreamCount));
+        EXIT_ON_FAILURE(
+            MsQuic->SetParam(
+                Session,
+                QUIC_PARAM_LEVEL_SESSION,
+                QUIC_PARAM_SESSION_PEER_UNIDI_STREAM_COUNT,
+                sizeof(PeerUnidiStreamCount),
+                &PeerUnidiStreamCount));
+
+        Server = new HttpServer(Session, LocalAddress);
+    }
+    ~HttpSession() {
+        delete Server;
+        MsQuic->SessionShutdown(
+            Session,
+            QUIC_CONNECTION_SHUTDOWN_FLAG_NONE,
+            0);
+        MsQuic->SessionClose(Session);
+    }
+private:
+    HQUIC Session;
+    HttpServer* Server;
+};
