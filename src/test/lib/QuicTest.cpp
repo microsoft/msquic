e3943eb235bc58fbb4090576110229b407606286
Chris Gray (WINDOWS PHONE) <chgray@microsoft.com>
2020-03-08 09:13:27 -0700

    Back out accidental check in of converted files

--- src/test/lib/QuicTest.cpp
+++ src/test/lib/QuicTest.cpp
@@ -42,7 +42,7 @@ void QuicTestInitialize()
             QUIC_PARAM_REGISTRATION_ENCRYPTION,
             sizeof(Disabled),
             &Disabled))) {
-        QuicTraceLogError(FN_quictest9eddd9dc5266f22ce5fe2958d12ddae0, "[test] Disabling encryption failed");
+        QuicTraceLogError("[test] Disabling encryption failed");
     }
 #endif
 }
@@ -55,10 +55,10 @@ struct TestScopeLogger
 {
     const char* Name;
     TestScopeLogger(const char* name) : Name(name) {
-        QuicTraceLogInfo(FN_quictest0785c7e818b20fe296fb9343da4ad6c7, "[test]---> %s",  Name);
+        QuicTraceLogInfo("[test]---> %s", Name);
     }
     ~TestScopeLogger() {
-        QuicTraceLogInfo(FN_quictest3a72f018a4cf860262d89c7671534363, "[test]<--- %s",  Name);
+        QuicTraceLogInfo("[test]<--- %s", Name);
     }
 };
 
