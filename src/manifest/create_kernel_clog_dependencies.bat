56c0be8c83463a65bd13c439fc632189d20278de
chgray <chgray@gmail.com>
2020-03-16 18:25:28 -0700

    PLATFORM env argument shouldnt be set - dotnet.exe apparently reads it

--- src/manifest/create_kernel_clog_dependencies.bat
+++ src/manifest/create_kernel_clog_dependencies.bat
@@ -1,6 +1,6 @@
 set SOLUTION_DIR=%1
-set PLATFORM=%2
-set CONFIGURATION=%3
+set PLATFORM_ARG=%2
+set CONFIGURATION_ARG=%3
 
 cmd /c %SOLUTION_DIR%\scripts\generate_kernel_clog.bat CORE_KERNEL_LIB %SOLUTION_DIR% %SOLUTION_DIR%\src\core 
 cmd /c %SOLUTION_DIR%\scripts\generate_kernel_clog.bat PLATFORM_KERNEL_LIB %SOLUTION_DIR% %SOLUTION_DIR%\src\platform 
@@ -9,5 +9,5 @@ cmd /c %SOLUTION_DIR%\scripts\generate_kernel_clog.bat TEST_BIN_KERNEL_LIB %SOLU
 cmd /c %SOLUTION_DIR%\scripts\generate_kernel_clog.bat TEST_LIB_KERNEL_LIB %SOLUTION_DIR% %SOLUTION_DIR%\src\test\lib 
 
 
-mkdir %SOLUTION_DIR%\bld\winkernel\%PLATFORM%_%CONFIGURATION%_schannel\inc
-mc.exe -um -h %SOLUTION_DIR%\bld\winkernel\%PLATFORM%_%CONFIGURATION%_schannel\inc -r %SOLUTION_DIR%\bld\winkernel\%PLATFORM%_%CONFIGURATION%_schannel\inc %SOLUTION_DIR%\src\manifest\MsQuicEtw.man
+mkdir %SOLUTION_DIR%\bld\winkernel\%PLATFORM_ARG%_%CONFIGURATION_ARG%_schannel\inc
+mc.exe -um -h %SOLUTION_DIR%\bld\winkernel\%PLATFORM_ARG%_%CONFIGURATION_ARG%_schannel\inc -r %SOLUTION_DIR%\bld\winkernel\%PLATFORM_ARG%_%CONFIGURATION_ARG%_schannel\inc %SOLUTION_DIR%\src\manifest\MsQuicEtw.man
