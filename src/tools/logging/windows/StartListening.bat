9a59be7f4e2fa129bef90273667b9b0970157102
chgray <chgray@gmail.com>
2020-03-19 09:26:11 -0700

    remove default config file, those are in clog submodules now, delete StartListening scripts in favor of log.ps1

--- src/tools/logging/windows/StartListening.bat
+++
@@ -1,4 +0,0 @@
-wevtutil um ..\..\..\manifest\MsQuicEtw.man
-wevtutil im ..\..\..\manifest\MsQuicEtw.man /rf:c:\Source\msquic\artifacts\windows\bin\Debug\msquic.dll /mf:c:\Source\msquic\artifacts\windows\bin\Debug\msquic.dll /pf:c:\Source\msquic\artifacts\windows\bin\Debug\msquic.dll
-
-wpr -start ..\..\..\manifest\MsQuic.wprp
