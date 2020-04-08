30f3f10ff98f63cd9781c071d7e4022de4fb6586
Nick Banks <nibanks@microsoft.com>
2020-04-08 11:10:42 -0700

    Fix CLOG_BUGS in the gtest code (#304)

--- src/test/bin/quic_gtest.cpp
+++ src/test/bin/quic_gtest.cpp
@@ -108,17 +108,17 @@ LogTestFailure(
     va_start(Args, Format);
     (void)_vsnprintf_s(Buffer, sizeof(Buffer), _TRUNCATE, Format, Args);
     va_end(Args);
-    CLOG_BUG_TraceLogError(FN_quic_gtestdad587a711d7f49ccd556377f5eff9ed, "[test] FAILURE - %s:%d - %s", File, Line, Buffer);
+    QuicTraceLogError(FN_quic_gtestdad587a711d7f49ccd556377f5eff9ed, "[test] FAILURE - %s:%d - %s", File, Line, Buffer);
     GTEST_MESSAGE_AT_(File, Line, Buffer, ::testing::TestPartResult::kFatalFailure);
 }
 
 struct TestLogger {
     const char* TestName;
     TestLogger(const char* Name) : TestName(Name) {
-        CLOG_BUG_TraceLogInfo(FN_quic_gtestbb702fe0fcf0a2c8153ffeaa97baed28, "[test] START %s", TestName);
+        QuicTraceLogInfo(FN_quic_gtestbb702fe0fcf0a2c8153ffeaa97baed28, "[test] START %s", TestName);
     }
     ~TestLogger() {
-        CLOG_BUG_TraceLogInfo(FN_quic_gtest94df3afe3f5519a593efadf4e20ffaa3, "[test] END %s", TestName);
+        QuicTraceLogInfo(FN_quic_gtest94df3afe3f5519a593efadf4e20ffaa3, "[test] END %s", TestName);
     }
 };
 
@@ -127,10 +127,10 @@ struct TestLoggerT {
     const char* TestName;
     TestLoggerT(const char* Name, const T& Params) : TestName(Name) {
         std::ostringstream stream; stream << Params;
-        CLOG_BUG_TraceLogInfo(FN_quic_gtestdd5240e195ab753f90e16d88b5739619, "[test] START %s, %s", TestName, stream.str().c_str());
+        QuicTraceLogInfo(FN_quic_gtestdd5240e195ab753f90e16d88b5739619, "[test] START %s, %s", TestName, stream.str().c_str());
     }
     ~TestLoggerT() {
-        CLOG_BUG_TraceLogInfo(FN_quic_gtest94df3afe3f5519a593efadf4e20ffaa3, "[test] END %s", TestName);
+        QuicTraceLogInfo(FN_quic_gtest94df3afe3f5519a593efadf4e20ffaa3, "[test] END %s", TestName);
     }
 };
 
