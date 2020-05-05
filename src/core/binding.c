b78e6aa158399431016724ede2401f71407ec9eb
Nick Banks <nibanks@microsoft.com>
2020-05-05 13:01:32 -0700

    Fixed some build failures by regenerating sidecar/manifest

--- src/core/binding.c
+++ src/core/binding.c
@@ -152,6 +152,7 @@ QuicBindingInitialize(
         CLOG_BYTEARRAY(LOG_ADDR_LEN(DatapathLocalAddr), (uint8_t*)&DatapathLocalAddr),
         CLOG_BYTEARRAY(LOG_ADDR_LEN(DatapathRemoteAddr), (uint8_t*)&DatapathRemoteAddr));
 
+    *NewBinding = Binding;
     Status = QUIC_STATUS_SUCCESS;
 
 Error:
