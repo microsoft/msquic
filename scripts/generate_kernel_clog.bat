99ee25b740fafdf7fc452213b2652689180244e9
Nick Banks <nibanks@microsoft.com>
2020-05-12 20:33:33 -0700

    Fix kernel build

--- scripts/generate_kernel_clog.bat
+++ scripts/generate_kernel_clog.bat
@@ -18,13 +18,13 @@ set CMAKE_CLOG_CONFIG_FILE=%CMAKE_SOURCE_DIR%\src\manifest\msquic.clog_config
 echo %CMAKE_SOURCE_DIR%
 
 if NOT EXIST %CMAKE_CLOG_BINS_DIRECTORY%\clog.exe (
-    echo Building CLOG 
+    echo Building CLOG
     echo dotnet build %CLOG_SOURCE_DIRECTORY%\clog.sln\clog_coreclr.sln -o %CMAKE_CLOG_BINS_DIRECTORY%
     dotnet build %CLOG_SOURCE_DIRECTORY%\clog.sln\clog_coreclr.sln -o %CMAKE_CLOG_BINS_DIRECTORY%
 )
 
 pushd %CMAKE_PROJECTDIR%
-for %%i in (*.c* *.c operation.h stream.h connection.h) do (
+for %%i in (*.c* *.c operation.h stream.h connection.h TestHelpers.h) do (
     echo CLOG Processing %%i
     if NOT EXIST %CMAKE_CLOG_OUTPUT_DIRECTORY%\%%i.clog.h (
         %CMAKE_CLOG_BINS_DIRECTORY%\clog.exe -p windows_kernel --scopePrefix %SCOPE_PREFIX% -c %CMAKE_CLOG_CONFIG_FILE% -s %CMAKE_CLOG_SIDECAR_DIRECTORY%\clog.sidecar -i %%i -o %CMAKE_CLOG_OUTPUT_DIRECTORY%\%%i.clog.h
