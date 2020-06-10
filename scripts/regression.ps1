279dfd6c3105c94a6d1e0d6210067f92690b41a1
Thad House <thhous@microsoft.com>
2020-06-10 07:40:24 -0700

    Start workking on a simple regression test runner

---
+++ scripts/regression.ps1
@@ -0,0 +1,69 @@
+<#
+
+.SYNOPSIS
+This script runs spinquic locally for a period of time.
+
+.PARAMETER Config
+    Specifies the build configuration to use.
+
+.PARAMETER Arch
+    The CPU architecture to use.
+
+.PARAMETER Tls
+    The TLS library use.
+
+#>
+
+param (
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("Debug", "Release")]
+    [string]$Config = "Debug",
+
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("x86", "x64", "arm", "arm64")]
+    [string]$Arch = "x64",
+
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("schannel", "openssl", "stub", "mitls")]
+    [string]$Tls = ""
+)
+
+Set-StrictMode -Version 'Latest'
+$PSDefaultParameterValues['*:ErrorAction'] = 'Stop'
+
+# Default TLS based on current platform.
+if ("" -eq $Tls) {
+    if ($IsWindows) {
+        $Tls = "schannel"
+    } else {
+        $Tls = "openssl"
+    }
+}
+
+# Root directory of the project.
+$RootDir = Split-Path $PSScriptRoot -Parent
+
+# Path to the run-executable Powershell script.
+$RunExecutable = Join-Path $RootDir ".azure/scripts/run-executable.ps1"
+
+# Path to the spinquic exectuable.
+$PingClient = $null
+if ($IsWindows) {
+    $PingClient = Join-Path $RootDir "\artifacts\windows\$($Arch)_$($Config)_$($Tls)\quicping.exe"
+} else {
+    $PingClient = Join-Path $RootDir "/artifacts/linux/$($Arch)_$($Config)_$($Tls)/quicping"
+}
+
+# Make sure the build is present.
+if (!(Test-Path $PingClient)) {
+    Write-Error "Build does not exist!`n `nRun the following to generate it:`n `n    $(Join-Path $RootDir "scripts" "build.ps1") -Config $Config -Arch $Arch -Tls $Tls`n"
+}
+
+# Build up all the arguments to pass to the Powershell script.
+$Arguments = "-Path $($PingClient) -Arguments '-target:localhost -uni:1 -length:100000000' -ShowOutput"
+
+
+
+
+# Run the script.
+Invoke-Expression ($RunExecutable + " " + $Arguments)
