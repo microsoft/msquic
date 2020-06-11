08957f4fee07243f20487b7f8c81499e6ab7bd68
Thad House <thhous@microsoft.com>
2020-06-11 14:19:36 -0700

    Fix publishing to not include root dir

--- .azure/scripts/publish-performance-results.ps1
+++ .azure/scripts/publish-performance-results.ps1
@@ -3,14 +3,14 @@ $RootDir = Split-Path $PSScriptRoot -Parent
 $RootDir = Split-Path $RootDir -Parent
 
 $GitPath = Join-Path $RootDir "artifacts/PerfDataGit"
-$ResultsPath = Join-Path $RootDir "artifacts/PerfDataResults"
+$ResultsPath = Join-Path $RootDir "artifacts/PerfDataResults/*"
 
 $env:GIT_REDIRECT_STDERR = '2>&1'
 git clone  --single-branch --branch data/performance https://github.com/microsoft/msquic $GitPath
 $currentLoc = Get-Location
 Set-Location -Path $GitPath
 
-Copy-Item -Path $ResultsPath -Destination $GitPath -Recurse
+Copy-Item -Path $ResultsPath -Destination $GitPath -Recurse -Force
 git add .
 git status
 Set-Location -Path $currentLoc
