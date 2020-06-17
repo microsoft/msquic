755b4597a42fcd930979544991b8a702f468ca3f
Thad House <ThadHouse@users.noreply.github.com>
2020-06-17 13:10:31 -0700

    Add debugging for perf publishing (#514)
    * Add debugging for perf publishing

--- .azure/scripts/publish-performance-results.ps1
+++ .azure/scripts/publish-performance-results.ps1
@@ -1,3 +1,6 @@
+Set-StrictMode -Version 'Latest'
+$PSDefaultParameterValues['*:ErrorAction'] = 'Stop'
+
 # Root directory of the project.
 $RootDir = Split-Path $PSScriptRoot -Parent
 $RootDir = Split-Path $RootDir -Parent
@@ -7,10 +10,14 @@ $ResultsPath = Join-Path $RootDir "artifacts/PerfDataResults"
 # Enumerate files
 $Files = Get-ChildItem -Path $ResultsPath -Recurse -File
 
+Write-Host $Files
+
 $Files | ForEach-Object {
     $DataToWrite = Get-Content $_ | ConvertFrom-Json
+    Write-Host $DataToWrite
     $DataToWrite | Add-Member -NotePropertyName "AuthKey" -NotePropertyValue $env:MAPPED_DEPLOYMENT_KEY
     $JsonToWrite = $DataToWrite | ConvertTo-Json
 
-    Invoke-RestMethod -Uri "https://msquicperformanceresults.azurewebsites.net/performance" -Body $JsonToWrite -Method 'Post' -ContentType "application/json"
+    $Result = Invoke-RestMethod -Uri "https://msquicperformanceresults.azurewebsites.net/performance" -Body $JsonToWrite -Method 'Post' -ContentType "application/json"
+    Write-Host $Result
 }
