cd999ea54aab5bfd0cb7d92130a1c1f5563d6b69
Thad House <thadhouse1@gmail.com>
2020-06-17 12:35:11 -0700

    Add debugging for perf publishing

--- .azure/scripts/publish-performance-results.ps1
+++ .azure/scripts/publish-performance-results.ps1
@@ -7,8 +7,11 @@ $ResultsPath = Join-Path $RootDir "artifacts/PerfDataResults"
 # Enumerate files
 $Files = Get-ChildItem -Path $ResultsPath -Recurse -File
 
+Write-Host $Files
+
 $Files | ForEach-Object {
     $DataToWrite = Get-Content $_ | ConvertFrom-Json
+    Write-Host $DataToWrite
     $DataToWrite | Add-Member -NotePropertyName "AuthKey" -NotePropertyValue $env:MAPPED_DEPLOYMENT_KEY
     $JsonToWrite = $DataToWrite | ConvertTo-Json
 
