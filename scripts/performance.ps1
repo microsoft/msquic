ccf0fb0ff3625af1eb360fdebd15829241715492
Nick Banks <nibanks@microsoft.com>
2020-06-19 14:41:26 -0700

    Write Server too

--- scripts/performance.ps1
+++ scripts/performance.ps1
@@ -178,19 +178,24 @@ function Run-Loopback-Test() {
 
     $allRunsResults = @()
 
-    1..$Runs | ForEach-Object {
-        $clientOutput = Run-Foreground-Executable -File (Join-Path $Artifacts $QuicPing) -Arguments "-target:localhost -port:4433 -uni:1 -length:$Length"
-        $parsedRunResult = Parse-Loopback-Results -Results $clientOutput
-        $allRunsResults += $parsedRunResult
-        if ($PGO) {
-            # Merge client PGO counts.
-            Merge-PGO-Counts $Artifacts
+    try {
+        1..$Runs | ForEach-Object {
+            $clientOutput = Run-Foreground-Executable -File (Join-Path $Artifacts $QuicPing) -Arguments "-target:localhost -port:4433 -uni:1 -length:$Length"
+            $parsedRunResult = Parse-Loopback-Results -Results $clientOutput
+            $allRunsResults += $parsedRunResult
+            if ($PGO) {
+                # Merge client PGO counts.
+                Merge-PGO-Counts $Artifacts
+            }
+            Write-Host "Run $($_): $parsedRunResult kbps"
+            $clientOutput | Write-Debug
         }
-        Write-Host "Run $($_): $parsedRunResult kbps"
-        $clientOutput | Write-Debug
-    }
 
-    $serverOutput = Stop-Background-Executable -Process $proc
+        $serverOutput = Stop-Background-Executable -Process $proc
+    } catch {
+        Stop-Background-Executable -Process $proc | Write-Host
+        throw
+    }
     if ($PGO) {
         # Merge server PGO counts.
         Merge-PGO-Counts $ServerDir
