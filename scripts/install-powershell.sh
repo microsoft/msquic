941ad51dd3725ed9d202c78adbb5f7c4fe5b68b7
Nick Banks <nibanks@microsoft.com>
2020-06-30 18:12:38 -0700

    More fixes

--- scripts/install-powershell.sh
+++ scripts/install-powershell.sh
@@ -5,3 +5,13 @@ sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update
 sudo add-apt-repository universe
 sudo apt-get install -y powershell
+
+wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
+chmod +x ./dotnet-install.sh
+./dotnet-install.sh -c Current
+
+export PATH="$PATH:$HOME/.dotnet"
+export PATH="$PATH:$HOME/.dotnet/tools"
+export DOTNET_ROOT="$HOME/.dotnet/"
+
+dotnet tool install --global PowerShell
