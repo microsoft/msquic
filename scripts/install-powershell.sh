2cd44abb07a3c231c90af47ed49dc80eec5cfeee
Thad House <thhous@microsoft.com>
2020-07-17 16:17:42 -0700

    Fix install powershell to install dotnet sdk through apt

--- scripts/install-powershell.sh
+++ scripts/install-powershell.sh
@@ -1,19 +1,9 @@
 #!/bin/sh
 
-wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
-chmod +x ./dotnet-install.sh
-./dotnet-install.sh -c Current
-
-echo "##vso[task.setvariable variable=PATH]${PATH}:${HOME}/.dotnet"
-echo "##vso[task.setvariable variable=PATH]${PATH}:${HOME}/.dotnet/tools"
-echo "##vso[task.setvariable variable=DOTNET_ROOT]${HOME}/.dotnet"
-
-export PATH="$PATH:$HOME/.dotnet"
-export PATH="$PATH:$HOME/.dotnet/tools"
-export DOTNET_ROOT="$HOME/.dotnet/"
-
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update
 sudo add-apt-repository universe
-sudo apt-get install -y powershell
+sudo apt-get install -y powershell apt-transport-https
+sudo apt-get update
+sudo apt-get install -y dotnet-sdk-3.1
