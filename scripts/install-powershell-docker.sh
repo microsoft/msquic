4dc236fe5d5d6121e6287210a8e4fa167edf8875
Thad House <thhous@microsoft.com>
2020-07-17 16:31:45 -0700

    Install to /usr/share

--- scripts/install-powershell-docker.sh
+++ scripts/install-powershell-docker.sh
@@ -2,10 +2,6 @@
 
 wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
 chmod +x ./dotnet-install.sh
-./dotnet-install.sh -c Current --install-dir /usr/share/dotnet
-
-export PATH="$PATH:$HOME/.dotnet"
-export PATH="$PATH:$HOME/.dotnet/tools"
-export DOTNET_ROOT="$HOME/.dotnet/"
+./dotnet-install.sh -c Current --install-dir /usr/share
 
 dotnet tool install -g powershell
