fd45df5b5d5d0d86bbfd26f6b56fd7869d73580b
Thad House <thhous@microsoft.com>
2020-07-17 16:38:13 -0700

    Install dotnet through package manager

--- scripts/install-powershell-docker.sh
+++ scripts/install-powershell-docker.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
-chmod +x ./dotnet-install.sh
-./dotnet-install.sh -c Current --install-dir /usr/share
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+
+apt-get update
+apt-get install -y apt-transport-https
+apt-get update
+apt-get install -y dotnet-sdk-3.1
 
 dotnet tool install -g powershell
