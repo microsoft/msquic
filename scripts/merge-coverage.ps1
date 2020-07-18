a04b6378be1bdd549b8a06a5b0c9ce81d79c91e6
Nick Banks <nibanks@microsoft.com>
2020-07-18 11:59:35 -0700

    Try to fix uploading and merging of coverage files

--- scripts/merge-coverage.ps1
+++ scripts/merge-coverage.ps1
@@ -3,12 +3,47 @@
 .SYNOPSIS
 This script merges the coverage data.
 
+.PARAMETER Config
+    Specifies the build configuration to test.
+
+.PARAMETER Arch
+    The CPU architecture to test.
+
+.PARAMETER Tls
+    The TLS library test.
+
 #>
+
+param (
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("Debug", "Release")]
+    [string]$Config = "Debug",
+
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("x86", "x64", "arm", "arm64")]
+    [string]$Arch = "x64",
+
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("schannel", "openssl", "stub", "mitls")]
+    [string]$Tls = ""
+)
+
+Set-StrictMode -Version 'Latest'
+$PSDefaultParameterValues['*:ErrorAction'] = 'Stop'
+
+# Default TLS based on current platform.
+if ("" -eq $Tls) {
+    if ($IsWindows) {
+        $Tls = "schannel"
+    } else {
+        $Tls = "openssl"
+    }
+}
 
 # Root directory of the project.
 $RootDir = Split-Path $PSScriptRoot -Parent
 
-$CoverageDir = Join-Path $RootDir "artifacts" "coverage"
+$CoverageDir = Join-Path $RootDir "artifacts\coverage\windows\$($Arch)_$($Config)_$($Tls)"
 
 $CoverageMergeParams = ""
 
