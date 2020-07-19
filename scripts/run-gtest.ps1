be9a4b11963055f8ac2baad177255c9c8ecccbfd
Nick Banks <nibanks@microsoft.com>
2020-07-18 18:16:58 -0700

    Another try

--- scripts/run-gtest.ps1
+++ scripts/run-gtest.ps1
@@ -518,10 +518,9 @@ if ($Kernel -ne "") {
     net.exe stop msquic /y | Out-Null
     Copy-Item C:\Windows\system32\drivers\msquic.sys C:\Windows\system32\drivers\msquic.sys.old
     Copy-Item (Join-Path $Kernel "msquictest.sys") (Split-Path $Path -Parent)
-    verifier.exe /volatile /flags 0x9BB
-    verifier.exe /volatile /adddriver msquic.sys msquictest.sys
+    verifier.exe /volatile /adddriver msquic.sys msquictest.sys /flags 0x9BB
     sfpcopy.exe (Join-Path $Kernel "msquic.sys") C:\Windows\system32\drivers\msquic.sys
-    net.exe start mcquic
+    net.exe start msquic
 }
 
 try {
