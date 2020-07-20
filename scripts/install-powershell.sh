8a79d5b44ea16917fc797a47ed3d42a23831f45c
Thad House <thhous@microsoft.com>
2020-07-20 08:40:31 -0700

    Switch to installing sdk through azure install task

--- scripts/install-powershell.sh
+++ scripts/install-powershell.sh
@@ -5,10 +5,3 @@ sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update
 sudo add-apt-repository universe
 sudo apt-get install -y powershell apt-transport-https
-sudo apt-get update
-sudo apt-get install -y dotnet-sdk-3.1
-sudo ldconfig
-
-echo "##vso[task.setvariable variable=PATH]${PATH}:${Home}/.dotnet/tools"
-
-echo "##vso[task.setvariable variable=DOTNET_ROOT]$(dirname $(realpath $(which dotnet)))"
