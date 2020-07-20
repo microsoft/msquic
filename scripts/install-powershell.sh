141a31958e6969d0baf46c8acbbdace75cc7fb27
Thad House <thhous@microsoft.com>
2020-07-20 10:04:07 -0700

    keep using azp to install dotnet

--- scripts/install-powershell.sh
+++ scripts/install-powershell.sh
@@ -5,7 +5,3 @@ sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update
 sudo add-apt-repository universe
 sudo apt-get install -y powershell apt-transport-https
-sudo apt-get update
-sudo apt-get install -y dotnet-sdk-3.1
-
-echo "##vso[task.setvariable variable=PATH]${PATH}:${Home}/.dotnet/tools"
