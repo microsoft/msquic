5bf71fb42ddac1a765b01239c1a63ac7c25d4c37
Thad House <ThadHouse@users.noreply.github.com>
2020-07-30 07:33:11 -0700

    Fix cmake installs since we added clog. (#636)

--- scripts/package-distribution.ps1
+++ scripts/package-distribution.ps1
@@ -81,9 +81,10 @@ foreach ($Build in $AllBuilds) {
         $Binaries += Join-Path $ArtifactsDir "msquic.pdb"
     } else {
         $Binaries += Join-Path $ArtifactsDir "libmsquic.so"
-        #Temporary until we fix CLOG
-        $Binaries += Join-Path $ArtifactsDir "libplatform.clog.so"
-        $Binaries += Join-Path $ArtifactsDir "libcore.clog.so"
+        $LttngBin = Join-Path $ArtifactsDir "libmsquic.lttng.so"
+        if (Test-Path $LttngBin) {
+            $Binaries += $LttngBin
+        }
     }
 
     $Libraries = @()
---
+++ src/bin/linux/traceprovider.c
@@ -0,0 +1,14 @@
+/*++
+
+    Copyright (c) Microsoft Corporation.
+    Licensed under the MIT License.
+
+Abstract:
+
+    Lttng trace provider
+
+--*/
+
+#define TRACEPOINT_CREATE_PROBES
+#include "quic_platform.h"
+#include "quic_trace.h"
