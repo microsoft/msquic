df6414947371551d21eca9c22ac7719ee596b727
Nick Banks <nibanks@microsoft.com>
2020-07-31 10:46:23 -0700

    Fix linux inline

--- src/core/inline.c
+++ src/core/inline.c
@@ -158,6 +158,20 @@ QuicPartitionIdGetIndex(
     );
 
 _IRQL_requires_max_(DISPATCH_LEVEL)
+uint8_t
+QuicPartitionIndexIncrement(
+    uint8_t PartitionIndex,
+    uint8_t Increment
+    );
+
+_IRQL_requires_max_(DISPATCH_LEVEL)
+uint8_t
+QuicPartitionIndexDecrement(
+    uint8_t PartitionIndex,
+    uint8_t Decrement
+    )
+
+_IRQL_requires_max_(DISPATCH_LEVEL)
 void
 QuicStreamAddRef(
     _In_ QUIC_STREAM* Stream,
