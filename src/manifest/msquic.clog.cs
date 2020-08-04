7a78d6e184c14f17af51653d5a05d9cdccd6f2cf
Thad House <thhous@microsoft.com>
2020-08-04 11:11:11 -0700

    Clean up c# clog code

--- src/manifest/msquic.clog.cs
+++ src/manifest/msquic.clog.cs
@@ -6,6 +6,7 @@
 --*/
 
 using System;
+using System.Text;
 using System.Runtime.InteropServices;
 
 namespace msquic.clog_config
@@ -40,10 +41,17 @@ namespace msquic.clog_config
         public static string ADDR(byte[] value)
         {
             int len = value.Length;
-            IntPtr i = System.Runtime.InteropServices.Marshal.AllocHGlobal(len);
-            System.Runtime.InteropServices.Marshal.Copy(value, 0, i, len);
+            IntPtr i = Marshal.AllocHGlobal(len);
             string msg = "";
-            SocketAddress sa2 = (SocketAddress)System.Runtime.InteropServices.Marshal.PtrToStructure(i, typeof(SocketAddress));
+            try 
+            {
+                Marshal.Copy(value, 0, i, len);
+                SocketAddress sa2 = (SocketAddress)Marshal.PtrToStructure(i, typeof(SocketAddress));
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(i);
+            }
 
             switch (sa2.si_family)
             {
@@ -51,23 +59,27 @@ namespace msquic.clog_config
                     msg += "Unspecified";
                     break;
                 case 2:  //< --v4
-                    msg += "IPV4:" + sa2.S_addr + ":" + sa2.sin_port;
+                    msg += $"IPV4:{sa2.S_addr}:{sa2.sin_port}";
                     break;
                 case 10:  //<--v6 (linux)
                 case 23: //<--v6
-                    msg += "IPV6: sin6_flowinfo=" + sa2.sin6_flowinfo + " port=" + sa2.sin_port + "part1=" + sa2.S_v6Addr1 + ", part2=" + sa2.S_v6Addr2;
+                    msg += $"IPV6: sin6_flowinfo={sa2.sin6_flowinfo} port={sa2.sin_port} part1={sa2.S_v6Addr1} part2={sa2.S_v6Addr2}";
                     break;
                 default:
-                    throw new Exception("Invalid SI_FAMILY : " + sa2.si_family);
+                    throw new NotSupportedException("Invalid SI_FAMILY : " + sa2.si_family);
             }
-
-            System.Runtime.InteropServices.Marshal.FreeHGlobal(i);
+            
             return msg;
         }
 
         public static string CID(byte [] value)
         {
-            return "CID!";
+            StringBuilder hex = new StringBuilder(value.Length * 2);
+            foreach (byte v in value)
+            {
+                hex.AppendFormat(v.ToString("X2"));
+            }
+            return hex.ToString();
         }
     }
 }
