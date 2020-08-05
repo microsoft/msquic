26cac400e69addfe095dc90a6686e90fe7ae59a3
Thad House <thhous@microsoft.com>
2020-08-05 13:31:30 -0700

    Make the changes not breaking

--- src/perf/dbserver/Data/PerformanceContext.cs
+++ src/perf/dbserver/Data/PerformanceContext.cs
@@ -15,8 +15,13 @@ namespace QuicDataServer.Data
         }
 
         public DbSet<DbPlatform> Platforms { get; set; } = null!;
+
         public DbSet<DbTest> Tests { get; set; } = null!;
         public DbSet<DbTestRecord> TestRecords { get; set; } = null!;
+
+        public DbSet<DbThroughputTestRecord> ThroughputTestRecords { get; set; } = null!;
+
+
         public DbSet<DbMachine> Machines { get; set; } = null!;
 
     }
