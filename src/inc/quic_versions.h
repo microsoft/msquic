dbaa2ff8e2b7cc7d974a22cfca5d3f4eb1167a75
Lars Eggert <lars@eggert.org>
2020-08-13 11:19:50 +0300

    Make versions uint32_t constants to avoid signed conversion warnings

--- src/inc/quic_versions.h
+++ src/inc/quic_versions.h
@@ -14,28 +14,28 @@ Abstract:
 //
 // The QUIC version numbers, in network byte order.
 //
-#define QUIC_VERSION_VER_NEG    0x00000000      // Version for 'Version Negotiation'
-#define QUIC_VERSION_1          0x01000000      // First official version
-#define QUIC_VERSION_MS_1       0x0000cdab      // First Microsoft version (currently same as latest draft)
-#define QUIC_VERSION_DRAFT_27   0x1b0000ff      // IETF draft 27
-#define QUIC_VERSION_DRAFT_28   0x1c0000ff      // IETF draft 28
-#define QUIC_VERSION_DRAFT_29   0x1d0000ff      // IETF draft 29
+#define QUIC_VERSION_VER_NEG    UINT32_C(0x00000000)      // Version for 'Version Negotiation'
+#define QUIC_VERSION_1          UINT32_C(0x01000000)      // First official version
+#define QUIC_VERSION_MS_1       UINT32_C(0x0000cdab)      // First Microsoft version (currently same as latest draft)
+#define QUIC_VERSION_DRAFT_27   UINT32_C(0x1b0000ff)      // IETF draft 27
+#define QUIC_VERSION_DRAFT_28   UINT32_C(0x1c0000ff)      // IETF draft 28
+#define QUIC_VERSION_DRAFT_29   UINT32_C(0x1d0000ff)      // IETF draft 29
 
 //
 // The QUIC version numbers, in host byte order.
 //
-#define QUIC_VERSION_VER_NEG_H  0x00000000      // Version for 'Version Negotiation'
-#define QUIC_VERSION_1_H        0x00000001      // First official version
-#define QUIC_VERSION_1_MS_H     0xabcd0000      // First Microsoft version (-1412628480 in decimal)
-#define QUIC_VERSION_DRAFT_27_H 0xff00001b      // IETF draft 27
-#define QUIC_VERSION_DRAFT_28_H 0xff00001c      // IETF draft 28
-#define QUIC_VERSION_DRAFT_29_H 0xff00001d      // IETF draft 29
+#define QUIC_VERSION_VER_NEG_H  UINT32_C(0x00000000)      // Version for 'Version Negotiation'
+#define QUIC_VERSION_1_H        UINT32_C(0x00000001)      // First official version
+#define QUIC_VERSION_1_MS_H     UINT32_C(0xabcd0000)      // First Microsoft version (-1412628480 in decimal)
+#define QUIC_VERSION_DRAFT_27_H UINT32_C(0xff00001b)      // IETF draft 27
+#define QUIC_VERSION_DRAFT_28_H UINT32_C(0xff00001c)      // IETF draft 28
+#define QUIC_VERSION_DRAFT_29_H UINT32_C(0xff00001d)      // IETF draft 29
 
 //
 // Represents a reserved version value; used to force version negotation.
 //
-#define QUIC_VERSION_RESERVED       0x0a0a0a0a
-#define QUIC_VERSION_RESERVED_MASK  0x0f0f0f0f
+#define QUIC_VERSION_RESERVED       UINT32_C(0x0a0a0a0a)
+#define QUIC_VERSION_RESERVED_MASK  UINT32_C(0x0f0f0f0f)
 
 //
 // The latest QUIC version number.
