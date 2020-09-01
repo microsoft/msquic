5211227c6e36c772e0796abab587e54216ad02e0
Thad House <thhous@microsoft.com>
2020-09-01 15:46:54 -0700

    Fix a bit of formatting

--- src/core/quicdef.h
+++ src/core/quicdef.h
@@ -414,7 +414,7 @@ QUIC_STATIC_ASSERT(
 // lifetime of a connection. Set to the lowest limit, which is for
 // AEAD_AES_128_CCM at 2^23.5 (rounded down)
 //
-#define QUIC_AEAD_INTEGRITY_LIMIT 0xB503F3
+#define QUIC_AEAD_INTEGRITY_LIMIT               11863283
 
 /*************************************************************
                   PERSISTENT SETTINGS
