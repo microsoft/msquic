62fab7ea97274fbc988d26ddc1b5b1aa63f0bd97
Nick Banks <nibanks@microsoft.com>
2020-09-02 11:33:12 -0700

    Fix NULL pointer read in listener

--- src/core/listener.c
+++ src/core/listener.c
@@ -559,7 +559,11 @@ QuicListenerParamGet(
         Stats->TotalAcceptedConnections = Listener->TotalAcceptedConnections;
         Stats->TotalRejectedConnections = Listener->TotalRejectedConnections;
 
-        Stats->Binding.Recv.DroppedPackets = Listener->Binding->Stats.Recv.DroppedPackets;
+        if (Listener->Binding != NULL) {
+            Stats->Binding.Recv.DroppedPackets = Listener->Binding->Stats.Recv.DroppedPackets;
+        } else {
+            Stats->Binding.Recv.DroppedPackets = 0;
+        }
 
         Status = QUIC_STATUS_SUCCESS;
         break;
