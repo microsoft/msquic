ff6c1aed3e6f8c05c232a2073be4751a224c1298
Nick Banks <nibanks@microsoft.com>
2020-11-19 11:50:23 -0800

    Tabs to spaces

--- scripts/emulated-performance.ps1
+++ scripts/emulated-performance.ps1
@@ -16,11 +16,11 @@ be in the current directory.
 
 .PARAMETER RandomLossDenominator
     For N > 0, indicates a random drop chance of 1 / N packets in the emulated network.
-	For N <= 0, indicates no random loss/drops in the emulated network.
+    For N <= 0, indicates no random loss/drops in the emulated network.
 
 .PARAMETER RandomReorderDenominator
     For N > 0, indicates a random delay chance of 1 / N packets in the emulated network.
-	For N <= 0, indicates no random reordering in the emulated network.
+    For N <= 0, indicates no random reordering in the emulated network.
 
 .PARAMETER ReorderDelayDeltaMs
     The extra delay applied to any reordered packets in the emulated network.
@@ -82,7 +82,7 @@ Sleep -Seconds 1
 # CSV header
 $Header = "RttMs, BottleneckMbps, BottleneckBufferPackets, RandomLossDenominator, RandomReorderDenominator, ReorderDelayDeltaMs, DurationMs, Pacing, RateKbps"
 for ($i = 0; $i -lt $NumIterations; $i++) {
-	$Header += ", RawRateKbps$($i+1)"
+    $Header += ", RawRateKbps$($i+1)"
 }
 Write-Host $Header
 
@@ -94,48 +94,48 @@ foreach ($ThisRandomLossDenominator in $RandomLossDenominator) {
 foreach ($ThisRandomReorderDenominator in $RandomReorderDenominator) {
 foreach ($ThisReorderDelayDeltaMs in $ReorderDelayDeltaMs) {
 
-	# Configure duonic for the desired network emulation options.
-	Write-Debug "Configure NIC: Rtt=$ThisRttMs ms, Bottneck=[$ThisBottleneckMbps mbps, $ThisBottleneckBufferPackets packets], RandomLoss=1/$ThisRandomLossDenominator, ReorderDelayDelta=$ThisReorderDelayDeltaMs ms, RandomReorder=1/$ThisRandomReorderDenominator"
-	$Output =
-		.\duonic.ps1 -Rdq `
-			-RttMs ([int]($ThisRttMs)) `
-			-BottleneckMbps $ThisBottleneckMbps `
-			-BottleneckBufferPackets $ThisBottleneckBufferPackets `
-			-RandomLossDenominator $ThisRandomLossDenominator `
-			-ReorderDelayDeltaMs $ThisReorderDelayDeltaMs `
-			-RandomReorderDenominator $ThisRandomReorderDenominator
-	if (!$Output.Contains("Done.")) {
-		Write-Error $Output
-	}
+    # Configure duonic for the desired network emulation options.
+    Write-Debug "Configure NIC: Rtt=$ThisRttMs ms, Bottneck=[$ThisBottleneckMbps mbps, $ThisBottleneckBufferPackets packets], RandomLoss=1/$ThisRandomLossDenominator, ReorderDelayDelta=$ThisReorderDelayDeltaMs ms, RandomReorder=1/$ThisRandomReorderDenominator"
+    $Output =
+        .\duonic.ps1 -Rdq `
+            -RttMs ([int]($ThisRttMs)) `
+            -BottleneckMbps $ThisBottleneckMbps `
+            -BottleneckBufferPackets $ThisBottleneckBufferPackets `
+            -RandomLossDenominator $ThisRandomLossDenominator `
+            -ReorderDelayDeltaMs $ThisReorderDelayDeltaMs `
+            -RandomReorderDenominator $ThisRandomReorderDenominator
+    if (!$Output.Contains("Done.")) {
+        Write-Error $Output
+    }
 
-	# Loop over all the test configurations.
-	foreach ($ThisDurationMs in $DurationMs) {
-	foreach ($ThisPacing in $Pacing) {
+    # Loop over all the test configurations.
+    foreach ($ThisDurationMs in $DurationMs) {
+    foreach ($ThisPacing in $Pacing) {
 
-		# Run through all the iterations and keep track of the results.
-		$Results = [System.Collections.ArrayList]@()
-		Write-Debug "Run upload test: Duration=$ThisDurationMs ms, Pacing=$ThisPacing"
-		for ($i = 0; $i -lt $NumIterations; $i++) {
+        # Run through all the iterations and keep track of the results.
+        $Results = [System.Collections.ArrayList]@()
+        Write-Debug "Run upload test: Duration=$ThisDurationMs ms, Pacing=$ThisPacing"
+        for ($i = 0; $i -lt $NumIterations; $i++) {
 
-			# Run the throughput upload test with the current configuration.
-			$Output = .\quicperf.exe -test:tput -bind:192.168.1.12 -target:192.168.1.11 -sendbuf:0 -upload:$ThisDurationMs -timed:1 -pacing:$ThisPacing
-			if (!$Output.Contains("App Main returning status 0")) {
-				Write-Error $Output
-			}
+            # Run the throughput upload test with the current configuration.
+            $Output = .\quicperf.exe -test:tput -bind:192.168.1.12 -target:192.168.1.11 -sendbuf:0 -upload:$ThisDurationMs -timed:1 -pacing:$ThisPacing
+            if (!$Output.Contains("App Main returning status 0")) {
+                Write-Error $Output
+            }
 
-			# Grab the result output text.
-			$Result = $Output.Split([Environment]::NewLine)[-2]
-			Write-Debug $Result
-			$Results.Add([int]$Result.Split(" ")[4]) | Out-Null
-		}
+            # Grab the result output text.
+            $Result = $Output.Split([Environment]::NewLine)[-2]
+            Write-Debug $Result
+            $Results.Add([int]$Result.Split(" ")[4]) | Out-Null
+        }
 
-		# Grab the average result and write the CSV output.
-		$RateKbps = [int]($Results | Measure-Object -Average).Average
-		$Row = "$ThisRttMs, $ThisBottleneckMbps, $ThisBottleneckBufferPackets, $ThisRandomLossDenominator, $ThisRandomReorderDenominator, $ThisReorderDelayDeltaMs, $ThisDurationMs, $ThisPacing, $RateKbps"
-		for ($i = 0; $i -lt $NumIterations; $i++) {
-			$Row += ", $($Results[$i])"
-		}
-		Write-Host $Row
-	}}
+        # Grab the average result and write the CSV output.
+        $RateKbps = [int]($Results | Measure-Object -Average).Average
+        $Row = "$ThisRttMs, $ThisBottleneckMbps, $ThisBottleneckBufferPackets, $ThisRandomLossDenominator, $ThisRandomReorderDenominator, $ThisReorderDelayDeltaMs, $ThisDurationMs, $ThisPacing, $RateKbps"
+        for ($i = 0; $i -lt $NumIterations; $i++) {
+            $Row += ", $($Results[$i])"
+        }
+        Write-Host $Row
+    }}
 
 }}}}}}
