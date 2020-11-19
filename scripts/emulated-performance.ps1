6e89bdd2be796c093e2f5c97200799c3d4dbbaaf
Nick Banks <nibanks@microsoft.com>
2020-11-19 10:25:51 -0800

    Add Emulated Performance Script

---
+++ scripts/emulated-performance.ps1
@@ -0,0 +1,133 @@
+<#
+
+.SYNOPSIS
+This script runs performance tests with various emulated network conditions. Note,
+this script requires duonic to be preinstalled on the system and quicperf.exe to
+be in the current directory.
+
+.PARAMETER RttMs
+    The round trip time(s) for the emulated network.
+
+.PARAMETER BottleneckMbps
+    The maximum rate(s) for the emulated network.
+
+.PARAMETER BottleneckBufferPackets
+    The maximum buffer size(s), in packets, for the emulated network.
+
+.PARAMETER RandomLossDenominator
+    For N > 0, indicates a random drop chance of 1 / N packets in the emulated network.
+	For N <= 0, indicates no random loss/drops in the emulated network.
+
+.PARAMETER RandomReorderDenominator
+    For N > 0, indicates a random delay chance of 1 / N packets in the emulated network.
+	For N <= 0, indicates no random reordering in the emulated network.
+
+.PARAMETER ReorderDelayDeltaMs
+    The extra delay applied to any reordered packets in the emulated network.
+
+.PARAMETER DurationMs
+    The duration(s) of each test run over the emulated network.
+
+.PARAMETER Pacing
+    The pacing enabled/disable flag(s) used for each test run over the emulated network.
+
+.PARAMETER NumIterations
+    The number(s) of iterations to run of each test over the emulated network.
+
+#>
+
+param (
+
+    [Parameter(Mandatory = $false)]
+    [Int32[]]$RttMs = 60,
+
+    [Parameter(Mandatory = $false)]
+    [Int32[]]$BottleneckMbps = 20,
+
+    [Parameter(Mandatory = $false)]
+    [Int32[]]$BottleneckBufferPackets = 1000,
+
+    [Parameter(Mandatory = $false)]
+    [Int32[]]$RandomLossDenominator = 0,
+
+    [Parameter(Mandatory = $false)]
+    [Int32[]]$RandomReorderDenominator = 0,
+
+    [Parameter(Mandatory = $false)]
+    [Int32[]]$ReorderDelayDeltaMs = 0,
+
+    [Parameter(Mandatory = $false)]
+    [Int32[]]$DurationMs = 10000,
+
+    [Parameter(Mandatory = $false)]
+    [Int32[]]$Pacing = (0, 1),
+
+    [Parameter(Mandatory = $false)]
+    [Int32]$NumIterations = 1
+)
+
+Set-StrictMode -Version 'Latest'
+$PSDefaultParameterValues['*:ErrorAction'] = 'Stop'
+
+# Start the perf server listening.
+$pinfo = New-Object System.Diagnostics.ProcessStartInfo
+$pinfo.FileName = Join-Path $PSScriptRoot "quicperf.exe"
+$pinfo.Arguments = "-selfsign:1"
+$pinfo.UseShellExecute = $false
+$p = New-Object System.Diagnostics.Process
+$p.StartInfo = $pinfo
+$p.Start() | Out-Null
+Sleep -Seconds 1
+
+# CSV header
+Write-Host "RttMs, BottleneckMbps, BottleneckBufferPackets, RandomLossDenominator, RandomReorderDenominator, ReorderDelayDeltaMs, DurationMs, Pacing, RateKbps"
+
+# Loop over all the network emulation configurations.
+foreach ($ThisRttMs in $RttMs) {
+foreach ($ThisBottleneckMbps in $BottleneckMbps) {
+foreach ($ThisBottleneckBufferPackets in $BottleneckBufferPackets) {
+foreach ($ThisRandomLossDenominator in $RandomLossDenominator) {
+foreach ($ThisRandomReorderDenominator in $RandomReorderDenominator) {
+foreach ($ThisReorderDelayDeltaMs in $ReorderDelayDeltaMs) {
+
+	# Configure duonic for the desired network emulation options.
+	Write-Debug "Configure NIC: Rtt=$ThisRttMs ms, Bottneck=[$ThisBottleneckMbps mbps, $ThisBottleneckBufferPackets packets], RandomLoss=1/$ThisRandomLossDenominator, ReorderDelayDelta=$ThisReorderDelayDeltaMs ms, RandomReorder=1/$ThisRandomReorderDenominator"
+	$Output =
+		.\duonic.ps1 -Rdq `
+			-RttMs ([int]($ThisRttMs)) `
+			-BottleneckMbps $ThisBottleneckMbps `
+			-BottleneckBufferPackets $ThisBottleneckBufferPackets `
+			-RandomLossDenominator $ThisRandomLossDenominator `
+			-ReorderDelayDeltaMs $ThisReorderDelayDeltaMs `
+			-RandomReorderDenominator $ThisRandomReorderDenominator
+	if (!$Output.Contains("Done.")) {
+		Write-Error $Output
+	}
+
+	# Loop over all the test configurations.
+	foreach ($ThisDurationMs in $DurationMs) {
+	foreach ($ThisPacing in $Pacing) {
+
+		# Run through all the iterations and keep track of the results.
+		$Results = [System.Collections.ArrayList]@()
+		Write-Debug "Run upload test: Duration=$ThisDurationMs ms, Pacing=$ThisPacing"
+		for ($i = 0; $i -lt $NumIterations; $i++) {
+
+			# Run the throughput upload test with the current configuration.
+			$Output = .\quicperf.exe -test:tput -bind:192.168.1.12 -target:192.168.1.11 -sendbuf:0 -upload:$ThisDurationMs -timed:1 -pacing:$ThisPacing
+			if (!$Output.Contains("App Main returning status 0")) {
+				Write-Error $Output
+			}
+
+			# Grab the result output text.
+			$Result = $Output.Split([Environment]::NewLine)[-2]
+			Write-Debug $Result
+			$Results.Add([int]$Result.Split(" ")[4]) | Out-Null
+		}
+
+		# Grab the average result and write the CSV output.
+		$RateKbps = [int]($Results | Measure-Object -Average).Average
+		Write-Host "$ThisRttMs, $ThisBottleneckMbps, $ThisBottleneckBufferPackets, $ThisRandomLossDenominator, $ThisRandomReorderDenominator, $ThisReorderDelayDeltaMs, $ThisDurationMs, $ThisPacing, $RateKbps"
+	}}
+
+}}}}}}
