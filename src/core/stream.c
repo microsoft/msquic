46fb0b7f08e70768dd85af4b76a08b1333845378
Nick Banks <nibanks@microsoft.com>
2020-11-30 18:00:34 -0800

    Add a log

--- src/core/stream.c
+++ src/core/stream.c
@@ -88,6 +88,11 @@ QuicStreamInitialize(
 #if 1 // Special case code to force bugcheck or failure. Will be removed when no longer needed.
     QUIC_FRE_ASSERT(Connection->Settings.StreamRecvBufferDefault != 0x80000000u);
     if (Connection->Settings.StreamRecvBufferDefault == 0x40000000u) {
+        QuicTraceEvent(
+            StreamError,
+            "[strm][%p] ERROR, %s.",
+            Stream,
+            "Unsupported receive buffer size");
         Status = QUIC_STATUS_NOT_SUPPORTED;
         goto Exit;
     }
