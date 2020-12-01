a2771c3566a26ec71d172c8a8b185cbd3d3de0ef
Nick Banks <nibanks@microsoft.com>
2020-12-01 12:46:04 -0800

    WPA: Datapath Batching Improvements

--- src/plugins/wpa/DataModel/QuicDatapath.cs
+++ src/plugins/wpa/DataModel/QuicDatapath.cs
@@ -38,58 +38,6 @@ namespace MsQuicTracing.DataModel
 
         private readonly List<QuicEvent> Events = new List<QuicEvent>();
 
-        public IReadOnlyList<QuicDatapathData> GetDatapathEvents(long resolutionNanoSec = 25 * 1000 * 1000) // 25 ms default
-        {
-            var Resolution = new TimestampDelta(resolutionNanoSec);
-
-            int eventCount = Events.Count;
-            int eventIndex = 0;
-
-            var sample = new QuicDatapathData();
-            var datapathEvents = new List<QuicDatapathData>();
-            foreach (var evt in Events)
-            {
-                if (eventIndex == 0)
-                {
-                    sample.TimeStamp = evt.TimeStamp;
-                }
-                eventIndex++;
-
-                if (evt.EventId == QuicEventId.DatapathSend)
-                {
-                    var _evt = evt as QuicDatapathSendEvent;
-                    sample.BytesSent += _evt!.TotalSize;
-                    sample.SendEventCount++;
-                }
-                else if (evt.EventId == QuicEventId.DatapathRecv)
-                {
-                    var _evt = evt as QuicDatapathRecvEvent;
-                    sample.BytesReceived += _evt!.TotalSize;
-                    sample.ReceiveEventCount++;
-                }
-                else
-                {
-                    continue;
-                }
-
-                if (sample.TimeStamp + Resolution <= evt.TimeStamp || eventIndex == eventCount)
-                {
-                    sample.Duration = evt.TimeStamp - sample.TimeStamp;
-                    sample.TxRate = (sample.BytesSent * 8 * 1000 * 1000 * 1000) / (ulong)sample.Duration.ToNanoseconds;
-                    sample.RxRate = (sample.BytesReceived * 8 * 1000 * 1000 * 1000) / (ulong)sample.Duration.ToNanoseconds;
-
-                    datapathEvents.Add(sample);
-
-                    sample.TimeStamp = evt.TimeStamp;
-                    sample.BytesSent = 0;
-                    sample.BytesReceived = 0;
-                    sample.SendEventCount = 0;
-                    sample.ReceiveEventCount = 0;
-                }
-            }
-            return datapathEvents;
-        }
-
         internal QuicDatapath(ulong pointer, uint processId)
         {
             Id = NextId++;
