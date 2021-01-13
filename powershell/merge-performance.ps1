9fad96fe334971d2fc8b682fda8a13c6c9d3e651
Thad House <thhous@microsoft.com>
2021-01-13 15:15:01 -0800

    Fix branch name for PR's

--- powershell/merge-performance.ps1
+++ powershell/merge-performance.ps1
@@ -27,8 +27,7 @@ $PSDefaultParameterValues['*:ErrorAction'] = 'Stop'
 
 $PotentialPR = $Env:SYSTEM_PULLREQUEST_TARGETBRANCH
 if (![string]::IsNullOrWhiteSpace($PotentialPR)) {
-    Write-Output $PotentialPR
-    $BranchName = $PotentialPR.Substring(11);
+    $BranchName = $PotentialPR;
 } elseif ($Branch.StartsWith("refs/heads/")) {
     # Remove the 'refs/heads/' prefix.
     $BranchName = $Branch.Substring(11);
