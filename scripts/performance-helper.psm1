17f5f68da5b3037fc937d8743dfba91e3b8af57a
Nick Banks <nibanks@microsoft.com>
2021-01-19 08:42:57 -0800

    Revert perf-helper change

--- scripts/performance-helper.psm1
+++ scripts/performance-helper.psm1
@@ -63,7 +63,11 @@ $WpaQUICLogProfileXml = `
         <Stack Value="SampledProfile"/>
       </Stacks>
     </SystemProvider>
-    <EventProvider Id="MsQuicEtwPerf" Name="ff15e657-4f26-570e-88ab-0796b258d11c" NonPagedMemory="true" Level="5"/>
+    <EventProvider Id="MsQuicEtwPerf" Name="ff15e657-4f26-570e-88ab-0796b258d11c" NonPagedMemory="true" Level="5">
+      <Keywords>
+        <Keyword Value="0xE0000000"/>
+      </Keywords>
+    </EventProvider>
     <Profile Id="CPU.Light.File" Name="CPU" Description="CPU Stacks" LoggingMode="File" DetailLevel="Light">
       <Collectors>
         <SystemCollectorId Value="SC_HighVolume">
