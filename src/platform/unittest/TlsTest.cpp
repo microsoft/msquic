2921a997e66ec6a4436f39d3985d87f838d15551
Thad House <ThadHouse@users.noreply.github.com>
2021-02-01 10:57:08 -0800

    Fix missing field initializer in TlsTest (#1206)
    * Fix missing field initializer in TlsTest
    
    There are 6 fields, but only 4 initializers, which means TicketKey and AsyncHandler were never initialized.

--- src/platform/unittest/TlsTest.cpp
+++ src/platform/unittest/TlsTest.cpp
@@ -78,6 +78,8 @@ protected:
             QUIC_CREDENTIAL_TYPE_NONE,
             QUIC_CREDENTIAL_FLAG_CLIENT,
             NULL,
+            NULL,
+            NULL,
             NULL
         };
         VERIFY_QUIC_SUCCESS(
