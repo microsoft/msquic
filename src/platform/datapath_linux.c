687aecf638897609a837620b1ecbc2cbbc8934cc
Thad House <thhous@microsoft.com>
2021-02-10 15:12:51 -0800

    1 more semicolon

--- src/platform/datapath_linux.c
+++ src/platform/datapath_linux.c
@@ -1605,7 +1605,7 @@ CxPlatSocketCreateUdp(
 
     CXPLAT_DBG_ASSERT(Datapath->UdpHandlers.Receive != NULL);
 
-    uint32_t SocketCount = IsServerSocket ? Datapath->ProcCount : 1
+    uint32_t SocketCount = IsServerSocket ? Datapath->ProcCount : 1;
     CXPLAT_FRE_ASSERT(SocketCount > 0);
     size_t BindingLength =
         sizeof(CXPLAT_SOCKET) +
