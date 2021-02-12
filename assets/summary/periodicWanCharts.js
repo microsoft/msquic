f18e287efc4ce292b72e840d62a793a08f578716
Nick Banks <nibanks@microsoft.com>
2021-02-12 14:22:36 -0800

    Improve WAN Perf Charts

--- assets/summary/periodicWanCharts.js
+++ assets/summary/periodicWanCharts.js
@@ -1,21 +1,68 @@
-window.onload = function() {
+
+var filteredWanPerfData = []
+
+function generateWanPerfData() {
+    var baseData = wanPerfData[platformList[0]]
+    for (let i = 0; i < baseData.length; i +=2) {
+        var tcpData = wanPerfData[platformList[0]][i]
+        var quicData = wanPerfData[platformList[0]][i+1]
+        filteredWanPerfData.push({
+            "BottleneckMbps": tcpData.BottleneckMbps,
+            "RttMs": tcpData.RttMs,
+            "BottleneckBufferPackets": tcpData.BottleneckBufferPackets,
+            "RandomLossDenominator": tcpData.RandomLossDenominator,
+            "RandomReorderDenominator": tcpData.RandomReorderDenominator,
+            "ReorderDelayDeltaMs": tcpData.ReorderDelayDeltaMs,
+            "DurationMs": tcpData.DurationMs,
+            "TcpRateKbps": tcpData.RateKbps,
+            "QuicRateKbps": quicData.RateKbps,
+            "DiffPercent": (quicData.RateKbps - tcpData.RateKbps) / (tcpData.BottleneckMbps * 10)
+        })
+    }
+}
+
+function generateWanTable() {
     var table = document.getElementById("WanTable");
     var thead = document.createElement('thead');
     var tr = document.createElement('tr');
     thead.appendChild(tr);
     table.appendChild(thead)
-    var dataList = []
-    // Load each field, get its name
-    for (var name in wanPerfData[platformList[0]][0]) {
-        // Ignore RawRateKbps
-        if (name == "RawRateKbps") continue
-        var element = document.createElement("th");
-        element.innerText = name;
-        tr.appendChild(element);
-        dataList.push({ data: name });
-    }
+    var columnNames = [
+        "Network (Mbps)",
+        "RTT (ms)",
+        "Queue (pkts)",
+        "Loss (1/N)",
+        "Reorder (1/N)",
+        "Reorder Delay (ms)",
+        //"Duration (ms)",
+        "STCP Goodput (Kbps)",
+        "QUIC Goodput (Kbps)",
+        "Diff (%)"
+    ].forEach (
+        name => {
+            var element = document.createElement("th");
+            element.innerText = name;
+            tr.appendChild(element);
+        }
+    )
     $('#WanTable').DataTable({
-        data: wanPerfData[platformList[0]],
-        columns: dataList
+        data: filteredWanPerfData,
+        columns: [
+            { data: "BottleneckMbps" },
+            { data: "RttMs" },
+            { data: "BottleneckBufferPackets" },
+            { data: "RandomLossDenominator" },
+            { data: "RandomReorderDenominator" },
+            { data: "ReorderDelayDeltaMs" },
+            //{ data: "DurationMs" },
+            { data: "TcpRateKbps" },
+            { data: "QuicRateKbps" },
+            { data: "DiffPercent" }
+        ]
     })
+}
+
+window.onload = function() {
+    generateWanPerfData()
+    generateWanTable()
 }
