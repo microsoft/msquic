e44ebf585839fe53f8818c907f8473a843945422
Thad House <thhous@microsoft.com>
2021-03-27 21:57:28 -0700

    Get windows defs generated, requires some slight header fixes.

--- scripts/generate-dotnet.ps1
+++ scripts/generate-dotnet.ps1
@@ -13,4 +13,11 @@ $MsQuicWindowsHeader = Join-Path $RootDir src inc msquic_winuser.h
 
 ClangSharpPInvokeGenerator -f $MsQuicHeader -n Microsoft.Quic -o $MsQuicGeneratedSource -m MsQuic -l msquic -c exclude-enum-operators -r _SOCKADDR_INET=QuicAddr -c generate-macro-bindings
 
-ClangSharpPInvokeGenerator -f $MsQuicWindowsHeader -n Microsoft.Quic -o $MsQuicWindowsGeneratedSource -m MsQuic -l msquic -c generate-macro-bindings
+ClangSharpPInvokeGenerator -f $MsQuicWindowsHeader -n Microsoft.Quic -o $MsQuicWindowsGeneratedSource -m MsQuic_Windows -l msquic -c generate-macro-bindings -c exclude-funcs-with-body `
+    -e QUIC_ADDR_STR `
+    -e QUIC_ADDR_V4_PORT_OFFSET `
+    -e QUIC_ADDR_V4_IP_OFFSET `
+    -e QUIC_ADDR_V6_PORT_OFFSET `
+    -e QUIC_ADDR_V6_IP_OFFSET `
+
+
--- src/cs/lib/msquic_generated_windows.cs
+++ src/cs/lib/msquic_generated_windows.cs
@@ -1,209 +1,7 @@
 namespace Microsoft.Quic
 {
-    public unsafe partial struct QUIC_ADDR_STR
+    public static partial class MsQuic_Windows
     {
-        [NativeTypeName("char [64]")]
-        public fixed sbyte Address[64];
-    }
-
-    public static unsafe partial class MsQuic
-    {
-        [return: NativeTypeName("BOOLEAN")]
-        public static byte QuicAddrIsValid([NativeTypeName("const QUIC_ADDR *const")] _SOCKADDR_INET* Addr)
-        {
-            return (byte)((Addr->si_family == 0 || Addr->si_family == 2 || Addr->si_family == 23) ? 1 : 0);
-        }
-
-        [return: NativeTypeName("BOOLEAN")]
-        public static byte QuicAddrCompareIp([NativeTypeName("const QUIC_ADDR *const")] _SOCKADDR_INET* Addr1, [NativeTypeName("const QUIC_ADDR *const")] _SOCKADDR_INET* Addr2)
-        {
-            if (Addr1->si_family == 2)
-            {
-                return (byte)((memcmp(&Addr1->Ipv4.sin_addr, &Addr2->Ipv4.sin_addr, sizeof(in_addr)) == 0) ? 1 : 0);
-            }
-            else
-            {
-                return (byte)((memcmp(&Addr1->Ipv6.sin6_addr, &Addr2->Ipv6.sin6_addr, sizeof(in6_addr)) == 0) ? 1 : 0);
-            }
-        }
-
-        [return: NativeTypeName("BOOLEAN")]
-        public static byte QuicAddrCompare([NativeTypeName("const QUIC_ADDR *const")] _SOCKADDR_INET* Addr1, [NativeTypeName("const QUIC_ADDR *const")] _SOCKADDR_INET* Addr2)
-        {
-            if (Addr1->si_family != Addr2->si_family || Addr1->Ipv4.sin_port != Addr2->Ipv4.sin_port)
-            {
-                return 0;
-            }
-
-            return QuicAddrCompareIp(Addr1, Addr2);
-        }
-
-        [return: NativeTypeName("BOOLEAN")]
-        public static byte QuicAddrIsWildCard([NativeTypeName("const QUIC_ADDR *const")] _SOCKADDR_INET* Addr)
-        {
-            if (Addr->si_family == 0)
-            {
-                return 1;
-            }
-            else if (Addr->si_family == 2)
-            {
-                in_addr ZeroAddr = new in_addr
-                {
-                    S_un = new _S_un_e__Union
-                    {
-                        S_un_b = new _S_un_b_e__Struct
-                        {
-                            s_b1 = 0,
-                        },
-                    },
-                };
-
-                return (byte)((memcmp(&Addr->Ipv4.sin_addr, &ZeroAddr, sizeof(in_addr)) == 0) ? 1 : 0);
-            }
-            else
-            {
-                in6_addr ZeroAddr = new in6_addr
-                {
-                    u = new _u_e__Union
-                    {
-                        Byte = new byte[16]
-                        {
-                            0,
-                            default,
-                            default,
-                            default,
-                            default,
-                            default,
-                            default,
-                            default,
-                            default,
-                            default,
-                            default,
-                            default,
-                            default,
-                            default,
-                            default,
-                            default,
-                        },
-                    },
-                };
-
-                return (byte)((memcmp(&Addr->Ipv6.sin6_addr, &ZeroAddr, sizeof(in6_addr)) == 0) ? 1 : 0);
-            }
-        }
-
-        [return: NativeTypeName("QUIC_ADDRESS_FAMILY")]
-        public static ushort QuicAddrGetFamily([NativeTypeName("const QUIC_ADDR *const")] _SOCKADDR_INET* Addr)
-        {
-            return (ushort)(Addr->si_family);
-        }
-
-        public static void QuicAddrSetFamily([NativeTypeName("QUIC_ADDR *")] _SOCKADDR_INET* Addr, [NativeTypeName("QUIC_ADDRESS_FAMILY")] ushort Family)
-        {
-            Addr->si_family = (ushort)(Family);
-        }
-
-        [return: NativeTypeName("uint16_t")]
-        public static ushort QuicAddrGetPort([NativeTypeName("const QUIC_ADDR *const")] _SOCKADDR_INET* Addr)
-        {
-            return ((ushort)((((Addr->Ipv4.sin_port) & 0x00ff) << 8) | (((Addr->Ipv4.sin_port) & 0xff00) >> 8)));
-        }
-
-        public static void QuicAddrSetPort([NativeTypeName("QUIC_ADDR *")] _SOCKADDR_INET* Addr, [NativeTypeName("uint16_t")] ushort Port)
-        {
-            Addr->Ipv4.sin_port = ((ushort)((((Port) & 0x00ff) << 8) | (((Port) & 0xff00) >> 8)));
-        }
-
-        public static void QuicAddrSetToLoopback([NativeTypeName("QUIC_ADDR *")] _SOCKADDR_INET* Addr)
-        {
-            if (Addr->si_family == 2)
-            {
-                Addr->Ipv4.sin_addr.S_un.S_un_b.s_b1 = 127;
-                Addr->Ipv4.sin_addr.S_un.S_un_b.s_b4 = 1;
-            }
-            else
-            {
-                Addr->Ipv6.sin6_addr.u.Byte[15] = 1;
-            }
-        }
-
-        public static void QuicAddrIncrement([NativeTypeName("QUIC_ADDR *")] _SOCKADDR_INET* Addr)
-        {
-            if (Addr->si_family == 2)
-            {
-                Addr->Ipv4.sin_addr.S_un.S_un_b.s_b4++;
-            }
-            else
-            {
-                Addr->Ipv6.sin6_addr.u.Byte[15]++;
-            }
-        }
-
-        [return: NativeTypeName("uint32_t")]
-        public static uint QuicAddrHash([NativeTypeName("const QUIC_ADDR *")] _SOCKADDR_INET* Addr)
-        {
-            uint Hash = 5387;
-
-            if (Addr->si_family == 2)
-            {
-                Hash = ((Hash << 5) - Hash) + (Addr->Ipv4.sin_port & 0xFF);
-                Hash = ((Hash << 5) - Hash) + (Addr->Ipv4.sin_port >> 8);
-                for (byte i = 0; i < sizeof(in_addr); ++i)
-                {
-                    Hash = ((Hash << 5) - Hash) + (i[((byte*)(&Addr->Ipv4.sin_addr))]);
-                }
-            }
-            else
-            {
-                Hash = ((Hash << 5) - Hash) + (Addr->Ipv6.sin6_port & 0xFF);
-                Hash = ((Hash << 5) - Hash) + (Addr->Ipv6.sin6_port >> 8);
-                for (byte i = 0; i < sizeof(in6_addr); ++i)
-                {
-                    Hash = ((Hash << 5) - Hash) + (i[((byte*)(&Addr->Ipv6.sin6_addr))]);
-                }
-            }
-
-            return Hash;
-        }
-
-        [return: NativeTypeName("BOOLEAN")]
-        public static byte QuicAddrFromString([NativeTypeName("const char *")] sbyte* AddrStr, [NativeTypeName("uint16_t")] ushort Port, [NativeTypeName("QUIC_ADDR *")] _SOCKADDR_INET* Addr)
-        {
-            Addr->Ipv4.sin_port = ((ushort)((((Port) & 0x00ff) << 8) | (((Port) & 0xff00) >> 8)));
-            if (RtlIpv4StringToAddressExA(AddrStr, 0, &Addr->Ipv4.sin_addr, &Addr->Ipv4.sin_port) == 0)
-            {
-                Addr->si_family = 2;
-            }
-            else if (RtlIpv6StringToAddressExA(AddrStr, &Addr->Ipv6.sin6_addr, &Addr->Ipv6.Anonymous.sin6_scope_id, &Addr->Ipv6.sin6_port) == 0)
-            {
-                Addr->si_family = 23;
-            }
-            else
-            {
-                return 0;
-            }
-
-            return 1;
-        }
-
-        [return: NativeTypeName("BOOLEAN")]
-        public static byte QuicAddrToString([NativeTypeName("const QUIC_ADDR *")] _SOCKADDR_INET* Addr, [NativeTypeName("QUIC_ADDR_STR *")] QUIC_ADDR_STR* AddrStr)
-        {
-            int Status;
-            uint AddrStrLen = unchecked(64);
-
-            if (Addr->si_family == 2)
-            {
-                Status = RtlIpv4AddressToStringExA(&Addr->Ipv4.sin_addr, Addr->Ipv4.sin_port, AddrStr->Address, &AddrStrLen);
-            }
-            else
-            {
-                Status = RtlIpv6AddressToStringExA(&Addr->Ipv6.sin6_addr, 0, Addr->Ipv6.sin6_port, AddrStr->Address, &AddrStrLen);
-            }
-
-            return (byte)((Status == 0) ? 1 : 0);
-        }
-
         [NativeTypeName("#define ERROR_QUIC_USER_CANCELED _HRESULT_TYPEDEF_(0x80410002L)")]
         public const int ERROR_QUIC_USER_CANCELED = unchecked((int)(0x80410002));
 
@@ -243,17 +41,17 @@ namespace Microsoft.Quic
         [NativeTypeName("#define QUIC_STATUS_INVALID_PARAMETER E_INVALIDARG")]
         public const int QUIC_STATUS_INVALID_PARAMETER = unchecked((int)(0x80070057));
 
-        [NativeTypeName("#define QUIC_STATUS_INVALID_STATE E_NOT_VALID_STATE")]
-        public static readonly int QUIC_STATUS_INVALID_STATE = HRESULT_FROM_WIN32(5023);
+        [NativeTypeName("#define QUIC_STATUS_INVALID_STATE __HRESULT_FROM_WIN32(ERROR_INVALID_STATE)")]
+        public static readonly int QUIC_STATUS_INVALID_STATE = unchecked((int)(5023) <= 0 ? ((int)(5023)) : ((int)(((5023) & 0x0000FFFF) | (7 << 16) | 0x80000000)));
 
         [NativeTypeName("#define QUIC_STATUS_NOT_SUPPORTED E_NOINTERFACE")]
         public const int QUIC_STATUS_NOT_SUPPORTED = unchecked((int)(0x80004002));
 
-        [NativeTypeName("#define QUIC_STATUS_NOT_FOUND HRESULT_FROM_WIN32(ERROR_NOT_FOUND)")]
-        public static readonly int QUIC_STATUS_NOT_FOUND = HRESULT_FROM_WIN32(1168);
+        [NativeTypeName("#define QUIC_STATUS_NOT_FOUND __HRESULT_FROM_WIN32(ERROR_NOT_FOUND)")]
+        public static readonly int QUIC_STATUS_NOT_FOUND = unchecked((int)(1168) <= 0 ? ((int)(1168)) : ((int)(((1168) & 0x0000FFFF) | (7 << 16) | 0x80000000)));
 
-        [NativeTypeName("#define QUIC_STATUS_BUFFER_TOO_SMALL E_NOT_SUFFICIENT_BUFFER")]
-        public static readonly int QUIC_STATUS_BUFFER_TOO_SMALL = HRESULT_FROM_WIN32(122);
+        [NativeTypeName("#define QUIC_STATUS_BUFFER_TOO_SMALL __HRESULT_FROM_WIN32(ERROR_INSUFFICIENT_BUFFER)")]
+        public static readonly int QUIC_STATUS_BUFFER_TOO_SMALL = unchecked((int)(122) <= 0 ? ((int)(122)) : ((int)(((122) & 0x0000FFFF) | (7 << 16) | 0x80000000)));
 
         [NativeTypeName("#define QUIC_STATUS_HANDSHAKE_FAILURE ERROR_QUIC_HANDSHAKE_FAILURE")]
         public const int QUIC_STATUS_HANDSHAKE_FAILURE = unchecked((int)(0x80410000));
@@ -261,8 +59,8 @@ namespace Microsoft.Quic
         [NativeTypeName("#define QUIC_STATUS_ABORTED E_ABORT")]
         public const int QUIC_STATUS_ABORTED = unchecked((int)(0x80004004));
 
-        [NativeTypeName("#define QUIC_STATUS_ADDRESS_IN_USE HRESULT_FROM_WIN32(WSAEADDRINUSE)")]
-        public static readonly int QUIC_STATUS_ADDRESS_IN_USE = HRESULT_FROM_WIN32(10048);
+        [NativeTypeName("#define QUIC_STATUS_ADDRESS_IN_USE __HRESULT_FROM_WIN32(WSAEADDRINUSE)")]
+        public static readonly int QUIC_STATUS_ADDRESS_IN_USE = unchecked((int)(10048) <= 0 ? ((int)(10048)) : ((int)(((10048) & 0x0000FFFF) | (7 << 16) | 0x80000000)));
 
         [NativeTypeName("#define QUIC_STATUS_CONNECTION_TIMEOUT ERROR_QUIC_CONNECTION_TIMEOUT")]
         public const int QUIC_STATUS_CONNECTION_TIMEOUT = unchecked((int)(0x80410006));
@@ -270,14 +68,14 @@ namespace Microsoft.Quic
         [NativeTypeName("#define QUIC_STATUS_CONNECTION_IDLE ERROR_QUIC_CONNECTION_IDLE")]
         public const int QUIC_STATUS_CONNECTION_IDLE = unchecked((int)(0x80410005));
 
-        [NativeTypeName("#define QUIC_STATUS_UNREACHABLE HRESULT_FROM_WIN32(ERROR_HOST_UNREACHABLE)")]
-        public static readonly int QUIC_STATUS_UNREACHABLE = HRESULT_FROM_WIN32(1232);
+        [NativeTypeName("#define QUIC_STATUS_UNREACHABLE __HRESULT_FROM_WIN32(ERROR_HOST_UNREACHABLE)")]
+        public static readonly int QUIC_STATUS_UNREACHABLE = unchecked((int)(1232) <= 0 ? ((int)(1232)) : ((int)(((1232) & 0x0000FFFF) | (7 << 16) | 0x80000000)));
 
         [NativeTypeName("#define QUIC_STATUS_INTERNAL_ERROR ERROR_QUIC_INTERNAL_ERROR")]
         public const int QUIC_STATUS_INTERNAL_ERROR = unchecked((int)(0x80410003));
 
-        [NativeTypeName("#define QUIC_STATUS_CONNECTION_REFUSED HRESULT_FROM_WIN32(ERROR_CONNECTION_REFUSED)")]
-        public static readonly int QUIC_STATUS_CONNECTION_REFUSED = HRESULT_FROM_WIN32(1225);
+        [NativeTypeName("#define QUIC_STATUS_CONNECTION_REFUSED __HRESULT_FROM_WIN32(ERROR_CONNECTION_REFUSED)")]
+        public static readonly int QUIC_STATUS_CONNECTION_REFUSED = unchecked((int)(1225) <= 0 ? ((int)(1225)) : ((int)(((1225) & 0x0000FFFF) | (7 << 16) | 0x80000000)));
 
         [NativeTypeName("#define QUIC_STATUS_PROTOCOL_ERROR ERROR_QUIC_PROTOCOL_VIOLATION")]
         public const int QUIC_STATUS_PROTOCOL_ERROR = unchecked((int)(0x80410004));
@@ -285,8 +83,8 @@ namespace Microsoft.Quic
         [NativeTypeName("#define QUIC_STATUS_VER_NEG_ERROR ERROR_QUIC_VER_NEG_FAILURE")]
         public const int QUIC_STATUS_VER_NEG_ERROR = unchecked((int)(0x80410001));
 
-        [NativeTypeName("#define QUIC_STATUS_TLS_ERROR HRESULT_FROM_WIN32(WSA_SECURE_HOST_NOT_FOUND)")]
-        public static readonly int QUIC_STATUS_TLS_ERROR = HRESULT_FROM_WIN32(11032);
+        [NativeTypeName("#define QUIC_STATUS_TLS_ERROR __HRESULT_FROM_WIN32(WSA_SECURE_HOST_NOT_FOUND)")]
+        public static readonly int QUIC_STATUS_TLS_ERROR = unchecked((int)(11032) <= 0 ? ((int)(11032)) : ((int)(((11032) & 0x0000FFFF) | (7 << 16) | 0x80000000)));
 
         [NativeTypeName("#define QUIC_STATUS_USER_CANCELED ERROR_QUIC_USER_CANCELED")]
         public const int QUIC_STATUS_USER_CANCELED = unchecked((int)(0x80410002));
@@ -305,18 +103,6 @@ namespace Microsoft.Quic
 
         [NativeTypeName("#define QUIC_STATUS_EXPIRED_CERTIFICATE QUIC_STATUS_TLS_ALERT(45)")]
         public const int QUIC_STATUS_EXPIRED_CERTIFICATE = unchecked(((int)(0x80410100)) | (0xff & 45));
-
-        [NativeTypeName("#define QUIC_ADDR_V4_PORT_OFFSET FIELD_OFFSET(SOCKADDR_IN, sin_port)")]
-        public static readonly int QUIC_ADDR_V4_PORT_OFFSET = ((int)((long)(&(((sockaddr_in*)(0))->sin_port))));
-
-        [NativeTypeName("#define QUIC_ADDR_V4_IP_OFFSET FIELD_OFFSET(SOCKADDR_IN, sin_addr)")]
-        public static readonly int QUIC_ADDR_V4_IP_OFFSET = ((int)((long)(&(((sockaddr_in*)(0))->sin_addr))));
-
-        [NativeTypeName("#define QUIC_ADDR_V6_PORT_OFFSET FIELD_OFFSET(SOCKADDR_IN6, sin6_port)")]
-        public static readonly int QUIC_ADDR_V6_PORT_OFFSET = ((int)((long)(&(((sockaddr_in6*)(0))->sin6_port))));
-
-        [NativeTypeName("#define QUIC_ADDR_V6_IP_OFFSET FIELD_OFFSET(SOCKADDR_IN6, sin6_addr)")]
-        public static readonly int QUIC_ADDR_V6_IP_OFFSET = ((int)((long)(&(((sockaddr_in6*)(0))->sin6_addr))));
 
         [NativeTypeName("#define QUIC_ADDRESS_FAMILY_UNSPEC AF_UNSPEC")]
         public const int QUIC_ADDRESS_FAMILY_UNSPEC = 0;
