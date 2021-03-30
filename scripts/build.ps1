2885254c138f7fb056cbac214b0ad8140537a30a
Thad House <thhous@microsoft.com>
2021-03-30 10:04:10 -0700

    1 last fix

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -209,7 +209,7 @@ if ($Generator -eq "") {
     if ($IsWindows) {
         $Generator = "Visual Studio 16 2019"
     } else {
-        $Generator = "Unix MakeFiles"
+        $Generator = "Unix Makefiles"
     }
 }
 
