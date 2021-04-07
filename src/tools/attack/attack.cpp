84df1ee5458ec9ed50209ebcea51365ac1863910
Thad House <thhous@microsoft.com>
2021-04-07 15:48:34 -0700

    1 last fix

--- src/tools/attack/attack.cpp
+++ src/tools/attack/attack.cpp
@@ -162,7 +162,7 @@ void RunAttackRandom(CXPLAT_SOCKET* Binding, uint16_t Length, bool ValidQuic)
             &LocalAddress,
             &ServerAddress,
             SendData,
-            CxPlatProcCurrentNumber())));
+            (uint16_6)CxPlatProcCurrentNumber())));
     }
 }
 
@@ -293,7 +293,7 @@ void RunAttackValidInitial(CXPLAT_SOCKET* Binding)
             &LocalAddress,
             &ServerAddress,
             SendData,
-            CxPlatProcCurrentNumber())));
+            (uint16_6)CxPlatProcCurrentNumber())));
     }
 }
 
