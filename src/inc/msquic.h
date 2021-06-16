2364b1033a3ea62a51971f727b70a17c4a610692
Nick Banks <nibanks@microsoft.com>
2021-06-16 10:10:49 -0700

    Undo added spaces in header

--- src/inc/msquic.h
+++ src/inc/msquic.h
@@ -523,8 +523,8 @@ typedef struct QUIC_SETTINGS {
     uint32_t KeepAliveIntervalMs;
     uint16_t PeerBidiStreamCount;
     uint16_t PeerUnidiStreamCount;
-    uint16_t RetryMemoryLimit;                  // Global only
-    uint16_t LoadBalancingMode;                 // Global only
+    uint16_t RetryMemoryLimit;              // Global only
+    uint16_t LoadBalancingMode;             // Global only
     uint8_t MaxOperationsPerDrain;
     uint8_t SendBufferingEnabled            : 1;
     uint8_t PacingEnabled                   : 1;
