2c2a1b099cc19cd06b87e726e05c5e04a0674e40
Thad House <thhous@microsoft.com>
2021-06-22 12:01:27 -0700

    Fix another leak

--- src/core/library.c
+++ src/core/library.c
@@ -543,6 +543,7 @@ MsQuicLibraryUninitialize(
     QuicSettingsCleanup(&MsQuicLib.Settings);
 
     CXPLAT_FREE(MsQuicLib.DefaultCompatibilityList, QUIC_POOL_DEFAULT_COMPAT_VER_LIST);
+    MsQuicLib.DefaultCompatibilityList = NULL;
 
     QuicTraceEvent(
         LibraryUninitialized,
