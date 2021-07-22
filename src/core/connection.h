7cafdc4aa4ff42803218c4a421b673c4c45207b6
Anthony Rossi <41394064+anrossi@users.noreply.github.com>
2021-07-22 10:11:15 -0700

    Version Negotiation Extension v4 (#1826)

--- src/core/connection.h
+++ src/core/connection.h
@@ -27,7 +27,7 @@ typedef struct QUIC_LISTENER QUIC_LISTENER;
 // Note - Keep quictypes.h's copy up to date.
 //
 typedef union QUIC_CONNECTION_STATE {
-    uint32_t Flags;
+    uint64_t Flags;
     struct {
         BOOLEAN Allocated       : 1;    // Allocated. Used for Debugging.
         BOOLEAN Initialized     : 1;    // Initialized successfully. Used for Debugging.
@@ -159,6 +159,17 @@ typedef union QUIC_CONNECTION_STATE {
         //
         BOOLEAN InlineApiExecution : 1;
 
+        //
+        // True when a server attempts Compatible Version Negotiation
+        BOOLEAN CompatibleVerNegotiationAttempted : 1;
+
+        //
+        // True once a client connection has completed a compatible version
+        // negotiation, and false otherwise. Used to prevent packets with invalid
+        // version fields from being accepted.
+        //
+        BOOLEAN CompatibleVerNegotiationCompleted : 1;
+
 #ifdef CxPlatVerifierEnabledByAddr
         //
         // The calling app is being verified (app or driver verifier).
@@ -168,7 +179,7 @@ typedef union QUIC_CONNECTION_STATE {
     };
 } QUIC_CONNECTION_STATE;
 
-CXPLAT_STATIC_ASSERT(sizeof(QUIC_CONNECTION_STATE) == sizeof(uint32_t), "Ensure correct size/type");
+CXPLAT_STATIC_ASSERT(sizeof(QUIC_CONNECTION_STATE) == sizeof(uint64_t), "Ensure correct size/type");
 
 //
 // Different references on a connection.
@@ -571,15 +582,15 @@ typedef struct QUIC_CONNECTION {
 #endif
 
     //
-    // Received version negotiation list from a previous connection attempt.
-    //
-    const uint32_t* ReceivedNegotiationVersions;
-    uint32_t ReceivedNegotiationVersionsLength;
-
-    //
-    // Previously-attempted QUIC version.
+    // Previously-attempted QUIC version, after Incompatible Version Negotiation.
     //
     uint32_t PreviousQuicVersion;
+
+    //
+    // Initially-attempted QUIC version.
+    // Only populated during compatible version negotiation.
+    //
+    uint32_t OriginalQuicVersion;
 
     //
     // The size of the keep alive padding.
