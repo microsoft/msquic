a23f08040c3aefc004f608810b86b27098c49e7a
Nick Banks <nibanks@microsoft.com>
2021-07-23 17:19:06 -0500

    Support ConnectionClose in NEW_CONNECTION Event (#1849)

--- src/test/bin/quic_gtest.cpp
+++ src/test/bin/quic_gtest.cpp
@@ -336,6 +336,16 @@ TEST(Basic, CreateConnection) {
     }
 }
 
+TEST_P(WithBool, RejectConnection) {
+    TestLoggerT<ParamType> Logger("QuicTestConnectionRejection", GetParam());
+    if (TestingKernelMode) {
+        uint8_t Param = (uint8_t)GetParam();
+        ASSERT_TRUE(DriverClient.Run(IOCTL_QUIC_RUN_CONNECTION_REJECTION, Param));
+    } else {
+        QuicTestConnectionRejection(GetParam());
+    }
+}
+
 #ifdef QUIC_TEST_DATAPATH_HOOKS_ENABLED
 
 TEST_P(WithFamilyArgs, LocalPathChanges) {
