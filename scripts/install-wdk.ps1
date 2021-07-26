76410f86812c4633fda09a6a8e5b2d452e786cf5
Thad House <thhous@microsoft.com>
2021-07-26 12:16:44 -0700

    Install WDK

---
+++ scripts/install-wdk.ps1
@@ -0,0 +1,186 @@
+function Get-ToolsetContent
+{
+    $toolsetJson = Get-Content -Path $env:TOOLSET_JSON_PATH -Raw
+    ConvertFrom-Json -InputObject $toolsetJson
+}
+
+function Start-DownloadWithRetry
+{
+    Param
+    (
+        [Parameter(Mandatory)]
+        [string] $Url,
+        [string] $Name,
+        [string] $DownloadPath = "${env:Temp}",
+        [int] $Retries = 20
+    )
+
+    if ([String]::IsNullOrEmpty($Name)) {
+        $Name = [IO.Path]::GetFileName($Url)
+    }
+
+    $filePath = Join-Path -Path $DownloadPath -ChildPath $Name
+
+    #Default retry logic for the package.
+    while ($Retries -gt 0)
+    {
+        try
+        {
+            Write-Host "Downloading package from: $Url to path $filePath ."
+            (New-Object System.Net.WebClient).DownloadFile($Url, $filePath)
+            break
+        }
+        catch
+        {
+            Write-Host "There is an error during package downloading:`n $_"
+            $Retries--
+
+            if ($Retries -eq 0)
+            {
+                Write-Host "File can't be downloaded. Please try later or check that file exists by url: $Url"
+                exit 1
+            }
+
+            Write-Host "Waiting 30 seconds before retrying. Retries left: $Retries"
+            Start-Sleep -Seconds 30
+        }
+    }
+
+    return $filePath
+}
+
+function Install-Binary
+{
+    <#
+    .SYNOPSIS
+        A helper function to install executables.
+    .DESCRIPTION
+        Download and install .exe or .msi binaries from specified URL.
+    .PARAMETER Url
+        The URL from which the binary will be downloaded. Required parameter.
+    .PARAMETER Name
+        The Name with which binary will be downloaded. Required parameter.
+    .PARAMETER ArgumentList
+        The list of arguments that will be passed to the installer. Required for .exe binaries.
+    .EXAMPLE
+        Install-Binary -Url "https://go.microsoft.com/fwlink/p/?linkid=2083338" -Name "winsdksetup.exe" -ArgumentList ("/features", "+", "/quiet")
+    #>
+
+    Param
+    (
+        [Parameter(Mandatory)]
+        [String] $Url,
+        [Parameter(Mandatory)]
+        [String] $Name,
+        [String[]] $ArgumentList
+    )
+
+    Write-Host "Downloading $Name..."
+    $filePath = Start-DownloadWithRetry -Url $Url -Name $Name
+
+    # MSI binaries should be installed via msiexec.exe
+    $fileExtension = ([System.IO.Path]::GetExtension($Name)).Replace(".", "")
+    if ($fileExtension -eq "msi")
+    {
+        $ArgumentList = ('/i', $filePath, '/QN', '/norestart')
+        $filePath = "msiexec.exe"
+    }
+
+    try
+    {
+        Write-Host "Starting Install $Name..."
+        $process = Start-Process -FilePath $filePath -ArgumentList $ArgumentList -Wait -PassThru
+
+        $exitCode = $process.ExitCode
+        if ($exitCode -eq 0 -or $exitCode -eq 3010)
+        {
+            Write-Host "Installation successful"
+        }
+        else
+        {
+            Write-Host "Non zero exit code returned by the installation process: $exitCode"
+            exit $exitCode
+        }
+    }
+    catch
+    {
+        Write-Host "Failed to install the $fileExtension ${Name}: $($_.Exception.Message)"
+        exit 1
+    }
+}
+
+function Install-VsixExtension
+{
+    Param
+    (
+        [string] $Url,
+        [Parameter(Mandatory = $true)]
+        [string] $Name,
+        [string] $FilePath,
+        [Parameter(Mandatory = $true)]
+        [string] $VSversion,
+        [int] $retries = 20,
+        [switch] $InstallOnly
+    )
+
+    if (-not $InstallOnly)
+        {
+            $FilePath = Start-DownloadWithRetry -Url $Url -Name $Name
+        }
+
+    $argumentList = ('/quiet', "`"$FilePath`"")
+
+    Write-Host "Starting Install $Name..."
+    $vsEdition = "Enterprise"
+    try
+    {
+        #There are 2 types of packages at the moment - exe and vsix
+        if ($Name -match "vsix")
+        {
+            $process = Start-Process -FilePath "C:\Program Files (x86)\Microsoft Visual Studio\$VSversion\${vsEdition}\Common7\IDE\VSIXInstaller.exe" -ArgumentList $argumentList -Wait -PassThru
+        }
+        else
+        {
+            $process = Start-Process -FilePath ${env:Temp}\$Name /Q -Wait -PassThru
+        }
+    }
+    catch
+    {
+        Write-Host "There is an error during $Name installation"
+        $_
+        exit 1
+    }
+
+    $exitCode = $process.ExitCode
+
+    if ($exitCode -eq 0 -or $exitCode -eq 1001) # 1001 means the extension is already installed
+    {
+        Write-Host "$Name installed successfully"
+    }
+    else
+    {
+        Write-Host "Unsuccessful exit code returned by the installation process: $exitCode."
+        exit 1
+    }
+
+    #Cleanup downloaded installation files
+    if (-not $InstallOnly)
+        {
+            Remove-Item -Force -Confirm:$false $FilePath
+        }
+}
+
+# Requires Windows SDK with the same version number as the WDK
+$winSdkUrl = "https://go.microsoft.com/fwlink/p/?linkid=2120843"
+$wdkUrl = "https://go.microsoft.com/fwlink/?linkid=2128854"
+$FilePath = "C:\Program Files (x86)\Windows Kits\10\Vsix\VS2019\WDK.vsix"
+$VSver = "2019"
+
+# `winsdksetup.exe /features + /quiet` installs all features without showing the GUI
+Install-Binary -Url $winSdkUrl -Name "winsdksetup.exe" -ArgumentList $argumentList
+
+# `wdksetup.exe /features + /quiet` installs all features without showing the GUI
+Install-Binary -Url $wdkUrl -Name "wdksetup.exe" -ArgumentList $argumentList
+
+# Need to install the VSIX to get the build targets when running VSBuild
+Install-VsixExtension -FilePath $FilePath -Name "WDK.vsix" -VSversion $VSver -InstallOnly
