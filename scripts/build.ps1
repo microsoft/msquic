3d918dd193fdd708a619e831a1b9307040a3c183
Thad House <thadhouse1@gmail.com>
2021-08-18 22:22:04 -0700

    Increase minimum target to 10.15

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -306,7 +306,7 @@ function CMake-Generate {
         $IosTCFile = Join-Path $RootDir cmake toolchains ios.cmake
         $Arguments +=  " -DCMAKE_TOOLCHAIN_FILE=""$IosTCFile"" -DENABLE_ARC=0 -DENABLE_BITCODE=0"
         switch ($Arch) {
-            "x64"   { $Arguments += " -DPLATFORM=MAC -DDEPLOYMENT_TARGET=""10.14"" -DCMAKE_OSX_DEPLOYMENT_TARGET=""10.14"""}
+            "x64"   { $Arguments += " -DPLATFORM=MAC -DDEPLOYMENT_TARGET=""10.15"" -DCMAKE_OSX_DEPLOYMENT_TARGET=""10.15"""}
             "arm64" { $Arguments += " -DPLATFORM=MAC_ARM64 -DDEPLOYMENT_TARGET=""11.0"" -DCMAKE_OSX_DEPLOYMENT_TARGET=""11.0"""}
         }
     }
