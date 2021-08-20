995056cb1ca0ced0379a327bc2b8a0682fd44cfd
Wizmann <mail.kuuy@gmail.com>
2021-08-20 10:37:03 +0800

    change congestion control api

--- src/core/congestion_control.h
+++ src/core/congestion_control.h
@@ -158,7 +158,7 @@ _IRQL_requires_max_(DISPATCH_LEVEL)
 BOOLEAN
 QuicCongestionControlOnDataAcknowledged(
     _In_ QUIC_CONGESTION_CONTROL* Cc,
-    _In_ uint64_t TimeNow, // millisec
+    _In_ uint64_t TimeNow, // microsecond
     _In_ uint64_t LargestPacketNumberAcked,
     _In_ uint32_t NumRetransmittableBytes,
     _In_ uint32_t SmoothedRtt
