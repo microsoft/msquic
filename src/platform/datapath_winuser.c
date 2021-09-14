76ea1447eb6d10ba2a17155fade845b2a1033eac
Thad House <87844845+thhous-msft@users.noreply.github.com>
2021-09-14 11:12:26 -0700

    Fix datapath retry count not incrementing (#1986)
    * Fix datapath retry count not incrementing
    
    * Assert if retry failure is for any reason other than OOM

--- src/platform/datapath_winuser.c
+++ src/platform/datapath_winuser.c
@@ -3326,9 +3326,10 @@ Drop:
     QUIC_STATUS Status;
     do {
         Status = CxPlatSocketStartReceive(SocketProc, DatapathProc);
-    } while (!QUIC_SUCCEEDED(Status) && RetryCount < 10);
+    } while (!QUIC_SUCCEEDED(Status) && ++RetryCount < 10);
 
     if (!QUIC_SUCCEEDED(Status)) {
+        CXPLAT_DBG_ASSERT(Status == QUIC_STATUS_OUT_OF_MEMORY);
         QuicTraceEvent(
             DatapathErrorStatus,
             "[data][%p] ERROR, %u, %s.",
