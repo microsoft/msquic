67df818eb4798b9a167dcd178ef45f41d1e13631
Nick Banks <nibanks@microsoft.com>
2021-09-15 08:59:48 -0400

    Some Congestion Control Cleanup

--- src/generated/linux/cubic.c.clog.h
+++ src/generated/linux/cubic.c.clog.h
@@ -1,3 +1,25 @@
+#include <clog.h>
+#undef TRACEPOINT_PROVIDER
+#define TRACEPOINT_PROVIDER CLOG_CUBIC_C
+#undef TRACEPOINT_PROBE_DYNAMIC_LINKAGE
+#define  TRACEPOINT_PROBE_DYNAMIC_LINKAGE
+#undef TRACEPOINT_INCLUDE
+#define TRACEPOINT_INCLUDE "cubic.c.clog.h.lttng.h"
+#if !defined(DEF_CLOG_CUBIC_C) || defined(TRACEPOINT_HEADER_MULTI_READ)
+#define DEF_CLOG_CUBIC_C
+#include <lttng/tracepoint.h>
+#define __int64 __int64_t
+#include "cubic.c.clog.h.lttng.h"
+#endif
+#include <lttng/tracepoint-event.h>
+#ifndef _clog_MACRO_QuicTraceEvent
+#define _clog_MACRO_QuicTraceEvent  1
+#define QuicTraceEvent(a, ...) _clog_CAT(_clog_ARGN_SELECTOR(__VA_ARGS__), _clog_CAT(_,a(#a, __VA_ARGS__)))
+#endif
+#ifdef __cplusplus
+extern "C" {
+#endif
+#ifndef _clog_7_ARGS_TRACE_ConnCubic
 
 
 
@@ -18,21 +40,15 @@
 // arg5 = arg5 = Ctx->WindowMax
 // arg6 = arg6 = Ctx->WindowLastMax
 ----------------------------------------------------------*/
-TRACEPOINT_EVENT(CLOG_CUBIC_IMPL_C, ConnCubic,
-    TP_ARGS(
-        const void *, arg2,
-        unsigned int, arg3,
-        unsigned int, arg4,
-        unsigned int, arg5,
-        unsigned int, arg6), 
-    TP_FIELDS(
-        ctf_integer_hex(uint64_t, arg2, arg2)
-        ctf_integer(unsigned int, arg3, arg3)
-        ctf_integer(unsigned int, arg4, arg4)
-        ctf_integer(unsigned int, arg5, arg5)
-        ctf_integer(unsigned int, arg6, arg6)
-    )
-)
+#define _clog_7_ARGS_TRACE_ConnCubic(uniqueId, encoded_arg_string, arg2, arg3, arg4, arg5, arg6)\
+tracepoint(CLOG_CUBIC_C, ConnCubic , arg2, arg3, arg4, arg5, arg6);\
+
+#endif
+
+
+
+
+#ifndef _clog_3_ARGS_TRACE_ConnCongestion
 
 
 
@@ -45,13 +61,15 @@ TRACEPOINT_EVENT(CLOG_CUBIC_IMPL_C, ConnCubic,
         Connection);
 // arg2 = arg2 = Connection
 ----------------------------------------------------------*/
-TRACEPOINT_EVENT(CLOG_CUBIC_IMPL_C, ConnCongestion,
-    TP_ARGS(
-        const void *, arg2), 
-    TP_FIELDS(
-        ctf_integer_hex(uint64_t, arg2, arg2)
-    )
-)
+#define _clog_3_ARGS_TRACE_ConnCongestion(uniqueId, encoded_arg_string, arg2)\
+tracepoint(CLOG_CUBIC_C, ConnCongestion , arg2);\
+
+#endif
+
+
+
+
+#ifndef _clog_3_ARGS_TRACE_ConnPersistentCongestion
 
 
 
@@ -64,13 +82,15 @@ TRACEPOINT_EVENT(CLOG_CUBIC_IMPL_C, ConnCongestion,
         Connection);
 // arg2 = arg2 = Connection
 ----------------------------------------------------------*/
-TRACEPOINT_EVENT(CLOG_CUBIC_IMPL_C, ConnPersistentCongestion,
-    TP_ARGS(
-        const void *, arg2), 
-    TP_FIELDS(
-        ctf_integer_hex(uint64_t, arg2, arg2)
-    )
-)
+#define _clog_3_ARGS_TRACE_ConnPersistentCongestion(uniqueId, encoded_arg_string, arg2)\
+tracepoint(CLOG_CUBIC_C, ConnPersistentCongestion , arg2);\
+
+#endif
+
+
+
+
+#ifndef _clog_3_ARGS_TRACE_ConnRecoveryExit
 
 
 
@@ -83,13 +103,15 @@ TRACEPOINT_EVENT(CLOG_CUBIC_IMPL_C, ConnPersistentCongestion,
                 Connection);
 // arg2 = arg2 = Connection
 ----------------------------------------------------------*/
-TRACEPOINT_EVENT(CLOG_CUBIC_IMPL_C, ConnRecoveryExit,
-    TP_ARGS(
-        const void *, arg2), 
-    TP_FIELDS(
-        ctf_integer_hex(uint64_t, arg2, arg2)
-    )
-)
+#define _clog_3_ARGS_TRACE_ConnRecoveryExit(uniqueId, encoded_arg_string, arg2)\
+tracepoint(CLOG_CUBIC_C, ConnRecoveryExit , arg2);\
+
+#endif
+
+
+
+
+#ifndef _clog_3_ARGS_TRACE_ConnSpuriousCongestion
 
 
 
@@ -102,13 +124,15 @@ TRACEPOINT_EVENT(CLOG_CUBIC_IMPL_C, ConnRecoveryExit,
         Connection);
 // arg2 = arg2 = Connection
 ----------------------------------------------------------*/
-TRACEPOINT_EVENT(CLOG_CUBIC_IMPL_C, ConnSpuriousCongestion,
-    TP_ARGS(
-        const void *, arg2), 
-    TP_FIELDS(
-        ctf_integer_hex(uint64_t, arg2, arg2)
-    )
-)
+#define _clog_3_ARGS_TRACE_ConnSpuriousCongestion(uniqueId, encoded_arg_string, arg2)\
+tracepoint(CLOG_CUBIC_C, ConnSpuriousCongestion , arg2);\
+
+#endif
+
+
+
+
+#ifndef _clog_12_ARGS_TRACE_ConnOutFlowStats
 
 
 
@@ -139,28 +163,17 @@ TRACEPOINT_EVENT(CLOG_CUBIC_IMPL_C, ConnSpuriousCongestion,
 // arg10 = arg10 = Connection->SendBuffer.PostedBytes
 // arg11 = arg11 = Path->GotFirstRttSample ? Path->SmoothedRtt : 0
 ----------------------------------------------------------*/
-TRACEPOINT_EVENT(CLOG_CUBIC_IMPL_C, ConnOutFlowStats,
-    TP_ARGS(
-        const void *, arg2,
-        unsigned long long, arg3,
-        unsigned int, arg4,
-        unsigned int, arg5,
-        unsigned int, arg6,
-        unsigned int, arg7,
-        unsigned long long, arg8,
-        unsigned long long, arg9,
-        unsigned long long, arg10,
-        unsigned int, arg11), 
-    TP_FIELDS(
-        ctf_integer_hex(uint64_t, arg2, arg2)
-        ctf_integer(uint64_t, arg3, arg3)
-        ctf_integer(unsigned int, arg4, arg4)
-        ctf_integer(unsigned int, arg5, arg5)
-        ctf_integer(unsigned int, arg6, arg6)
-        ctf_integer(unsigned int, arg7, arg7)
-        ctf_integer(uint64_t, arg8, arg8)
-        ctf_integer(uint64_t, arg9, arg9)
-        ctf_integer(uint64_t, arg10, arg10)
-        ctf_integer(unsigned int, arg11, arg11)
-    )
-)
+#define _clog_12_ARGS_TRACE_ConnOutFlowStats(uniqueId, encoded_arg_string, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11)\
+tracepoint(CLOG_CUBIC_C, ConnOutFlowStats , arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11);\
+
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+#ifdef CLOG_INLINE_IMPLEMENTATION
+#include "quic.clog_cubic.c.clog.h.c"
+#endif
