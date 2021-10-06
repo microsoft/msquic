bc84aa8139fa4a1e3a3752977faa88a7cdb499c2
Thad House <thhous@microsoft.com>
2021-10-06 13:43:33 -0700

    Hopefully just 1 last fix

--- scripts/package-distribution.ps1
+++ scripts/package-distribution.ps1
@@ -140,6 +140,6 @@ foreach ($Build in $AllBuilds) {
     # For now, package only x64 Release binaries
     if ($Platform -eq "linux" -and $BuildBaseName -like "*x64_Release*") {
         Write-Output "Packaging $Build"
-        scripts/make-packages.sh  --output $DistDir
+        & $RootDir/scripts/make-packages.sh --output $DistDir
     }
 }
