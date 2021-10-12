83aff873af82d50a4d2b7145f52fe0b939a290e7
Nick Banks <nibanks@microsoft.com>
2021-10-12 09:19:24 -0400

    Initial RX

--- src/platform/datapath_dpdk.c
+++ src/platform/datapath_dpdk.c
@@ -68,6 +68,55 @@ CxPlatDataPathRecvDataToRecvPacket(
 #define MBUF_CACHE_SIZE 250
 #define BURST_SIZE 32
 
+static uint16_t
+CxPlatDataPathRx(uint16_t port, uint16_t qidx,
+        struct rte_mbuf **pkts, uint16_t nb_pkts,
+        uint16_t max_pkts, void *_)
+{
+    CXPLAT_DATAPATH* Datapath = (CXPLAT_DATAPATH*)_;
+    if (nb_pkts != 0) {
+        printf("[%p] RX: %u\n", Datapath, nb_pkts);
+        for (uint16_t i = 0; i < nb_pkts; i++) {
+            printf("  PktLen  = %u\n", pkts[i]->pkt_len);
+        }
+    }
+    return nb_pkts;
+}
+
+static void
+lcore_main(void)
+{
+	uint16_t port;
+
+	RTE_ETH_FOREACH_DEV(port)
+		if (rte_eth_dev_socket_id(port) > 0 &&
+				rte_eth_dev_socket_id(port) !=
+						(int)rte_socket_id())
+			printf("WARNING, port %u is on remote NUMA node to "
+					"polling thread.\n\tPerformance will "
+					"not be optimal.\n", port);
+
+	printf("\nCore %u forwarding packets. [Ctrl+C to quit]\n",
+			rte_lcore_id());
+	for (;;) {
+		RTE_ETH_FOREACH_DEV(port) {
+			struct rte_mbuf *bufs[BURST_SIZE];
+			const uint16_t nb_rx = rte_eth_rx_burst(port, 0,
+					bufs, BURST_SIZE);
+			if (unlikely(nb_rx == 0))
+				continue;
+			/*const uint16_t nb_tx = rte_eth_tx_burst(port ^ 1, 0,
+					bufs, nb_rx);
+			if (unlikely(nb_tx < nb_rx)) {
+				uint16_t buf;
+
+				for (buf = nb_tx; buf < nb_rx; buf++)
+					rte_pktmbuf_free(bufs[buf]);
+			}*/
+		}
+	}
+}
+
 _IRQL_requires_max_(PASSIVE_LEVEL)
 QUIC_STATUS
 CxPlatDataPathInitialize(
@@ -98,7 +147,6 @@ CxPlatDataPathInitialize(
         },
     };
 	const uint16_t rx_rings = 1, tx_rings = 1;
-    uint16_t NumPorts;
     struct rte_eth_dev_info DeviceInfo;
 	struct rte_eth_rxconf rxconf;
 	struct rte_eth_txconf txconf;
@@ -116,7 +164,8 @@ CxPlatDataPathInitialize(
         goto Error;
     }
 
-    ret = rte_eal_init(ARRAYSIZE(argv), argv);
+    printf("Calling rte_eal_init...\n");
+    ret = rte_eal_init(ARRAYSIZE(argv), (char**)argv);
     if (ret < 0) {
         QuicTraceEvent(
             LibraryErrorStatus,
@@ -128,21 +177,11 @@ CxPlatDataPathInitialize(
     }
     CleanUpRte = TRUE;
 
-    NumPorts = rte_eth_dev_count_avail();
-	if (NumPorts < 2 || (NumPorts & 1)) {
-        QuicTraceEvent(
-            LibraryErrorStatus,
-            "[ lib] ERROR, %u, %s.",
-            NumPorts,
-            "rte_eth_dev_count_avail");
-        Status = QUIC_STATUS_INTERNAL_ERROR;
-        goto Error;
-    }
-
 	(*NewDataPath)->MemoryPool = rte_pktmbuf_pool_create("MBUF_POOL",
-		NUM_MBUFS * NumPorts, MBUF_CACHE_SIZE, 0,
+		NUM_MBUFS, MBUF_CACHE_SIZE, 0,
 		RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());
     if ((*NewDataPath)->MemoryPool == NULL) {
+        printf("rte_pktmbuf_pool_create failed\n");
         QuicTraceEvent(
             LibraryErrorStatus,
             "[ lib] ERROR, %u, %s.",
@@ -154,6 +193,7 @@ CxPlatDataPathInitialize(
 
     ret = rte_eth_dev_get_port_by_name(DeviceName, &Port);
     if (ret < 0) {
+        printf("rte_eth_dev_count_avail failed: %d\n", ret);
         QuicTraceEvent(
             LibraryErrorStatus,
             "[ lib] ERROR, %u, %s.",
@@ -166,6 +206,7 @@ CxPlatDataPathInitialize(
 
     ret = rte_eth_dev_info_get(Port, &DeviceInfo);
     if (ret < 0) {
+        printf("rte_eth_dev_info_get failed: %d\n", ret);
         QuicTraceEvent(
             LibraryErrorStatus,
             "[ lib] ERROR, %u, %s.",
@@ -175,12 +216,9 @@ CxPlatDataPathInitialize(
         goto Error;
     }
 
-    if (DeviceInfo.tx_offload_capa & DEV_TX_OFFLOAD_MBUF_FAST_FREE) {
-        PortConfig.txmode.offloads |= DEV_TX_OFFLOAD_MBUF_FAST_FREE;
-    }
-
     ret = rte_eth_dev_configure(Port, rx_rings, tx_rings, &PortConfig);
     if (ret < 0) {
+        printf("rte_eth_dev_configure failed: %d\n", ret);
         QuicTraceEvent(
             LibraryErrorStatus,
             "[ lib] ERROR, %u, %s.",
@@ -192,6 +230,7 @@ CxPlatDataPathInitialize(
 
     ret = rte_eth_dev_adjust_nb_rx_tx_desc(Port, &nb_rxd, &nb_txd);
     if (ret < 0) {
+        printf("rte_eth_dev_adjust_nb_rx_tx_desc failed: %d\n", ret);
         QuicTraceEvent(
             LibraryErrorStatus,
             "[ lib] ERROR, %u, %s.",
@@ -204,9 +243,9 @@ CxPlatDataPathInitialize(
 	rxconf = DeviceInfo.default_rxconf;
 
 	for (uint16_t q = 0; q < rx_rings; q++) {
-		ret = rte_eth_rx_queue_setup(Port, q, nb_rxd,
-			rte_eth_dev_socket_id(Port), &rxconf, (*NewDataPath)->MemoryPool);
+		ret = rte_eth_rx_queue_setup(Port, q, nb_rxd, rte_eth_dev_socket_id(Port), &rxconf, (*NewDataPath)->MemoryPool);
         if (ret < 0) {
+            printf("rte_eth_rx_queue_setup failed: %d\n", ret);
             QuicTraceEvent(
                 LibraryErrorStatus,
                 "[ lib] ERROR, %u, %s.",
@@ -220,9 +259,9 @@ CxPlatDataPathInitialize(
 	txconf = DeviceInfo.default_txconf;
 	txconf.offloads = PortConfig.txmode.offloads;
 	for (uint16_t q = 0; q < tx_rings; q++) {
-		ret = rte_eth_tx_queue_setup(Port, q, nb_txd,
-				rte_eth_dev_socket_id(Port), &txconf);
+		ret = rte_eth_tx_queue_setup(Port, q, nb_txd, rte_eth_dev_socket_id(Port), &txconf);
         if (ret < 0) {
+            printf("rte_eth_tx_queue_setup failed: %d\n", ret);
             QuicTraceEvent(
                 LibraryErrorStatus,
                 "[ lib] ERROR, %u, %s.",
@@ -235,6 +274,7 @@ CxPlatDataPathInitialize(
 
 	ret  = rte_eth_dev_start(Port);
     if (ret < 0) {
+        printf("rte_eth_dev_start failed: %d\n", ret);
         QuicTraceEvent(
             LibraryErrorStatus,
             "[ lib] ERROR, %u, %s.",
@@ -243,6 +283,43 @@ CxPlatDataPathInitialize(
         Status = QUIC_STATUS_INTERNAL_ERROR;
         goto Error;
     }
+
+    {
+        struct rte_ether_addr addr;
+        ret = rte_eth_macaddr_get(Port, &addr);
+        if (ret < 0) {
+            printf("rte_eth_macaddr_get failed: %d\n", ret);
+            QuicTraceEvent(
+                LibraryErrorStatus,
+                "[ lib] ERROR, %u, %s.",
+                ret,
+                "rte_eth_macaddr_get");
+            Status = QUIC_STATUS_INTERNAL_ERROR;
+            goto Error;
+        }
+        printf("Port %u MAC: %02"PRIx8" %02"PRIx8" %02"PRIx8
+                " %02"PRIx8" %02"PRIx8" %02"PRIx8"\n",
+                (unsigned)Port,
+                addr.addr_bytes[0], addr.addr_bytes[1],
+                addr.addr_bytes[2], addr.addr_bytes[3],
+                addr.addr_bytes[4], addr.addr_bytes[5]);
+    }
+
+    ret = rte_eth_promiscuous_enable(Port);
+    if (ret < 0) {
+        printf("rte_eth_promiscuous_enable failed: %d\n", ret);
+        QuicTraceEvent(
+            LibraryErrorStatus,
+            "[ lib] ERROR, %u, %s.",
+            ret,
+            "rte_eth_promiscuous_enable");
+        Status = QUIC_STATUS_INTERNAL_ERROR;
+        goto Error;
+    }
+
+	rte_eth_add_rx_callback(Port, 0, CxPlatDataPathRx, *NewDataPath);
+
+    lcore_main();
 
 Error:
 
