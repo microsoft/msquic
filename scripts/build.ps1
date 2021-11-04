b0f11a788192c05074d47e6b61c4f07c0665f662
Thad House <thhous@microsoft.com>
2021-11-04 10:58:18 -0700

    Maybe a better try

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -213,7 +213,8 @@ if ($Generator -eq "") {
             Install-Module VSSetup -Scope CurrentUser -Force -SkipPublisherCheck
         }
         Get-VSSetupInstance | Select-Object *
-        Get-VSSetupInstance | Select-Object -ExpandProperty Packages
+        #Get-VSSetupInstance | Select-Object -ExpandProperty Packages
+        Get-VSSetupInstance | Select-VSSetupInstance -Latest -Require Microsoft.VisualStudio.Component.VC.Tools.x86.x64
         $VsVersion = Get-VSSetupInstance | Select-VSSetupInstance -Latest -Require Microsoft.VisualStudio.Component.VC.Tools.x86.x64 | Select-Object -ExpandProperty DisplayName
         if ($VsVersion.Contains("2022")) {
             $Generator = "Visual Studio 17 2022"
