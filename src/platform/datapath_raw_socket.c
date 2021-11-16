5e46fad963a3dc2cf6005a6f6fc508d95d62a0d4
Nick Banks <nibanks@microsoft.com>
2021-11-16 11:06:25 -0500

    Update comment for xplat

--- src/platform/datapath_raw_socket.c
+++ src/platform/datapath_raw_socket.c
@@ -105,9 +105,9 @@ CxPlatTryAddSocket(
     QUIC_ADDR MappedAddress = {0};
 
     //
-    // Get (and reserve) a transport layer port from tcpip.sys by binding an
-    // auxiliary (dual stack) socket, and binding and connecting the socket to
-    // the desired addresses.
+    // Get (and reserve) a transport layer port from the OS networking stack by
+    // binding an auxiliary (dual stack) socket, and binding and connecting the
+    // socket to the desired addresses.
     //
 
     Socket->AuxSocket =
