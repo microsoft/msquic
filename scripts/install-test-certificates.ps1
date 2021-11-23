e71ee75c633b662fc09d9c7048f66e9729a37be5
Thad House <87844845+thhous-msft@users.noreply.github.com>
2021-11-23 14:12:08 -0800

    Fix tests on downlevel systems failing at end of test (#2177)
    * Try to debug tests failing at end
    
    * Disable logging
    
    * More printing
    
    * Smore
    
    * Another test
    
    * Final fix
    
    * Only 0 out last exit code once

--- scripts/install-test-certificates.ps1
+++ scripts/install-test-certificates.ps1
@@ -15,6 +15,10 @@ param (
     [Parameter(Mandatory = $true)]
     [string]$OutputFile = ""
 )
+
+Set-StrictMode -Version 'Latest'
+$PSDefaultParameterValues['*:ErrorAction'] = 'Stop'
+
 [System.DateTimeOffset]$NotBefore = [System.DateTimeOffset]::Now.AddDays(-1)
 [System.DateTimeOffset]$NotAfter = [System.DateTimeOffset]::Now.AddDays(365)
 
@@ -174,7 +178,7 @@ $Leaf = CreateLeafCert $IntermediateSigner
 
 $Collection = [System.Security.Cryptography.X509Certificates.X509Certificate2Collection]::new()
 
-$Collection.Add($Leaf[1]) # TODO(AnRossi):Why is $Leaf an array of an Int and the Cert?
+$Collection.Add($Leaf[1]) | Out-Null # TODO(AnRossi):Why is $Leaf an array of an Int and the Cert?
 #Export the intermediate and root certs and then import them to drop their private keys.
 $Collection.Import($IntermediateSigner.rawData)
 $Collection.Import($Root.rawData)
