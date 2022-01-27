0545a197ee69595086f497c1fdc04440e210d5f9
Yi Huang <csujedi@icloud.com>
2022-01-26 23:16:54 -0800

    larger!

--- src/core/quicdef.h
+++ src/core/quicdef.h
@@ -196,7 +196,7 @@ typedef struct QUIC_PATH QUIC_PATH;
 //
 #define QUIC_MAX_RECEIVE_QUEUE_COUNT            1024
 #else
-#define QUIC_MAX_RECEIVE_QUEUE_COUNT            2048
+#define QUIC_MAX_RECEIVE_QUEUE_COUNT            4096
 #endif
 
 //
