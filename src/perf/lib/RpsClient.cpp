microsoft/msquic

commit 47ee814b4dc0d113d983f9bc71222ba4025c2825

Remove Level from SetParam/GetParam (#2322)
* Remove Level from SetParam/GetParam

* Fix CLOG

* Fix param API test case

* Fix rust param level

* Fix interop IDs

* Update spinquic getparam calls

* Fix spinquic setparam

* Fix Linux spinquic

* Fix issue

* More fixes

* Fix clog and sidecar

Co-authored-by: Thad House <thhous@microsoft.com>

src/perf/lib/RpsClient.cpp
@@ -222,7 +222,6 @@ RpsClient::Start(
         Status =
             MsQuic->SetParam(
                 Connections[i],
-                QUIC_PARAM_LEVEL_CONNECTION,
                 QUIC_PARAM_CONN_SHARE_UDP_BINDING,
                 sizeof(Opt),
                 &Opt);
@@ -235,7 +234,6 @@ RpsClient::Start(
             Status =
                 MsQuic->SetParam(
                     Connections[i],
-                    QUIC_PARAM_LEVEL_CONNECTION,
                     QUIC_PARAM_CONN_LOCAL_ADDRESS,
                     sizeof(QUIC_ADDR),
                     &LocalAddresses[i % RPS_MAX_CLIENT_PORT_COUNT]);
@@ -262,7 +260,6 @@ RpsClient::Start(
             Status =
                 MsQuic->GetParam(
                     Connections[i],
-                    QUIC_PARAM_LEVEL_CONNECTION,
                     QUIC_PARAM_CONN_LOCAL_ADDRESS,
                     &AddrLen,
                     &LocalAddresses[i]);

src/core/api.c
@@ -1289,16 +1289,13 @@ Exit:
     return Status;
 }
 
-#define QUIC_PARAM_GENERATOR(Level, Value) (((Level + 1) & 0x3F) << 26 | (Value & 0x3FFFFFF))
-
 _IRQL_requires_max_(PASSIVE_LEVEL)
 QUIC_STATUS
 QUIC_API
 MsQuicSetParam(
-    _When_(Level == QUIC_PARAM_LEVEL_GLOBAL, _Reserved_)
-    _When_(Level != QUIC_PARAM_LEVEL_GLOBAL, _In_ _Pre_defensive_)
+    _When_(QUIC_PARAM_IS_GLOBAL(Param), _Reserved_)
+    _When_(!QUIC_PARAM_IS_GLOBAL(Param), _In_ _Pre_defensive_)
         HQUIC Handle,
-    _In_ _Pre_defensive_ QUIC_PARAM_LEVEL Level,
     _In_ uint32_t Param,
     _In_ uint32_t BufferLength,
     _In_reads_bytes_(BufferLength)
@@ -1307,26 +1304,11 @@ MsQuicSetParam(
 {
     CXPLAT_PASSIVE_CODE();
 
-    if ((Param & 0xFC000000) != 0) {
+    if ((Handle == NULL) ^ QUIC_PARAM_IS_GLOBAL(Param)) {
         //
-        // Has level embedded parameter. Validate matches passed in level.
+        // Ensure global parameters don't have a handle passed in, and vice
+        // versa.
         //
-        QUIC_PARAM_LEVEL ParamContainedLevel = ((Param >> 26) & 0x3F) - 1;
-        if (ParamContainedLevel != Level) {
-            QuicTraceEvent(
-                LibraryError,
-                "[ lib] ERROR, %s.",
-                "Param level does not match param value");
-            return QUIC_STATUS_INVALID_PARAMETER;
-        }
-    } else {
-        //
-        // Missing level embedded parameter. Inject level into parameter.
-        //
-        Param = QUIC_PARAM_GENERATOR(Level, Param);
-    }
-
-    if ((Handle == NULL) ^ (Level == QUIC_PARAM_LEVEL_GLOBAL)) {
         return QUIC_STATUS_INVALID_PARAMETER;
     }
 
@@ -1338,7 +1320,7 @@ MsQuicSetParam(
 
     QUIC_STATUS Status;
 
-    if (Level == QUIC_PARAM_LEVEL_GLOBAL) {
+    if (QUIC_PARAM_IS_GLOBAL(Param)) {
         //
         // Global parameters are processed inline.
         //
@@ -1352,7 +1334,7 @@ MsQuicSetParam(
         //
         // Registration, Configuration and Listener parameters are processed inline.
         //
-        Status = QuicLibrarySetParam(Handle, Level, Param, BufferLength, Buffer);
+        Status = QuicLibrarySetParam(Handle, Param, BufferLength, Buffer);
         goto Error;
     }
 
@@ -1381,7 +1363,7 @@ MsQuicSetParam(
         if (!AlreadyInline) {
             Connection->State.InlineApiExecution = TRUE;
         }
-        Status = QuicLibrarySetParam(Handle, Level, Param, BufferLength, Buffer);
+        Status = QuicLibrarySetParam(Handle, Param, BufferLength, Buffer);
         if (!AlreadyInline) {
             Connection->State.InlineApiExecution = FALSE;
         }
@@ -1402,7 +1384,6 @@ MsQuicSetParam(
     ApiCtx.Completed = &CompletionEvent;
     ApiCtx.Status = &Status;
     ApiCtx.SET_PARAM.Handle = Handle;
-    ApiCtx.SET_PARAM.Level = Level;
     ApiCtx.SET_PARAM.Param = Param;
     ApiCtx.SET_PARAM.BufferLength = BufferLength;
     ApiCtx.SET_PARAM.Buffer = Buffer;
@@ -1431,10 +1412,9 @@ _IRQL_requires_max_(PASSIVE_LEVEL)
 QUIC_STATUS
 QUIC_API
 MsQuicGetParam(
-    _When_(Level == QUIC_PARAM_LEVEL_GLOBAL, _Reserved_)
-    _When_(Level != QUIC_PARAM_LEVEL_GLOBAL, _In_ _Pre_defensive_)
+    _When_(QUIC_PARAM_IS_GLOBAL(Param), _Reserved_)
+    _When_(!QUIC_PARAM_IS_GLOBAL(Param), _In_ _Pre_defensive_)
         HQUIC Handle,
-    _In_ _Pre_defensive_ QUIC_PARAM_LEVEL Level,
     _In_ uint32_t Param,
     _Inout_ _Pre_defensive_ uint32_t* BufferLength,
     _Out_writes_bytes_opt_(*BufferLength)
@@ -1443,27 +1423,12 @@ MsQuicGetParam(
 {
     CXPLAT_PASSIVE_CODE();
 
-    if ((Param & 0xFC000000) != 0) {
-        //
-        // Has level embedded parameter. Validate matches passed in level.
-        //
-        QUIC_PARAM_LEVEL ParamContainedLevel = ((Param >> 26) & 0x3F) - 1;
-        if (ParamContainedLevel != Level) {
-            QuicTraceEvent(
-                LibraryError,
-                "[ lib] ERROR, %s.",
-                "Param level does not match param value");
-            return QUIC_STATUS_INVALID_PARAMETER;
-        }
-    } else {
-        //
-        // Missing level embedded parameter. Inject level into parameter.
-        //
-        Param = QUIC_PARAM_GENERATOR(Level, Param);
-    }
-
-    if (((Handle == NULL) ^ (Level == QUIC_PARAM_LEVEL_GLOBAL)) ||
+    if ((Handle == NULL) ^ QUIC_PARAM_IS_GLOBAL(Param) ||
         BufferLength == NULL) {
+        //
+        // Ensure global parameters don't have a handle passed in, and vice
+        // versa.
+        //
         return QUIC_STATUS_INVALID_PARAMETER;
     }
 
@@ -1475,7 +1440,7 @@ MsQuicGetParam(
         QUIC_TRACE_API_GET_PARAM,
         Handle);
 
-    if (Level == QUIC_PARAM_LEVEL_GLOBAL) {
+    if (QUIC_PARAM_IS_GLOBAL(Param)) {
         //
         // Global parameters are processed inline.
         //
@@ -1489,7 +1454,7 @@ MsQuicGetParam(
         //
         // Registration, Configuration and Listener parameters are processed inline.
         //
-        Status = QuicLibraryGetParam(Handle, Level, Param, BufferLength, Buffer);
+        Status = QuicLibraryGetParam(Handle, Param, BufferLength, Buffer);
         goto Error;
     }
 
@@ -1518,7 +1483,7 @@ MsQuicGetParam(
         if (!AlreadyInline) {
             Connection->State.InlineApiExecution = TRUE;
         }
-        Status = QuicLibraryGetParam(Handle, Level, Param, BufferLength, Buffer);
+        Status = QuicLibraryGetParam(Handle, Param, BufferLength, Buffer);
         if (!AlreadyInline) {
             Connection->State.InlineApiExecution = FALSE;
         }
@@ -1539,7 +1504,6 @@ MsQuicGetParam(
     ApiCtx.Completed = &CompletionEvent;
     ApiCtx.Status = &Status;
     ApiCtx.GET_PARAM.Handle = Handle;
-    ApiCtx.GET_PARAM.Level = Level;
     ApiCtx.GET_PARAM.Param = Param;
     ApiCtx.GET_PARAM.BufferLength = BufferLength;
     ApiCtx.GET_PARAM.Buffer = Buffer;

src/core/api.h
@@ -228,10 +228,9 @@ _IRQL_requires_max_(PASSIVE_LEVEL)
 QUIC_STATUS
 QUIC_API
 MsQuicSetParam(
-    _When_(Level == QUIC_PARAM_LEVEL_GLOBAL, _Reserved_)
-    _When_(Level != QUIC_PARAM_LEVEL_GLOBAL, _In_ _Pre_defensive_)
+    _When_(QUIC_PARAM_IS_GLOBAL(Param), _Reserved_)
+    _When_(!QUIC_PARAM_IS_GLOBAL(Param), _In_ _Pre_defensive_)
         HQUIC Handle,
-    _In_ _Pre_defensive_ QUIC_PARAM_LEVEL Level,
     _In_ uint32_t Param,
     _In_ uint32_t BufferLength,
     _In_reads_bytes_(BufferLength)
@@ -242,10 +241,9 @@ _IRQL_requires_max_(PASSIVE_LEVEL)
 QUIC_STATUS
 QUIC_API
 MsQuicGetParam(
-    _When_(Level == QUIC_PARAM_LEVEL_GLOBAL, _Reserved_)
-    _When_(Level != QUIC_PARAM_LEVEL_GLOBAL, _In_ _Pre_defensive_)
+    _When_(QUIC_PARAM_IS_GLOBAL(Param), _Reserved_)
+    _When_(!QUIC_PARAM_IS_GLOBAL(Param), _In_ _Pre_defensive_)
         HQUIC Handle,
-    _In_ _Pre_defensive_ QUIC_PARAM_LEVEL Level,
     _In_ uint32_t Param,
     _Inout_ _Pre_defensive_ uint32_t* BufferLength,
     _Out_writes_bytes_opt_(*BufferLength)

src/core/connection.c
@@ -6802,7 +6802,6 @@ QuicConnProcessApiOperation(
         Status =
             QuicLibrarySetParam(
                 ApiCtx->SET_PARAM.Handle,
-                ApiCtx->SET_PARAM.Level,
                 ApiCtx->SET_PARAM.Param,
                 ApiCtx->SET_PARAM.BufferLength,
                 ApiCtx->SET_PARAM.Buffer);
@@ -6812,7 +6811,6 @@ QuicConnProcessApiOperation(
         Status =
             QuicLibraryGetParam(
                 ApiCtx->GET_PARAM.Handle,
-                ApiCtx->GET_PARAM.Level,
                 ApiCtx->GET_PARAM.Param,
                 ApiCtx->GET_PARAM.BufferLength,
                 ApiCtx->GET_PARAM.Buffer);

src/core/library.c
@@ -1027,7 +1027,6 @@ _IRQL_requires_max_(PASSIVE_LEVEL)
 QUIC_STATUS
 QuicLibrarySetParam(
     _In_ HQUIC Handle,
-    _In_ QUIC_PARAM_LEVEL Level,
     _In_ uint32_t Param,
     _In_ uint32_t BufferLength,
     _In_reads_bytes_(BufferLength)
@@ -1095,9 +1094,9 @@ QuicLibrarySetParam(
         goto Error;
     }
 
-    switch (Level)
+    switch (Param & 0x7F000000)
     {
-    case QUIC_PARAM_LEVEL_REGISTRATION:
+    case QUIC_PARAM_PREFIX_REGISTRATION:
         if (Registration == NULL) {
             Status = QUIC_STATUS_INVALID_PARAMETER;
         } else {
@@ -1105,7 +1104,7 @@ QuicLibrarySetParam(
         }
         break;
 
-    case QUIC_PARAM_LEVEL_CONFIGURATION:
+    case QUIC_PARAM_PREFIX_CONFIGURATION:
         if (Configuration == NULL) {
             Status = QUIC_STATUS_INVALID_PARAMETER;
         } else {
@@ -1113,7 +1112,7 @@ QuicLibrarySetParam(
         }
         break;
 
-    case QUIC_PARAM_LEVEL_LISTENER:
+    case QUIC_PARAM_PREFIX_LISTENER:
         if (Listener == NULL) {
             Status = QUIC_STATUS_INVALID_PARAMETER;
         } else {
@@ -1121,7 +1120,7 @@ QuicLibrarySetParam(
         }
         break;
 
-    case QUIC_PARAM_LEVEL_CONNECTION:
+    case QUIC_PARAM_PREFIX_CONNECTION:
         if (Connection == NULL) {
             Status = QUIC_STATUS_INVALID_PARAMETER;
         } else {
@@ -1129,7 +1128,8 @@ QuicLibrarySetParam(
         }
         break;
 
-    case QUIC_PARAM_LEVEL_TLS:
+    case QUIC_PARAM_PREFIX_TLS:
+    case QUIC_PARAM_PREFIX_TLS_SCHANNEL:
         if (Connection == NULL || Connection->Crypto.TLS == NULL) {
             Status = QUIC_STATUS_INVALID_PARAMETER;
         } else {
@@ -1137,7 +1137,7 @@ QuicLibrarySetParam(
         }
         break;
 
-    case QUIC_PARAM_LEVEL_STREAM:
+    case QUIC_PARAM_PREFIX_STREAM:
         if (Stream == NULL) {
             Status = QUIC_STATUS_INVALID_PARAMETER;
         } else {
@@ -1159,7 +1159,6 @@ _IRQL_requires_max_(PASSIVE_LEVEL)
 QUIC_STATUS
 QuicLibraryGetParam(
     _In_ HQUIC Handle,
-    _In_ QUIC_PARAM_LEVEL Level,
     _In_ uint32_t Param,
     _Inout_ uint32_t* BufferLength,
     _Out_writes_bytes_opt_(*BufferLength)
@@ -1229,9 +1228,9 @@ QuicLibraryGetParam(
         goto Error;
     }
 
-    switch (Level)
+    switch (Param & 0x7F000000)
     {
-    case QUIC_PARAM_LEVEL_REGISTRATION:
+    case QUIC_PARAM_PREFIX_REGISTRATION:
         if (Registration == NULL) {
             Status = QUIC_STATUS_INVALID_PARAMETER;
         } else {
@@ -1239,7 +1238,7 @@ QuicLibraryGetParam(
         }
         break;
 
-    case QUIC_PARAM_LEVEL_CONFIGURATION:
+    case QUIC_PARAM_PREFIX_CONFIGURATION:
         if (Configuration == NULL) {
             Status = QUIC_STATUS_INVALID_PARAMETER;
         } else {
@@ -1247,7 +1246,7 @@ QuicLibraryGetParam(
         }
         break;
 
-    case QUIC_PARAM_LEVEL_LISTENER:
+    case QUIC_PARAM_PREFIX_LISTENER:
         if (Listener == NULL) {
             Status = QUIC_STATUS_INVALID_PARAMETER;
         } else {
@@ -1255,7 +1254,7 @@ QuicLibraryGetParam(
         }
         break;
 
-    case QUIC_PARAM_LEVEL_CONNECTION:
+    case QUIC_PARAM_PREFIX_CONNECTION:
         if (Connection == NULL) {
             Status = QUIC_STATUS_INVALID_PARAMETER;
         } else {
@@ -1263,7 +1262,8 @@ QuicLibraryGetParam(
         }
         break;
 
-    case QUIC_PARAM_LEVEL_TLS:
+    case QUIC_PARAM_PREFIX_TLS:
+    case QUIC_PARAM_PREFIX_TLS_SCHANNEL:
         if (Connection == NULL || Connection->Crypto.TLS == NULL) {
             Status = QUIC_STATUS_INVALID_PARAMETER;
         } else {
@@ -1271,7 +1271,7 @@ QuicLibraryGetParam(
         }
         break;
 
-    case QUIC_PARAM_LEVEL_STREAM:
+    case QUIC_PARAM_PREFIX_STREAM:
         if (Stream == NULL) {
             Status = QUIC_STATUS_INVALID_PARAMETER;
         } else {

src/core/library.h
@@ -474,7 +474,6 @@ _IRQL_requires_max_(PASSIVE_LEVEL)
 QUIC_STATUS
 QuicLibrarySetParam(
     _In_ HQUIC Handle,
-    _In_ QUIC_PARAM_LEVEL Level,
     _In_ uint32_t Param,
     _In_ uint32_t BufferLength,
     _In_reads_bytes_(BufferLength)
@@ -485,7 +484,6 @@ _IRQL_requires_max_(PASSIVE_LEVEL)
 QUIC_STATUS
 QuicLibraryGetParam(
     _In_ HQUIC Handle,
-    _In_ QUIC_PARAM_LEVEL Level,
     _In_ uint32_t Param,
     _Inout_ uint32_t* BufferLength,
     _Out_writes_bytes_opt_(*BufferLength)

src/core/operation.h
@@ -145,14 +145,12 @@ typedef struct QUIC_API_CONTEXT {
 
         struct {
             HQUIC Handle;
-            uint32_t Level;
             uint32_t Param;
             uint32_t BufferLength;
             const void* Buffer;
         } SET_PARAM;
         struct {
             HQUIC Handle;
-            uint32_t Level;
             uint32_t Param;
             uint32_t* BufferLength;
             void* Buffer;

src/cs/lib/msquic_generated.cs
@@ -1375,17 +1375,6 @@ namespace Microsoft.Quic
         }
     }
 
-    public enum QUIC_PARAM_LEVEL
-    {
-        QUIC_PARAM_LEVEL_GLOBAL,
-        QUIC_PARAM_LEVEL_REGISTRATION,
-        QUIC_PARAM_LEVEL_CONFIGURATION,
-        QUIC_PARAM_LEVEL_LISTENER,
-        QUIC_PARAM_LEVEL_CONNECTION,
-        QUIC_PARAM_LEVEL_TLS,
-        QUIC_PARAM_LEVEL_STREAM,
-    }
-
     public unsafe partial struct QUIC_SCHANNEL_CONTEXT_ATTRIBUTE_W
     {
         [NativeTypeName("unsigned long")]
@@ -2065,10 +2054,10 @@ namespace Microsoft.Quic
         public delegate* unmanaged[Cdecl]<QUIC_HANDLE*, void*, void*, void> SetCallbackHandler;
 
         [NativeTypeName("QUIC_SET_PARAM_FN")]
-        public delegate* unmanaged[Cdecl]<QUIC_HANDLE*, QUIC_PARAM_LEVEL, uint, uint, void*, int> SetParam;
+        public delegate* unmanaged[Cdecl]<QUIC_HANDLE*, uint, uint, void*, int> SetParam;
 
         [NativeTypeName("QUIC_GET_PARAM_FN")]
-        public delegate* unmanaged[Cdecl]<QUIC_HANDLE*, QUIC_PARAM_LEVEL, uint, uint*, void*, int> GetParam;
+        public delegate* unmanaged[Cdecl]<QUIC_HANDLE*, uint, uint*, void*, int> GetParam;
 
         [NativeTypeName("QUIC_REGISTRATION_OPEN_FN")]
         public delegate* unmanaged[Cdecl]<QUIC_REGISTRATION_CONFIG*, QUIC_HANDLE**, int> RegistrationOpen;
@@ -2164,109 +2153,118 @@ namespace Microsoft.Quic
         [NativeTypeName("#define QUIC_MAX_TICKET_KEY_COUNT 16")]
         public const int QUIC_MAX_TICKET_KEY_COUNT = 16;
 
-        [NativeTypeName("#define QUIC_PARAM_GLOBAL_RETRY_MEMORY_PERCENT 0x4000000")]
-        public const int QUIC_PARAM_GLOBAL_RETRY_MEMORY_PERCENT = 0x4000000;
+        [NativeTypeName("#define QUIC_PARAM_GLOBAL_RETRY_MEMORY_PERCENT 0x01000000")]
+        public const int QUIC_PARAM_GLOBAL_RETRY_MEMORY_PERCENT = 0x01000000;
 
-        [NativeTypeName("#define QUIC_PARAM_GLOBAL_SUPPORTED_VERSIONS 0x4000001")]
-        public const int QUIC_PARAM_GLOBAL_SUPPORTED_VERSIONS = 0x4000001;
+        [NativeTypeName("#define QUIC_PARAM_GLOBAL_SUPPORTED_VERSIONS 0x01000001")]
+        public const int QUIC_PARAM_GLOBAL_SUPPORTED_VERSIONS = 0x01000001;
 
-        [NativeTypeName("#define QUIC_PARAM_GLOBAL_LOAD_BALACING_MODE 0x4000002")]
-        public const int QUIC_PARAM_GLOBAL_LOAD_BALACING_MODE = 0x4000002;
+        [NativeTypeName("#define QUIC_PARAM_GLOBAL_LOAD_BALACING_MODE 0x01000002")]
+        public const int QUIC_PARAM_GLOBAL_LOAD_BALACING_MODE = 0x01000002;
 
-        [NativeTypeName("#define QUIC_PARAM_GLOBAL_PERF_COUNTERS 0x4000003")]
-        public const int QUIC_PARAM_GLOBAL_PERF_COUNTERS = 0x4000003;
+        [NativeTypeName("#define QUIC_PARAM_GLOBAL_PERF_COUNTERS 0x01000003")]
+        public const int QUIC_PARAM_GLOBAL_PERF_COUNTERS = 0x01000003;
 
-        [NativeTypeName("#define QUIC_PARAM_GLOBAL_SETTINGS 0x4000004")]
-        public const int QUIC_PARAM_GLOBAL_SETTINGS = 0x4000004;
+        [NativeTypeName("#define QUIC_PARAM_GLOBAL_SETTINGS 0x01000004")]
+        public const int QUIC_PARAM_GLOBAL_SETTINGS = 0x01000004;
 
-        [NativeTypeName("#define QUIC_PARAM_GLOBAL_VERSION 0x4000005")]
-        public const int QUIC_PARAM_GLOBAL_VERSION = 0x4000005;
+        [NativeTypeName("#define QUIC_PARAM_GLOBAL_VERSION 0x01000005")]
+        public const int QUIC_PARAM_GLOBAL_VERSION = 0x01000005;
 
-        [NativeTypeName("#define QUIC_PARAM_REGISTRATION_CID_PREFIX 0x8000000")]
-        public const int QUIC_PARAM_REGISTRATION_CID_PREFIX = 0x8000000;
+        [NativeTypeName("#define QUIC_PARAM_REGISTRATION_CID_PREFIX 0x02000000")]
+        public const int QUIC_PARAM_REGISTRATION_CID_PREFIX = 0x02000000;
 
-        [NativeTypeName("#define QUIC_PARAM_CONFIGURATION_SETTINGS 0xC000000")]
-        public const int QUIC_PARAM_CONFIGURATION_SETTINGS = 0xC000000;
+        [NativeTypeName("#define QUIC_PARAM_CONFIGURATION_SETTINGS 0x03000000")]
+        public const int QUIC_PARAM_CONFIGURATION_SETTINGS = 0x03000000;
 
-        [NativeTypeName("#define QUIC_PARAM_CONFIGURATION_TICKET_KEYS 0xC000001")]
-        public const int QUIC_PARAM_CONFIGURATION_TICKET_KEYS = 0xC000001;
+        [NativeTypeName("#define QUIC_PARAM_CONFIGURATION_TICKET_KEYS 0x03000001")]
+        public const int QUIC_PARAM_CONFIGURATION_TICKET_KEYS = 0x03000001;
 
-        [NativeTypeName("#define QUIC_PARAM_LISTENER_LOCAL_ADDRESS 0x10000000")]
-        public const int QUIC_PARAM_LISTENER_LOCAL_ADDRESS = 0x10000000;
+        [NativeTypeName("#define QUIC_PARAM_LISTENER_LOCAL_ADDRESS 0x04000000")]
+        public const int QUIC_PARAM_LISTENER_LOCAL_ADDRESS = 0x04000000;
 
-        [NativeTypeName("#define QUIC_PARAM_LISTENER_STATS 0x10000001")]
-        public const int QUIC_PARAM_LISTENER_STATS = 0x10000001;
+        [NativeTypeName("#define QUIC_PARAM_LISTENER_STATS 0x04000001")]
+        public const int QUIC_PARAM_LISTENER_STATS = 0x04000001;
 
-        [NativeTypeName("#define QUIC_PARAM_CONN_QUIC_VERSION 0x14000000")]
-        public const int QUIC_PARAM_CONN_QUIC_VERSION = 0x14000000;
+        [NativeTypeName("#define QUIC_PARAM_CONN_QUIC_VERSION 0x05000000")]
+        public const int QUIC_PARAM_CONN_QUIC_VERSION = 0x05000000;
 
-        [NativeTypeName("#define QUIC_PARAM_CONN_LOCAL_ADDRESS 0x14000001")]
-        public const int QUIC_PARAM_CONN_LOCAL_ADDRESS = 0x14000001;
+        [NativeTypeName("#define QUIC_PARAM_CONN_LOCAL_ADDRESS 0x05000001")]
+        public const int QUIC_PARAM_CONN_LOCAL_ADDRESS = 0x05000001;
 
-        [NativeTypeName("#define QUIC_PARAM_CONN_REMOTE_ADDRESS 0x14000002")]
-        public const int QUIC_PARAM_CONN_REMOTE_ADDRESS = 0x14000002;
+        [NativeTypeName("#define QUIC_PARAM_CONN_REMOTE_ADDRESS 0x05000002")]
+        public const int QUIC_PARAM_CONN_REMOTE_ADDRESS = 0x05000002;
 
-        [NativeTypeName("#define QUIC_PARAM_CONN_IDEAL_PROCESSOR 0x14000003")]
-        public const int QUIC_PARAM_CONN_IDEAL_PROCESSOR = 0x14000003;
+        [NativeTypeName("#define QUIC_PARAM_CONN_IDEAL_PROCESSOR 0x05000003")]
+        public const int QUIC_PARAM_CONN_IDEAL_PROCESSOR = 0x05000003;
 
-        [NativeTypeName("#define QUIC_PARAM_CONN_SETTINGS 0x14000004")]
-        public const int QUIC_PARAM_CONN_SETTINGS = 0x14000004;
+        [NativeTypeName("#define QUIC_PARAM_CONN_SETTINGS 0x05000004")]
+        public const int QUIC_PARAM_CONN_SETTINGS = 0x05000004;
 
-        [NativeTypeName("#define QUIC_PARAM_CONN_STATISTICS 0x14000005")]
-        public const int QUIC_PARAM_CONN_STATISTICS = 0x14000005;
+        [NativeTypeName("#define QUIC_PARAM_CONN_STATISTICS 0x05000005")]
+        public const int QUIC_PARAM_CONN_STATISTICS = 0x05000005;
 
-        [NativeTypeName("#define QUIC_PARAM_CONN_STATISTICS_PLAT 0x14000006")]
-        public const int QUIC_PARAM_CONN_STATISTICS_PLAT = 0x14000006;
+        [NativeTypeName("#define QUIC_PARAM_CONN_STATISTICS_PLAT 0x05000006")]
+        public const int QUIC_PARAM_CONN_STATISTICS_PLAT = 0x05000006;
 
-        [NativeTypeName("#define QUIC_PARAM_CONN_SHARE_UDP_BINDING 0x14000007")]
-        public const int QUIC_PARAM_CONN_SHARE_UDP_BINDING = 0x14000007;
+        [NativeTypeName("#define QUIC_PARAM_CONN_SHARE_UDP_BINDING 0x05000007")]
+        public const int QUIC_PARAM_CONN_SHARE_UDP_BINDING = 0x05000007;
 
-        [NativeTypeName("#define QUIC_PARAM_CONN_LOCAL_BIDI_STREAM_COUNT 0x14000008")]
-        public const int QUIC_PARAM_CONN_LOCAL_BIDI_STREAM_COUNT = 0x14000008;
+        [NativeTypeName("#define QUIC_PARAM_CONN_LOCAL_BIDI_STREAM_COUNT 0x05000008")]
+        public const int QUIC_PARAM_CONN_LOCAL_BIDI_STREAM_COUNT = 0x05000008;
 
-        [NativeTypeName("#define QUIC_PARAM_CONN_LOCAL_UNIDI_STREAM_COUNT 0x14000009")]
-        public const int QUIC_PARAM_CONN_LOCAL_UNIDI_STREAM_COUNT = 0x14000009;
+        [NativeTypeName("#define QUIC_PARAM_CONN_LOCAL_UNIDI_STREAM_COUNT 0x05000009")]
+        public const int QUIC_PARAM_CONN_LOCAL_UNIDI_STREAM_COUNT = 0x05000009;
 
-        [NativeTypeName("#define QUIC_PARAM_CONN_MAX_STREAM_IDS 0x1400000A")]
-        public const int QUIC_PARAM_CONN_MAX_STREAM_IDS = 0x1400000A;
+        [NativeTypeName("#define QUIC_PARAM_CONN_MAX_STREAM_IDS 0x0500000A")]
+        public const int QUIC_PARAM_CONN_MAX_STREAM_IDS = 0x0500000A;
 
-        [NativeTypeName("#define QUIC_PARAM_CONN_CLOSE_REASON_PHRASE 0x1400000B")]
-        public const int QUIC_PARAM_CONN_CLOSE_REASON_PHRASE = 0x1400000B;
+        [NativeTypeName("#define QUIC_PARAM_CONN_CLOSE_REASON_PHRASE 0x0500000B")]
+        public const int QUIC_PARAM_CONN_CLOSE_REASON_PHRASE = 0x0500000B;
 
-        [NativeTypeName("#define QUIC_PARAM_CONN_STREAM_SCHEDULING_SCHEME 0x1400000C")]
-        public const int QUIC_PARAM_CONN_STREAM_SCHEDULING_SCHEME = 0x1400000C;
+        [NativeTypeName("#define QUIC_PARAM_CONN_STREAM_SCHEDULING_SCHEME 0x0500000C")]
+        public const int QUIC_PARAM_CONN_STREAM_SCHEDULING_SCHEME = 0x0500000C;
 
-        [NativeTypeName("#define QUIC_PARAM_CONN_DATAGRAM_RECEIVE_ENABLED 0x1400000D")]
-        public const int QUIC_PARAM_CONN_DATAGRAM_RECEIVE_ENABLED = 0x1400000D;
+        [NativeTypeName("#define QUIC_PARAM_CONN_DATAGRAM_RECEIVE_ENABLED 0x0500000D")]
+        public const int QUIC_PARAM_CONN_DATAGRAM_RECEIVE_ENABLED = 0x0500000D;
 
-        [NativeTypeName("#define QUIC_PARAM_CONN_DATAGRAM_SEND_ENABLED 0x1400000E")]
-        public const int QUIC_PARAM_CONN_DATAGRAM_SEND_ENABLED = 0x1400000E;
+        [NativeTypeName("#define QUIC_PARAM_CONN_DATAGRAM_SEND_ENABLED 0x0500000E")]
+        public const int QUIC_PARAM_CONN_DATAGRAM_SEND_ENABLED = 0x0500000E;
 
-        [NativeTypeName("#define QUIC_PARAM_CONN_RESUMPTION_TICKET 0x14000010")]
-        public const int QUIC_PARAM_CONN_RESUMPTION_TICKET = 0x14000010;
+        [NativeTypeName("#define QUIC_PARAM_CONN_DISABLE_1RTT_ENCRYPTION 0x0500000F")]
+        public const int QUIC_PARAM_CONN_DISABLE_1RTT_ENCRYPTION = 0x0500000F;
 
-        [NativeTypeName("#define QUIC_PARAM_CONN_PEER_CERTIFICATE_VALID 0x14000011")]
-        public const int QUIC_PARAM_CONN_PEER_CERTIFICATE_VALID = 0x14000011;
+        [NativeTypeName("#define QUIC_PARAM_CONN_RESUMPTION_TICKET 0x05000010")]
+        public const int QUIC_PARAM_CONN_RESUMPTION_TICKET = 0x05000010;
 
-        [NativeTypeName("#define QUIC_PARAM_TLS_SCHANNEL_CONTEXT_ATTRIBUTE_W 0x19000000")]
-        public const int QUIC_PARAM_TLS_SCHANNEL_CONTEXT_ATTRIBUTE_W = 0x19000000;
+        [NativeTypeName("#define QUIC_PARAM_CONN_PEER_CERTIFICATE_VALID 0x05000011")]
+        public const int QUIC_PARAM_CONN_PEER_CERTIFICATE_VALID = 0x05000011;
 
-        [NativeTypeName("#define QUIC_PARAM_TLS_HANDSHAKE_INFO 0x18000000")]
-        public const int QUIC_PARAM_TLS_HANDSHAKE_INFO = 0x18000000;
+        [NativeTypeName("#define QUIC_PARAM_CONN_LOCAL_INTERFACE 0x05000012")]
+        public const int QUIC_PARAM_CONN_LOCAL_INTERFACE = 0x05000012;
 
-        [NativeTypeName("#define QUIC_PARAM_TLS_NEGOTIATED_ALPN 0x18000001")]
-        public const int QUIC_PARAM_TLS_NEGOTIATED_ALPN = 0x18000001;
+        [NativeTypeName("#define QUIC_PARAM_CONN_TLS_SECRETS 0x05000013")]
+        public const int QUIC_PARAM_CONN_TLS_SECRETS = 0x05000013;
 
-        [NativeTypeName("#define QUIC_PARAM_STREAM_ID 0x1C000000")]
-        public const int QUIC_PARAM_STREAM_ID = 0x1C000000;
+        [NativeTypeName("#define QUIC_PARAM_TLS_HANDSHAKE_INFO 0x06000000")]
+        public const int QUIC_PARAM_TLS_HANDSHAKE_INFO = 0x06000000;
 
-        [NativeTypeName("#define QUIC_PARAM_STREAM_0RTT_LENGTH 0x1C000001")]
-        public const int QUIC_PARAM_STREAM_0RTT_LENGTH = 0x1C000001;
+        [NativeTypeName("#define QUIC_PARAM_TLS_NEGOTIATED_ALPN 0x06000001")]
+        public const int QUIC_PARAM_TLS_NEGOTIATED_ALPN = 0x06000001;
 
-        [NativeTypeName("#define QUIC_PARAM_STREAM_IDEAL_SEND_BUFFER_SIZE 0x1C000002")]
-        public const int QUIC_PARAM_STREAM_IDEAL_SEND_BUFFER_SIZE = 0x1C000002;
+        [NativeTypeName("#define QUIC_PARAM_TLS_SCHANNEL_CONTEXT_ATTRIBUTE_W 0x07000000")]
+        public const int QUIC_PARAM_TLS_SCHANNEL_CONTEXT_ATTRIBUTE_W = 0x07000000;
 
-        [NativeTypeName("#define QUIC_PARAM_STREAM_PRIORITY 0x1C000003")]
-        public const int QUIC_PARAM_STREAM_PRIORITY = 0x1C000003;
+        [NativeTypeName("#define QUIC_PARAM_STREAM_ID 0x08000000")]
+        public const int QUIC_PARAM_STREAM_ID = 0x08000000;
+
+        [NativeTypeName("#define QUIC_PARAM_STREAM_0RTT_LENGTH 0x08000001")]
+        public const int QUIC_PARAM_STREAM_0RTT_LENGTH = 0x08000001;
+
+        [NativeTypeName("#define QUIC_PARAM_STREAM_IDEAL_SEND_BUFFER_SIZE 0x08000002")]
+        public const int QUIC_PARAM_STREAM_IDEAL_SEND_BUFFER_SIZE = 0x08000002;
+
+        [NativeTypeName("#define QUIC_PARAM_STREAM_PRIORITY 0x08000003")]
+        public const int QUIC_PARAM_STREAM_PRIORITY = 0x08000003;
     }
 }

commit 367f72d97be651cba1228aeff49f227029a92c0c

fix build break caused by 47ee814b4dc0d113d983f9bc71222ba4025c2825

src/perf/lib/RpsClient.cpp
@@ -224,7 +224,6 @@ RpsClient::Start(
             Status =
                 MsQuic->SetParam(
                     Connections[i].Handle,
-                    QUIC_PARAM_LEVEL_CONNECTION,
                     QUIC_PARAM_CONN_DISABLE_1RTT_ENCRYPTION,
                     sizeof(value),
                     &value);