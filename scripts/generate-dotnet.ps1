a579852b17ece4195ea66bc86bb050797be4b1ce
Thad House <87844845+thhous-msft@users.noreply.github.com>
2022-03-01 12:12:55 -0800

    Fix dotnet generator (#2446)

--- scripts/generate-dotnet.ps1
+++ scripts/generate-dotnet.ps1
@@ -12,22 +12,47 @@ $PSDefaultParameterValues['*:ErrorAction'] = 'Stop'
 # Root directory of the project.
 $RootDir = Split-Path $PSScriptRoot -Parent
 
+$ToolPath = Join-Path $RootDir build dotnetgenerator
+
+if (Test-Path $ToolPath) { Remove-Item $ToolPath -Recurse -Force | Out-Null }
+
+dotnet tool install ClangSharpPInvokeGenerator --version 13.0.0-beta1 --tool-path $ToolPath
+
 $MsQuicHeader = Join-Path $RootDir src inc msquic.h
 $MsQuicGeneratedSource = Join-Path $RootDir src cs lib msquic_generated.cs
 
 $LicenseHeader = Join-Path $RootDir src cs LicenseHeader.txt
 
-ClangSharpPInvokeGenerator -f $MsQuicHeader -n Microsoft.Quic -o $MsQuicGeneratedSource -m MsQuic -l msquic `
-    -c exclude-enum-operators -r _SOCKADDR_INET=QuicAddr -c generate-macro-bindings -h $LicenseHeader `
-    -e QUIC_UINT62_MAX -e MsQuicOpen2 -e QUIC_API_VERSION_1 -D QUIC_API_ENABLE_INSECURE_FEATURES `
-    -D QUIC_API_ENABLE_PREVIEW_FEATURES
+$ToolExe = Join-Path $ToolPath ClangSharpPInvokeGenerator.exe
 
-$ReplaceDir = $RootDir + "\"
+$Arguments = @(
+    "-f $MsQuicHeader", # Header to parse
+    "-n Microsoft.Quic", # Namespace to place generated code in
+    "-o $MsQuicGeneratedSource", # Output file
+    "-m MsQuic", # Class to place global functions and definitions in
+    "-l msquic", # Library to import for functions
+    "-c exclude-enum-operators", # Exclude enum operators from generated
+    "-r _SOCKADDR_INET=QuicAddr", # Remap _SOCKADDR_INET to QuicAddr
+    "-c generate-macro-bindings", # Generate values for macros (Such as status codes)
+    "-h $LicenseHeader", # Add license header to file
+    "-e QUIC_UINT62_MAX", # Exclude QUIC_UINT62_MAX from generator
+    "-e MsQuicOpen2", # Exclude MsQuicOpen2 macro from generator
+    "-e QUIC_API_VERSION_1", # Exclude v1 API define
+    "-D QUIC_API_ENABLE_INSECURE_FEATURES", # Enable insecure features to be generated
+    "-D QUIC_API_ENABLE_PREVIEW_FEATURES" # Enable preview features to be generated
+    "-e QUIC_DATAGRAM_SEND_STATE_IS_FINAL" # Cannot generate macro functions
+    "-e QUIC_PARAM_IS_GLOBAL" # Cannot generate macro functions
+)
 
-(Get-Content $MsQuicGeneratedSource)
-    .Replace($ReplaceDir, "") `
-    .Replace($ReplaceDir.Replace("\", "/"), "") `
-    .Replace('(?<!(\[Flags\])\n    )public enum .*?_FLAGS','[Flags]\n    $0') `
-    .Replace('(?<!using System;\n)(?!using System;\n)(using .*?;\n)+', 'using System;\n$0') `
+$FullArgs = $Arguments -join " "
+
+Invoke-Expression "$ToolExe $FullArgs"
+
+(Get-Content $MsQuicGeneratedSource) `
+    -replace '\(anonymous struct.+\)\"', "(anonymous struct)`"" `
+    -replace '\(anonymous union.+\)\"', "(anonymous union)`"" `
+    -replace "public enum .*?_FLAGS","[System.Flags]`n    `$0" `
     | `
     Out-File $MsQuicGeneratedSource
+
+$LASTEXITCODE = 0
