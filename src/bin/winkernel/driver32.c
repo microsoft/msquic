abee1c3d788fc5c294eb6fcb453ab932de215c65
Thad House <thhous@microsoft.com>
2022-03-01 23:16:19 -0800

    Replace 32 bit kernel msquic with shim library

---
+++ src/bin/winkernel/driver32.c
@@ -0,0 +1,81 @@
+/*++
+
+    Copyright (c) Microsoft Corporation.
+    Licensed under the MIT License.
+
+Abstract:
+
+    Main entry point to the MsQuic.sys 32 bit driver.
+
+    This driver does not work, and will always return not supported
+
+--*/
+
+#include "quic_platform.h"
+
+INITCODE DRIVER_INITIALIZE DriverEntry;
+PAGEDX EVT_WDF_DRIVER_UNLOAD EvtDriverUnload;
+
+INITCODE
+_Function_class_(DRIVER_INITIALIZE)
+_IRQL_requires_same_
+_IRQL_requires_(PASSIVE_LEVEL)
+NTSTATUS
+DriverEntry(
+    _In_ PDRIVER_OBJECT DriverObject,
+    _In_ PUNICODE_STRING RegistryPath
+    )
+/*++
+
+Routine Description:
+
+    DriverEntry initializes the driver and is the first routine called by the
+    system after the driver is loaded. DriverEntry specifies the other entry
+    points in the function driver, such as EvtDevice and DriverUnload.
+
+Parameters Description:
+
+    DriverObject - represents the instance of the function driver that is loaded
+    into memory. DriverEntry must initialize members of DriverObject before it
+    returns to the caller. DriverObject is allocated by the system before the
+    driver is loaded, and it is released by the system after the system unloads
+    the function driver from memory.
+
+    RegistryPath - represents the driver specific path in the Registry.
+    The function driver can use the path to store driver related data between
+    reboots. The path does not store hardware instance specific data.
+
+Return Value:
+
+    A success status as determined by NT_SUCCESS macro, if successful.
+
+--*/
+{
+    UNREFERENCED_PARAMETER(DriverObject);
+    UNREFERENCED_PARAMETER(RegistryPath);
+    return QUIC_STATUS_NOT_SUPPORTED;
+}
+
+_IRQL_requires_max_(PASSIVE_LEVEL)
+_Check_return_
+QUIC_STATUS
+QUIC_API
+MsQuicOpenVersion(
+    _In_ uint32_t Version,
+    _Out_ _Pre_defensive_ const void** QuicApi
+    )
+{
+    UNREFERENCED_PARAMETER(Version);
+    UNREFERENCED_PARAMETER(QuicApi);
+    return QUIC_STATUS_NOT_SUPPORTED;
+}
+
+_IRQL_requires_max_(PASSIVE_LEVEL)
+void
+QUIC_API
+MsQuicClose(
+    _In_ _Pre_defensive_ const void* QuicApi
+    )
+{
+    UNREFERENCED_PARAMETER(QuicApi);
+}
