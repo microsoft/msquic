645219e534855ba317ff170995e4cf05b49e1782
Nick Banks <nibanks@microsoft.com>
2022-03-08 09:29:47 -0500

    Fix prepare-machine for AZP

--- scripts/prepare-machine.ps1
+++ scripts/prepare-machine.ps1
@@ -203,7 +203,9 @@ if ($IsWindows) {
             $env:PATH = "${env:PATH};$NasmPath"
             [Environment]::SetEnvironmentVariable("PATH", $CurrentSystemPath, [System.EnvironmentVariableTarget]::Machine)
             Write-Host "##vso[task.setvariable variable=PATH;]${env:PATH}"
-            Write-Host $NasmPath | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
+            if ($env:GITHUB_PATH -ne $null) {
+                Write-Host $NasmPath | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
+            }
             Write-Host "PATH has been updated. You'll need to restart your terminal for this to take affect."
         }
 
@@ -225,7 +227,9 @@ if ($IsWindows) {
             $env:PATH = "${env:PATH};$JomPath"
             [Environment]::SetEnvironmentVariable("PATH", $CurrentSystemPath, [System.EnvironmentVariableTarget]::Machine)
             Write-Host "##vso[task.setvariable variable=PATH;]${env:PATH}"
-            Write-Host $JomPath | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
+            if ($env:GITHUB_PATH -ne $null) {
+                Write-Host $JomPath | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
+            }
             Write-Host "PATH has been updated. You'll need to restart your terminal for this to take affect."
         }
     }
