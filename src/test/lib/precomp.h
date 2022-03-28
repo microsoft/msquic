268001db1767ffa387aa9257527ddae069165b78
Matt Olson (726931) <maolson@microsoft.com>
2022-03-28 13:32:27 -0700

    Move QUIC_LOCALHOST_FOR_AF override before inclusion of msquic.hpp

--- src/test/lib/precomp.h
+++ src/test/lib/precomp.h
@@ -41,38 +41,3 @@
 #include "TestConnection.h"
 #include "TestListener.h"
 #include "DrillDescriptor.h"
-
-extern bool UseDuoNic;
-
-//
-// Override the SDK version of QUIC_LOCALHOST_FOR_AF to use duonic instead of localhost when desired.
-//
-#ifdef _MSQUIC_WINUSER_
-#undef QUIC_LOCALHOST_FOR_AF
-#define QUIC_LOCALHOST_FOR_AF(Af) (UseDuoNic ? ((Af == QUIC_ADDRESS_FAMILY_INET) ? "192.168.1.11" : "fc00::1:11") : "localhost")
-#endif // _MSQUIC_WINUSER_
-
-//
-// Set a QUIC_ADDR to the duonic "server" address.
-//
-inline
-void
-QuicAddrSetToDuoNic(
-    _Inout_ QUIC_ADDR* Addr
-    )
-{
-    if (QuicAddrGetFamily(Addr) == QUIC_ADDRESS_FAMILY_INET) {
-        // 192.168.1.11
-        ((uint32_t*)&(Addr->Ipv4.sin_addr))[0] = 184658112;
-    } else {
-        // fc00::1:11
-        ((uint16_t*)&(Addr->Ipv6.sin6_addr))[0] = 252;
-        ((uint16_t*)&(Addr->Ipv6.sin6_addr))[1] = 0;
-        ((uint16_t*)&(Addr->Ipv6.sin6_addr))[2] = 0;
-        ((uint16_t*)&(Addr->Ipv6.sin6_addr))[3] = 0;
-        ((uint16_t*)&(Addr->Ipv6.sin6_addr))[4] = 0;
-        ((uint16_t*)&(Addr->Ipv6.sin6_addr))[5] = 0;
-        ((uint16_t*)&(Addr->Ipv6.sin6_addr))[6] = 256;
-        ((uint16_t*)&(Addr->Ipv6.sin6_addr))[7] = 4352;
-    }
-}
