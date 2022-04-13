1823f08da9072350fbb9956b30cbfe734fd17b68
Nick Banks <nibanks@microsoft.com>
2022-04-13 09:05:24 -0400

    Update version

--- scripts/package-distribution.ps1
+++ scripts/package-distribution.ps1
@@ -19,7 +19,7 @@ $ArtifactsBinDir = Join-Path $BaseArtifactsDir "bin"
 # All direct subfolders are OS's
 $Platforms = Get-ChildItem -Path $ArtifactsBinDir
 
-$Version = "2.0.2"
+$Version = "2.0.3"
 
 $WindowsBuilds = @()
 $AllBuilds = @()
