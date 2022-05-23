60b78f64c8798ad1e413525b470d77ce8d0ef527
Yi Huang <huanyi@microsoft.com>
2022-05-23 14:37:07 -0700

    Use port set XDP rules (#2748)
    * done for perf test
    
    * add devkit json to az piple
    
    * add devkit json to az piple

--- scripts/performance.ps1
+++ scripts/performance.ps1
@@ -288,6 +288,7 @@ $RemoteDirectorySMB = $null
 Copy-Item -Path (Join-Path $RootDir scripts log.ps1) -Destination $LocalDirectory
 Copy-Item -Path (Join-Path $RootDir scripts xdp-devkit.json) -Destination $LocalDirectory
 Copy-Item -Path (Join-Path $RootDir scripts prepare-machine.ps1) -Destination $LocalDirectory
+Copy-Item -Path (Join-Path $RootDir scripts xdp-devkit.json) -Destination $LocalDirectory
 Copy-Item -Path (Join-Path $RootDir src manifest MsQuic.wprp) -Destination $LocalDirectory
 
 if ($Local) {
