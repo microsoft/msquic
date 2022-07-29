4aa5433f8410edd28e1febf5f591068307e3829b
Matt Olson (726931) <maolson@microsoft.com>
2022-07-29 16:01:39 -0700

    Rename QUIC_ACK_EVENT/QUIC_LOSS_EVENT fields to match their QUIC_LOSS_DETECTION counterparts

--- src/core/loss_detection.c
+++ src/core/loss_detection.c
@@ -1063,7 +1063,7 @@ QuicLossDetectionDetectAndHandleLostPackets(
 
             QUIC_LOSS_EVENT LossEvent = {
                 .LargestPacketNumberLost = LargestLostPacketNumber,
-                .LargestPacketNumberSent = LossDetection->LargestSentPacketNumber,
+                .LargestSentPacketNumber = LossDetection->LargestSentPacketNumber,
                 .NumRetransmittableBytes = LostRetransmittableBytes,
                 .PersistentCongestion =
                     LossDetection->ProbeCount > QUIC_PERSISTENT_CONGESTION_THRESHOLD
@@ -1201,8 +1201,8 @@ QuicLossDetectionDiscardPackets(
         QUIC_ACK_EVENT AckEvent = {
             .IsImplicit = TRUE,
             .TimeNow = TimeNow,
-            .LargestPacketNumberAcked = LossDetection->LargestAck,
-            .LargestPacketNumberSent = LossDetection->LargestSentPacketNumber,
+            .LargestAck = LossDetection->LargestAck,
+            .LargestSentPacketNumber = LossDetection->LargestSentPacketNumber,
             .NumRetransmittableBytes = AckedRetransmittableBytes,
             .SmoothedRtt = Path->SmoothedRtt,
             .SmallestRttSample = 0,
@@ -1508,8 +1508,8 @@ QuicLossDetectionProcessAckBlocks(
         QUIC_ACK_EVENT AckEvent = {
             .IsImplicit = FALSE,
             .TimeNow = TimeNow,
-            .LargestPacketNumberAcked = LossDetection->LargestAck,
-            .LargestPacketNumberSent = LossDetection->LargestSentPacketNumber,
+            .LargestAck = LossDetection->LargestAck,
+            .LargestSentPacketNumber = LossDetection->LargestSentPacketNumber,
             .NumRetransmittableBytes = AckedRetransmittableBytes,
             .SmoothedRtt = Connection->Paths[0].SmoothedRtt,
             .SmallestRttSample = SmallestRtt,
