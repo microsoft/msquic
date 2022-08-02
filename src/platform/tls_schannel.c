cd6f236682d6db35883b33022fd069378601ce19
Radek Zikmund <radekzikmund@microsoft.com>
2022-08-02 15:01:00 +0200

    Fix formatting

--- src/platform/tls_schannel.c
+++ src/platform/tls_schannel.c
@@ -772,8 +772,7 @@ CxPlatTlsSetClientCertPolicy(
             "SetCredentialsAttributesW(SECPKG_ATTR_CLIENT_CERT_POLICY) failed");
     }
 
-    if (SecStatus == SEC_E_UNSUPPORTED_FUNCTION)
-    {
+    if (SecStatus == SEC_E_UNSUPPORTED_FUNCTION) {
         return QUIC_STATUS_NOT_SUPPORTED;
     }
 
