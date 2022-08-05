910325327a4e32b7b55227f6f4f2405d290b2f12
Nick Banks <nibanks@microsoft.com>
2022-08-05 10:59:45 -0400

    Update main Version to v2.2 (#2948)

--- scripts/package-nuget.ps1
+++ scripts/package-nuget.ps1
@@ -155,7 +155,7 @@ $DistDir = Join-Path $BaseArtifactsDir "dist"
 $CurrentCommitHash = Get-GitHash -RepoDir $RootDir
 $RepoRemote = Get-GitRemote -RepoDir $RootDir
 
-$Version = "2.1.0"
+$Version = "2.2.0"
 
 $BuildId = $env:BUILD_BUILDID
 if ($null -ne $BuildId) {
