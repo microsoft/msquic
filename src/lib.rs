674607ba4b97fa3104a8f780e1de58c2699a63d7
Ahmet İbrahim AKSOY <liveans07@hotmail.com>
2022-08-09 15:05:02 +0300

    Update: Fix Rust pointer type

--- src/lib.rs
+++ src/lib.rs
@@ -761,7 +761,7 @@ pub const LISTENER_EVENT_NEW_CONNECTION: ListenerEventType = 0;
 pub struct ListenerEventNewConnection {
     pub info: *const NewConnectionInfo,
     pub connection: Handle,
-    pub new_negotiated_alpn: *u8,
+    pub new_negotiated_alpn: *const u8,
     pub new_negotiated_alpn_length: u8,
 }
 
