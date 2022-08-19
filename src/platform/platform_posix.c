f4056224545673de6412488661442b9d870427da
Tomas Weinfurt <tweinfurt@yahoo.com>
2022-08-19 16:30:40 -0700

    retry pthread_create also on ENOENT (#2998)

--- src/platform/platform_posix.c
+++ src/platform/platform_posix.c
@@ -652,11 +652,11 @@ CxPlatThreadCreate(
 #else // CXPLAT_USE_CUSTOM_THREAD_CONTEXT
 
     //
-    // If pthread_create fails with ENOKEY, then try again without the attribute
+    // If pthread_create fails with ENOKEY or ENOENT, then try again without the attribute
     // because the CPU might be offline.
     //
     if (pthread_create(Thread, &Attr, Config->Callback, Config->Context) &&
-        (errno != ENOKEY || pthread_create(Thread, NULL, Config->Callback, Config->Context))) {
+        ((errno != ENOKEY && errno != ENOENT) || pthread_create(Thread, NULL, Config->Callback, Config->Context))) {
         Status = errno;
         QuicTraceEvent(
             LibraryErrorStatus,
