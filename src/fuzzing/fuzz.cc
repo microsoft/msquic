fb1bb8eba083f9e0679584be76d57cd50aea2caa
ami-GS <1991.daiki@gmail.com>
2022-09-20 20:55:22 -0700

    fuzzing for global SetParam

---
+++ src/fuzzing/fuzz.cc
@@ -0,0 +1,36 @@
+#define CX_PLATFORM_LINUX 1
+#define QUIC_TEST_APIS 1
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <string>
+#include "msquic.h"
+#include "msquic.hpp"
+#include "quic_platform.h"
+
+const MsQuicApi *MsQuic;
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+	uint8_t *Buf = (uint8_t *)malloc(size);
+	if (Buf == NULL) {
+		return 0;
+	}
+	memcpy(Buf, data, size);
+
+	MsQuic = new(std::nothrow) MsQuicApi();
+
+	for (uint32_t Param = QUIC_PARAM_GLOBAL_RETRY_MEMORY_PERCENT;
+		Param <= QUIC_PARAM_GLOBAL_TLS_PROVIDER;
+		Param++) {
+		MsQuic->SetParam(
+			nullptr,
+			Param,
+			size,
+			&Buf);
+	}
+
+	delete MsQuic;
+	free(Buf);
+	return 0;
+}
