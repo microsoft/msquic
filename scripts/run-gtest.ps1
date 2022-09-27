858cf8148e0aeb91c6375e4fa77a6b59853aeb07
Nick Banks <nibanks@microsoft.com>
2022-09-27 12:26:46 -0400

    Backport Infra and Test Fixes to release/2.0 (#3086)

--- scripts/run-gtest.ps1
+++ scripts/run-gtest.ps1
@@ -47,7 +47,7 @@ as necessary.
 .Parameter EnableAppVerifier
     Enables all basic Application Verifier checks on the test binary.
 
-.Parameter EnableTcpipVerifier
+.Parameter EnableSystemVerifier
     Enables TCPIP verifier in user mode tests.
 
 .Parameter CodeCoverage
@@ -107,7 +107,7 @@ param (
     [switch]$EnableAppVerifier = $false,
 
     [Parameter(Mandatory = $false)]
-    [switch]$EnableTcpipVerifier = $false,
+    [switch]$EnableSystemVerifier = $false,
 
     [Parameter(Mandatory = $false)]
     [switch]$CodeCoverage = $false,
@@ -285,7 +285,7 @@ function Start-TestExecutable([String]$Arguments, [String]$OutputDir) {
     $pinfo = New-Object System.Diagnostics.ProcessStartInfo
     if ($IsWindows) {
         if ($Debugger) {
-            $pinfo.FileName = "windbg"
+            $pinfo.FileName = "windbgx"
             if ($InitialBreak) {
                 $pinfo.Arguments = "-G $($Path) $($Arguments)"
             } else {
@@ -674,9 +674,11 @@ if ($Kernel -ne "") {
     if ($LastExitCode) {
         Log ("sc.exe " + $LastExitCode)
     }
-    verifier.exe /volatile /adddriver msquicpriv.sys msquictestpriv.sys /flags 0x9BB
-    if ($LastExitCode) {
-        Log ("verifier.exe " + $LastExitCode)
+    if ($EnableSystemVerifier) {
+        verifier.exe /volatile /adddriver msquicpriv.sys msquictestpriv.sys /flags 0x9BB
+        if ($LastExitCode) {
+            Log ("verifier.exe " + $LastExitCode)
+        }
     }
     net.exe start msquicpriv
     if ($LastExitCode) {
@@ -684,7 +686,7 @@ if ($Kernel -ne "") {
     }
 }
 
-if ($IsWindows -and ($EnableTcpipVerifier -or $Kernel)) {
+if ($IsWindows -and $EnableSystemVerifier) {
     verifier.exe /volatile /adddriver afd.sys netio.sys tcpip.sys /flags 0x9BB
     if ($LastExitCode) {
         Log ("verifier.exe " + $LastExitCode)
@@ -758,11 +760,13 @@ try {
         net.exe stop msquicpriv /y | Out-Null
         sc.exe delete msquictestpriv | Out-Null
         sc.exe delete msquicpriv | Out-Null
-        verifier.exe /volatile /removedriver msquicpriv.sys msquictestpriv.sys
-        verifier.exe /volatile /flags 0x0
     }
 
-    if ($IsWindows -and ($EnableTcpipVerifier -or $Kernel)) {
+    if ($IsWindows -and $EnableSystemVerifier) {
+        if ($Kernel -ne "") {
+            verifier.exe /volatile /removedriver msquicpriv.sys msquictestpriv.sys
+            verifier.exe /volatile /flags 0x0
+        }
         verifier.exe /volatile /removedriver afd.sys netio.sys tcpip.sys
         verifier.exe /volatile /flags 0x0
     }
