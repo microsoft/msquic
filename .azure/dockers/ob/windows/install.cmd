3a19050b96d07bba702a8c36863bb7a33f560e53
Yi Huang <huanyi@microsoft.com>
2022-10-04 13:25:28 -0700

    Backport changes to release/2.0 for OBP builds (#3108)

---
+++ .azure/dockers/ob/windows/install.cmd
@@ -0,0 +1,4 @@
+
+"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -ExecutionPolicy Unrestricted -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -File "%1"
+exit /B %ERRORLEVEL%
+
