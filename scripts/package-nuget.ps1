f2a36043dcfe8364822af0ebe9858e7b1c5cbadb
Nick Banks <nibanks@microsoft.com>
2022-10-05 10:10:45 -0400

    Update patch version

--- scripts/package-nuget.ps1
+++ scripts/package-nuget.ps1
@@ -155,7 +155,7 @@ $DistDir = Join-Path $BaseArtifactsDir "dist"
 $CurrentCommitHash = Get-GitHash -RepoDir $RootDir
 $RepoRemote = Get-GitRemote -RepoDir $RootDir
 
-$Version = "2.1.2"
+$Version = "2.1.3"
 
 $BuildId = $env:BUILD_BUILDID
 if ($null -ne $BuildId) {
