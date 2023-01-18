0f9356147d460ff19e03d7abc6d7f976c73d8c03
Daiki AMINAKA <1991.daiki@gmail.com>
2023-01-18 00:20:12 +0000

    explicitly install flamegraph tools

--- scripts/log.ps1
+++ scripts/log.ps1
@@ -143,9 +143,13 @@ if ($IsLinux) {
     catch {
         Write-Host "Installing perf"
         sudo apt-get install -y linux-tools-$(uname -r)
+    }
+    if (Test-Path -Path /usr/bin/stackcollapse-perf.pl) {
         wget https://raw.githubusercontent.com/brendangregg/FlameGraph/master/stackcollapse-perf.pl -O /usr/bin/stackcollapse-perf.pl
-        wget https://raw.githubusercontent.com/brendangregg/FlameGraph/master/flamegraph.pl -O /usr/bin/flamegraph.pl
         chmod +x /usr/bin/stackcollapse-perf.pl
+    }
+    if (Test-Path -Path /usr/bin/flamegraph.pl) {
+        wget https://raw.githubusercontent.com/brendangregg/FlameGraph/master/flamegraph.pl -O /usr/bin/flamegraph.pl
         chmod +x /usr/bin/flamegraph.pl
     }
     #Write-Host "Checking perf command...... Done"
