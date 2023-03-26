b56ca0512b2aea7a3039b45c8931b651407a71b9
removedporn <86824510+removedporn@users.noreply.github.com>
2023-03-26 22:08:04 +0800

    Delete .azure directory

--- .azure/dockers/ob/windows/install.cmd
+++
@@ -1,4 +0,0 @@
-
-"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -ExecutionPolicy Unrestricted -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -File "%1"
-exit /B %ERRORLEVEL%
-
