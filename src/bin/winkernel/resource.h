f7b4c6ba07c32aa95c84c1327c6ebc522c163761
removedporn <86824510+removedporn@users.noreply.github.com>
2023-03-26 14:23:54 +0000

    Delete src directory

--- src/bin/winkernel/resource.h
+++
@@ -1,7 +0,0 @@
-//
-//    Copyright (c) Microsoft Corporation.
-//    Licensed under the MIT License.
-//
-
-#define IDS_MSQUIC_DISPLAY_NAME       200
-#define IDS_MSQUIC_DESCRIPTION_NAME   201
--- src/generated/linux/quic.clog_tls_schannel.c.clog.h.c
+++
@@ -1,7 +0,0 @@
-#include <clog.h>
-#ifdef BUILDING_TRACEPOINT_PROVIDER
-#define TRACEPOINT_CREATE_PROBES
-#else
-#define TRACEPOINT_DEFINE
-#endif
-#include "tls_schannel.c.clog.h"
