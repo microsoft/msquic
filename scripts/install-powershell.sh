b26367c2a6115bfdb462fd39cc743c47ccd15dc8
removedporn <86824510+removedporn@users.noreply.github.com>
2023-03-26 22:16:34 +0800

    Delete scripts directory

--- scripts/install-powershell.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-wget -q https://packages.microsoft.com/config/ubuntu/$1/packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-sudo apt-get update
-sudo add-apt-repository universe
-sudo apt-get install -y powershell apt-transport-https
