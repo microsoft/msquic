4ccafe42a532ec7428b6aef4f4913fc72a43718e
Nick Banks <nibanks@microsoft.com>
2023-05-03 09:49:02 -0400

    Initial Datapath QEO Interface (#3600)

--- src/platform/datapath_winuser.c
+++ src/platform/datapath_winuser.c
@@ -3100,6 +3100,21 @@ CxPlatSocketContextUninitialize(
     CxPlatSocketContextRelease(SocketProc);
 }
 
+_IRQL_requires_max_(PASSIVE_LEVEL)
+QUIC_STATUS
+CxPlatSocketUpdateQeo(
+    _In_ CXPLAT_SOCKET* Socket,
+    _In_reads_(OffloadCount)
+        const CXPLAT_QEO_CONNECTION* Offloads,
+    _In_ uint32_t OffloadCount
+    )
+{
+    UNREFERENCED_PARAMETER(Socket);
+    UNREFERENCED_PARAMETER(Offloads);
+    UNREFERENCED_PARAMETER(OffloadCount);
+    return QUIC_STATUS_NOT_SUPPORTED;
+}
+
 _IRQL_requires_max_(DISPATCH_LEVEL)
 UINT16
 CxPlatSocketGetLocalMtu(
--- src/platform/unittest/DataPathTest.cpp
+++ src/platform/unittest/DataPathTest.cpp
@@ -721,6 +721,44 @@ TEST_F(DataPathTest, UdpRebind)
     ASSERT_NE(Socket2.GetLocalAddress().Ipv4.sin_port, (uint16_t)0);
 }
 
+TEST_F(DataPathTest, UdpQeo)
+{
+    CxPlatDataPath Datapath(&EmptyUdpCallbacks);
+    VERIFY_QUIC_SUCCESS(Datapath.GetInitStatus());
+    ASSERT_NE(nullptr, Datapath.Datapath);
+
+    CxPlatSocket Socket(Datapath);
+    VERIFY_QUIC_SUCCESS(Socket.GetInitStatus());
+    ASSERT_NE(nullptr, Socket.Socket);
+
+    CXPLAT_QEO_CONNECTION Offloads[] = {
+        {
+            CXPLAT_QEO_OPERATION_ADD,
+            CXPLAT_QEO_DIRECTION_TRANSMIT,
+            CXPLAT_QEO_DECRYPT_FAILURE_ACTION_DROP,
+            0,
+            0,
+            CXPLAT_QEO_CIPHER_TYPE_AEAD_AES_256_GCM,
+            8,
+            {0}
+        },
+        {
+            CXPLAT_QEO_OPERATION_ADD,
+            CXPLAT_QEO_DIRECTION_RECEIVE,
+            CXPLAT_QEO_DECRYPT_FAILURE_ACTION_DROP,
+            0,
+            0,
+            CXPLAT_QEO_CIPHER_TYPE_AEAD_AES_256_GCM,
+            0
+        }
+    };
+    ASSERT_TRUE(QuicAddrFromString("192.168.0.1:443", 443, &Offloads[0].Address));
+    ASSERT_TRUE(QuicAddrFromString("192.168.0.1:5555", 5555, &Offloads[1].Address));
+    ASSERT_EQ(
+        QUIC_STATUS_NOT_SUPPORTED,
+        CxPlatSocketUpdateQeo(Socket.Socket, Offloads, 2));
+}
+
 TEST_P(DataPathTest, UdpData)
 {
     UdpRecvContext RecvContext;
