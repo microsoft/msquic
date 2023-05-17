9197a26d1ca40a9931f1c28c60d3fdbea7fc77c8
Nick Banks <nibanks@microsoft.com>
2023-05-17 17:08:29 -0400

    Don't forget to queue

--- src/core/api.c
+++ src/core/api.c
@@ -1142,6 +1142,7 @@ MsQuicStreamSend(
             Oper->API_CALL.Context->CONN_SHUTDOWN.Flags = QUIC_CONNECTION_SHUTDOWN_FLAG_SILENT;
             Oper->API_CALL.Context->CONN_SHUTDOWN.ErrorCode = 0;
             Oper->API_CALL.Context->CONN_SHUTDOWN.RegistrationShutdown = FALSE;
+            QuicConnQueueOper(Connection, Oper);
             goto Exit;
         }
         Oper->API_CALL.Context->Type = QUIC_API_TYPE_STRM_SEND;
