d0527a72461a442a4ddde8488efa0a30dcece0b7
Yi Huang <huanyi@microsoft.com>
2023-06-03 20:47:15 -0700

    2.2.2

--- scripts/package-nuget.ps1
+++ scripts/package-nuget.ps1
@@ -159,7 +159,7 @@ $DistDir = Join-Path $BaseArtifactsDir "dist"
 $CurrentCommitHash = Get-GitHash -RepoDir $RootDir
 $RepoRemote = Get-GitRemote -RepoDir $RootDir
 
-$Version = "2.2.1"
+$Version = "2.2.2"
 
 $BuildId = $env:BUILD_BUILDID
 if ($null -ne $BuildId) {
--- scripts/write-versions.ps1
+++ scripts/write-versions.ps1
@@ -26,7 +26,7 @@ $ArtifactsDir = $BuildConfig.ArtifactsDir
 $SourceVersion = $env:BUILD_SOURCEVERSION;
 $SourceBranch = $env:BUILD_SOURCEBRANCH;
 $BuildId = $env:BUILD_BUILDID;
-$VersionNumber = "2.2.1";
+$VersionNumber = "2.2.2";
 
 class BuildData {
     [string]$SourceVersion;
