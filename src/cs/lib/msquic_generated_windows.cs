91c6d97c138b26fa00b00e124fd7a072039707be
pedrobsaila <badrebsaila@outlook.com>
2023-06-08 18:34:48 +0200

    adjust to remarks

--- src/cs/lib/msquic_generated_windows.cs
+++ src/cs/lib/msquic_generated_windows.cs
@@ -114,8 +114,8 @@ namespace Microsoft.Quic
         [NativeTypeName("#define QUIC_STATUS_CERT_NO_CERT SEC_E_NO_CREDENTIALS")]
         public const int QUIC_STATUS_CERT_NO_CERT = unchecked((int)(0x8009030E));
 
-        [NativeTypeName("#define QUIC_STATUS_ADDRESS_NOT_AVAILABLE QUIC_STATUS_HRESULT_FROM_WIN32(WSAENETUNREACH)")]
-        public const int QUIC_STATUS_ADDRESS_NOT_AVAILABLE = unchecked((int)(((10051) & 0x0000FFFF) | (7 << 16) | 0x80000000));
+        [NativeTypeName("#define QUIC_STATUS_ADDRESS_NOT_AVAILABLE QUIC_STATUS_HRESULT_FROM_WIN32(WSAEAFNOSUPPORT)")]
+        public const int QUIC_STATUS_ADDRESS_NOT_AVAILABLE = unchecked((int)(((10047) & 0x0000FFFF) | (7 << 16) | 0x80000000));
 
         public const int QUIC_ADDRESS_FAMILY_UNSPEC = 0;
         public const int QUIC_ADDRESS_FAMILY_INET = 2;
