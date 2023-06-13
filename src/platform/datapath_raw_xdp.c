d219aee0b28f3615a749fc70bc7d9c6f6139d3aa
Nick Banks <nibanks@microsoft.com>
2023-06-13 10:12:34 -0700

    Connections Size, Not Count (#3695)

--- src/platform/datapath_raw_xdp.c
+++ src/platform/datapath_raw_xdp.c
@@ -1338,6 +1338,7 @@ CxPlatSocketUpdateQeo(
 
     QUIC_STATUS Status = QUIC_STATUS_SUCCESS;
     for (uint32_t i = 0; i < OffloadCount; i++) {
+        XdpInitializeQuicConnection(&Connections[i], sizeof(Connections[i]));
         Connections[i].Operation = Offloads[i].Operation;
         Connections[i].Direction = Offloads[i].Direction;
         Connections[i].DecryptFailureAction = Offloads[i].DecryptFailureAction;
@@ -1375,7 +1376,7 @@ CxPlatSocketUpdateQeo(
             Xdp->XdpApi->XdpQeoSet(
                 CONTAINING_RECORD(Entry, XDP_INTERFACE, Link)->XdpHandle,
                 Connections,
-                OffloadCount);
+                sizeof(Connections));
         if (QUIC_FAILED(Status)) {
             QuicTraceEvent(
                 LibraryErrorStatus,
