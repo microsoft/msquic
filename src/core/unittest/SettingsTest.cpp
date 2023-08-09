ff8b385047f251efe527215463b5794aaa7934ed
Yi Huang (from Dev Box) <huanyi@microsoft.com>
2023-08-08 17:46:56 -0700

    initialize lock

--- src/core/unittest/SettingsTest.cpp
+++ src/core/unittest/SettingsTest.cpp
@@ -511,6 +511,7 @@ TEST(SettingsTest, GlobalRawDataPathProcsSetAfterDataPathInit)
     Config->ProcessorCount = 2;
     Config->ProcessorList[0] = 0;
     Config->ProcessorList[1] = 1;
+    CxPlatLockInitialize(&MsQuicLib.Lock); // Initialize the lock so it can be acquired later
     MsQuicLib.PerProc = (QUIC_LIBRARY_PP*)1; // Pretend already initialized
     MsQuicLib.Datapath = (CXPLAT_DATAPATH*)1; // Pretend already initialized
     ASSERT_EQ(
