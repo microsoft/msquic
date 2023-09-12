4bb50752d767f5319e83b212fba806f4cb594f38
Jack He <jackhe@microsoft.com>
2023-09-12 11:14:20 -0700

    update git workflow

--- scripts/update-docfx-site.ps1
+++ scripts/update-docfx-site.ps1
@@ -2,12 +2,14 @@ $env:GIT_REDIRECT_STDERR = '2>&1'
 
 # Checkout the Performance branch (where data and docfx is stored)
 git fetch
+# Stash any changes to the current branch
+git stash
 git checkout performance-dupe
 
 # Commit the output file.
 git config user.email "quicdev@microsoft.com"
 git config user.name "QUIC Dev[bot]"
-git add ./_site
+git add ./_site -f
 git status
 git commit -m "Update DocFx after documentation changes."
 git pull
