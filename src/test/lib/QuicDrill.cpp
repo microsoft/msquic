20773b633d807ac13764b0451011c9ece4b50f1c
Jack He (Github) <jackhe2345@gmail.com>
2023-10-12 07:57:16 -0700

    Drill test failure triage (#3913)

--- src/test/lib/QuicDrill.cpp
+++ src/test/lib/QuicDrill.cpp
@@ -503,6 +503,10 @@ QuicDrillTestServerVNPacket(
     MsQuicRegistration Registration(true);
     TEST_QUIC_SUCCEEDED(Registration.GetInitStatus());
 
+    if (QuitTestIsFeatureSupported(CXPLAT_DATAPATH_FEATURE_RAW)) {
+        return;
+    }
+
     QUIC_ADDRESS_FAMILY QuicAddrFamily = (Family == 4) ? QUIC_ADDRESS_FAMILY_INET : QUIC_ADDRESS_FAMILY_INET6;
     QuicAddr ServerLocalAddr(QuicAddrFamily);
 
