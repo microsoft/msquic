8bfb191993b7413181d3adb2601239b8427b6375
Gaurav Singh <gaurasingh@microsoft.com>
2023-10-16 13:49:16 +0530

    added construction of initial packet and modification of server code

--- src/tools/dir/spinquic_receivepacket.cpp
+++ src/tools/dir/spinquic_receivepacket.cpp
@@ -19,9 +19,12 @@
 #ifndef NOMINMAX
 #define NOMINMAX
 #endif
-#include "precomp.h"
+#include "precomp.h" // from core directory
 #include "msquichelper.h"
 #include "msquic.hpp"
+
+#define MagicCid 0x989898989898989ull
+const QUIC_HKDF_LABELS HkdfLabels = { "quic key", "quic iv", "quic hp", "quic ku" };
 
 
 #define ASSERT_ON_FAILURE(x) \
@@ -175,6 +178,223 @@ void printf_buf(const char* name, void* buf, uint32_t len)
     printf("\n");
 }
 
+struct TlsContext
+{
+    CXPLAT_TLS* Ptr;
+    CXPLAT_SEC_CONFIG* SecConfig;
+    CXPLAT_TLS_PROCESS_STATE State;
+    uint8_t AlpnListBuffer[256];
+
+    TlsContext() :
+        Ptr(nullptr), SecConfig(nullptr) {
+
+        AlpnListBuffer[0] = (uint8_t)strlen(Alpn);
+        memcpy(&AlpnListBuffer[1], Alpn, AlpnListBuffer[0]);
+
+        CxPlatZeroMemory(&State, sizeof(State));
+        State.Buffer = (uint8_t*)CXPLAT_ALLOC_NONPAGED(8000, QUIC_POOL_TOOL);
+        State.BufferAllocLength = 8000;
+
+        QUIC_CREDENTIAL_CONFIG CredConfig = {
+            QUIC_CREDENTIAL_TYPE_NONE,
+            QUIC_CREDENTIAL_FLAG_CLIENT | QUIC_CREDENTIAL_FLAG_NO_CERTIFICATE_VALIDATION,
+            NULL, NULL, NULL, NULL
+        };
+        CXPLAT_TLS_CALLBACKS TlsCallbacks = {
+            OnRecvQuicTP,
+            NULL
+        };
+        QUIC_SUCCEEDED(
+            CxPlatTlsSecConfigCreate(
+                &CredConfig,
+                CXPLAT_TLS_CREDENTIAL_FLAG_NONE,
+                &TlsCallbacks,
+                &SecConfig,
+                OnSecConfigCreateComplete));
+
+        QUIC_CONNECTION Connection = {};
+
+        QUIC_TRANSPORT_PARAMETERS TP = {0};
+        TP.Flags |= QUIC_TP_FLAG_INITIAL_MAX_DATA;
+        TP.InitialMaxData = 10000;
+        TP.Flags |= QUIC_TP_FLAG_INITIAL_MAX_STRM_DATA_BIDI_LOCAL;
+        TP.InitialMaxStreamDataBidiLocal = 10000;
+        TP.Flags |= QUIC_TP_FLAG_INITIAL_MAX_STRM_DATA_BIDI_REMOTE;
+        TP.InitialMaxStreamDataBidiRemote = 10000;
+        TP.Flags |= QUIC_TP_FLAG_INITIAL_MAX_STRMS_BIDI;
+        TP.InitialMaxBidiStreams = 3;
+        TP.Flags |= QUIC_TP_FLAG_INITIAL_MAX_STRMS_UNI;
+        TP.InitialMaxUniStreams = 3;
+        TP.Flags |= QUIC_TP_FLAG_INITIAL_SOURCE_CONNECTION_ID;
+        TP.InitialSourceConnectionIDLength = sizeof(uint64_t);
+        *(uint64_t*)&TP.InitialSourceConnectionID[0] = MagicCid;
+
+        CXPLAT_TLS_CONFIG Config = {0};
+        Config.IsServer = FALSE;
+        Config.SecConfig = SecConfig;
+        Config.HkdfLabels = &HkdfLabels;
+        Config.AlpnBuffer = AlpnListBuffer;
+        Config.AlpnBufferLength = AlpnListBuffer[0] + 1;
+        Config.TPType = TLS_EXTENSION_TYPE_QUIC_TRANSPORT_PARAMETERS;
+        Config.LocalTPBuffer =
+            QuicCryptoTlsEncodeTransportParameters(&Connection, FALSE, &TP, NULL, &Config.LocalTPLength);
+        if (!Config.LocalTPBuffer) {
+            printf("Failed to encode transport parameters!\n");
+        }
+        Config.Connection = (QUIC_CONNECTION*)this;
+        Config.ServerName = Sni;
+
+        QUIC_SUCCEEDED(
+            CxPlatTlsInitialize(
+                &Config,
+                &State,
+                &Ptr));
+    }
+
+    ~TlsContext() {
+        CxPlatTlsUninitialize(Ptr);
+        if (SecConfig) {
+            CxPlatTlsSecConfigDelete(SecConfig);
+        }
+        CXPLAT_FREE(State.Buffer, QUIC_POOL_TOOL);
+        for (uint8_t i = 0; i < QUIC_PACKET_KEY_COUNT; ++i) {
+            QuicPacketKeyFree(State.ReadKeys[i]);
+            QuicPacketKeyFree(State.WriteKeys[i]);
+        }
+    }
+
+private:
+
+    _Function_class_(CXPLAT_SEC_CONFIG_CREATE_COMPLETE)
+    static void
+    QUIC_API
+    OnSecConfigCreateComplete(
+        _In_ const QUIC_CREDENTIAL_CONFIG* /* CredConfig */,
+        _In_opt_ void* Context,
+        _In_ QUIC_STATUS /* Status */,
+        _In_opt_ CXPLAT_SEC_CONFIG* SecConfig
+        )
+    {
+        *(CXPLAT_SEC_CONFIG**)Context = SecConfig;
+    }
+
+    CXPLAT_TLS_RESULT_FLAGS
+    ProcessData(
+        _In_reads_bytes_(*BufferLength)
+            const uint8_t * Buffer,
+        _In_ uint32_t * BufferLength
+        )
+    {
+        auto Result =
+            CxPlatTlsProcessData(
+                Ptr,
+                CXPLAT_TLS_CRYPTO_DATA,
+                Buffer,
+                BufferLength,
+                &State);
+
+        if (Result & CXPLAT_TLS_RESULT_ERROR) {
+            printf("Failed to process data!\n");
+            exit(0);
+        }
+
+        return Result;
+    }
+
+public:
+
+    CXPLAT_TLS_RESULT_FLAGS
+    ProcessData(
+        _Inout_ CXPLAT_TLS_PROCESS_STATE* PeerState = nullptr
+        )
+    {
+        if (PeerState == nullptr) {
+            //
+            // Special case for client hello/initial.
+            //
+            uint32_t Zero = 0;
+            return ProcessData(nullptr, &Zero);
+        }
+
+        uint32_t Result = 0;
+
+        while (PeerState->BufferLength != 0) {
+            uint32_t BufferLength;
+            uint32_t StartOffset = PeerState->BufferTotalLength - PeerState->BufferLength;
+            if (PeerState->BufferOffset1Rtt != 0 && StartOffset >= PeerState->BufferOffset1Rtt) {
+                BufferLength = PeerState->BufferLength;
+
+            } else if (PeerState->BufferOffsetHandshake != 0 && StartOffset >= PeerState->BufferOffsetHandshake) {
+                if (PeerState->BufferOffset1Rtt != 0) {
+                    BufferLength = (uint16_t)(PeerState->BufferOffset1Rtt - StartOffset);
+                } else {
+                    BufferLength = PeerState->BufferLength;
+                }
+
+            } else {
+                if (PeerState->BufferOffsetHandshake != 0) {
+                    BufferLength = (uint16_t)(PeerState->BufferOffsetHandshake - StartOffset);
+                } else {
+                    BufferLength = PeerState->BufferLength;
+                }
+            }
+
+            Result |=
+                (uint32_t)ProcessData(
+                    PeerState->Buffer,
+                    &BufferLength);
+
+            PeerState->BufferLength -= (uint16_t)BufferLength;
+            CxPlatMoveMemory(
+                PeerState->Buffer,
+                PeerState->Buffer + BufferLength,
+                PeerState->BufferLength);
+        }
+
+        return (CXPLAT_TLS_RESULT_FLAGS)Result;
+    }
+
+private:
+
+    static BOOLEAN
+    OnRecvQuicTP(
+        _In_ QUIC_CONNECTION* Connection,
+        _In_ uint16_t TPLength,
+        _In_reads_(TPLength) const uint8_t* TPBuffer
+        )
+    {
+        UNREFERENCED_PARAMETER(Connection);
+        UNREFERENCED_PARAMETER(TPLength);
+        UNREFERENCED_PARAMETER(TPBuffer);
+        return TRUE;
+    }
+};
+
+static 
+void WriteInitialCryptoFrame(    
+    _Inout_ uint16_t* Offset,
+    _In_ uint16_t BufferLength,
+    _Out_writes_to_(BufferLength, *Offset)
+        uint8_t* Buffer)
+{
+    TlsContext ClientContext;
+    ClientContext.ProcessData();
+
+    QUIC_CRYPTO_EX Frame = {
+        0, ClientContext.State.BufferLength, ClientContext.State.Buffer
+    };
+
+    if (!QuicCryptoFrameEncode(
+            &Frame,
+            Offset,
+            BufferLength,
+            Buffer)) {
+        printf("QuicCryptoFrameEncode failure!\n");
+        exit(0);
+    }
+}
+
+
 void WriteClientInitialPacket(  
     _In_ uint32_t PacketNumber,
     _In_ uint8_t CidLength,
@@ -189,8 +409,8 @@ void WriteClientInitialPacket(
     uint8_t CryptoBuffer[4096];
     uint16_t BufferSize = sizeof(CryptoBuffer);
     uint16_t CryptoBufferLength = 0;
-    // WriteInitialCryptoFrame(
-    //     Alpn, Sni, &CryptoBufferLength, BufferSize, CryptoBuffer);
+    WriteInitialCryptoFrame(
+         &CryptoBufferLength, BufferSize, CryptoBuffer);
     uint8_t CidBuffer[sizeof(QUIC_CID) + 256] = {0};
     QUIC_CID* Cid = (QUIC_CID*)CidBuffer;
     Cid->IsInitial = TRUE;
@@ -236,22 +456,31 @@ void start(){
     };
     CxPlatSystemLoad();
     CxPlatInitialize();
-    CxPlatDataPathInitialize(
+    QUIC_STATUS Status = CxPlatDataPathInitialize(
         0,
         &DatapathCallbacks,
         NULL,
         NULL,
         &Datapath);
-    QUIC_ADDR sockAddr;
-    CxPlatDataPathResolveAddress(
+    if (QUIC_FAILED(Status)) {
+        printf("Datapath init failed 0x%x", Status);
+        return;
+    }
+    QUIC_ADDR sockAddr = {0};
+    QUIC_ADDRESS_FAMILY Family = QUIC_ADDRESS_FAMILY_INET6; // fuzz
+    QuicAddrSetFamily(&sockAddr, Family);
+    Status = CxPlatDataPathResolveAddress(
                     Datapath,
                     Sni,
                     &sockAddr);
+    if (QUIC_FAILED(Status)) {
+        printf("Address Resolution Failed 0x%x", Status);
+        return;
+    }
     QuicAddrSetPort(&sockAddr, 9999);
     MsQuic = new(std::nothrow) MsQuicApi();
     // make a server
     MsQuicRegistration Registration(true);
-
     QUIC_SUCCEEDED(Registration.GetInitStatus());
 
     auto CredConfig = CxPlatGetSelfSignedCert(CXPLAT_SELF_SIGN_CERT_USER, FALSE, NULL);
@@ -271,7 +500,10 @@ void start(){
     UdpConfig.Flags = 0;
     UdpConfig.InterfaceIndex = 0;
     UdpConfig.CallbackContext = nullptr;
-    QUIC_STATUS Status =
+    QUIC_ADDR_STR str;
+    QuicAddrToString(&sockAddr, &str);
+    printf("Local address: %s\n", str.Address);
+    Status =
         CxPlatSocketCreateUdp(
             Datapath,
             &UdpConfig,
@@ -285,9 +517,7 @@ void start(){
     CXPLAT_ROUTE Route = {0};
     CxPlatSocketGetLocalAddress(Binding, &Route.LocalAddress);
     Route.RemoteAddress = sockAddr;
-    QUIC_ADDR_STR *str = nullptr;
-    QuicAddrToString(&sockAddr, str);
-    printf("Local address: %s\n", str->Address);
+
     //
     const uint64_t PacketNumber = 0;
     uint8_t Packet[512] = {0};
@@ -299,26 +529,6 @@ void start(){
         Packet,
         &PacketLength,
         &HeaderLength);
-
-
-    // make a client
-    // make a random parameter generator using fuzzing data
-    // make a initial packet
-    // CXPLAT_SOCKET* Binding;
-    // PacketWriter* Writer;
-    // uint64_t PacketNumber = 0;
-    // uint8_t Packet[512] = {0};
-    // uint16_t PacketLength, HeaderLength;
-    // Writer->WriteClientInitialPacket(
-    //     PacketNumber,
-    //     sizeof(uint64_t),
-    //     sizeof(Packet),
-    //     Packet,
-    //     &PacketLength,
-    //     &HeaderLength);
-
-    // send the packet
-
 
 }
 
