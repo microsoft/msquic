8a99d98ef23386621276ab94e3ccbf73c959e4d1
Nick Banks <nibanks@microsoft.com>
2024-02-13 11:14:32 -0500

    Fix Incorrect Test Comparison (#4131)

--- src/test/lib/ApiTest.cpp
+++ src/test/lib/ApiTest.cpp
@@ -4836,7 +4836,10 @@ QuicTestGetPerfCounters()
             nullptr),
         QUIC_STATUS_BUFFER_TOO_SMALL);
 
-    TEST_EQUAL(BufferLength, sizeof(uint64_t) * QUIC_PERF_COUNTER_MAX);
+    if (BufferLength < sizeof(uint64_t) * QUIC_PERF_COUNTER_MAX) {
+        TEST_FAILURE("Perf counters length too small");
+        return;
+    }
 
     //
     // Test getting the full array of counters.
