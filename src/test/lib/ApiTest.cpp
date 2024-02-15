279aedd2c52f15e85f148cc6ba0f3654552327ed
Nick Banks <nibanks@microsoft.com>
2024-02-15 12:04:46 -0500

    Another Fix for Perf Counters Test (#4137)

--- src/test/lib/ApiTest.cpp
+++ src/test/lib/ApiTest.cpp
@@ -4801,6 +4801,7 @@ QuicTestGetPerfCounters()
     // Test getting the full array of counters.
     //
     uint64_t Counters[QUIC_PERF_COUNTER_MAX];
+    BufferLength = sizeof(Counters);
     TEST_QUIC_SUCCEEDED(
         MsQuic->GetParam(
             nullptr,
