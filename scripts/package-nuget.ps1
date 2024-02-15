c084859840634c404148492b87c971efca33e7ee
Nick Banks <nibanks@microsoft.com>
2024-02-15 12:26:31 -0500

    Increment Patch Version

--- scripts/package-nuget.ps1
+++ scripts/package-nuget.ps1
@@ -153,7 +153,7 @@ $DistDir = Join-Path $BaseArtifactsDir "dist"
 $CurrentCommitHash = Get-GitHash -RepoDir $RootDir
 $RepoRemote = Get-GitRemote -RepoDir $RootDir
 
-$Version = "2.3.3"
+$Version = "2.3.4"
 
 $BuildId = $env:BUILD_BUILDID
 if ($null -ne $BuildId) {
