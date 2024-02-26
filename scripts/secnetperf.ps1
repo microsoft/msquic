220a7a35d9a5980ac2d698a07b340428b0613ef9
Nick Banks <nibanks@microsoft.com>
2024-02-26 12:37:51 -0500

    Revert change

--- scripts/secnetperf.ps1
+++ scripts/secnetperf.ps1
@@ -284,6 +284,7 @@ VALUES ("$TestId-tcp-$tcp", "$MsQuicCommit", $env, $env, $item, NULL, "$io", "$t
 "@
             }
         } elseif ($Test.Metric -eq "latency") {
+            $json["$testId-$transport-lat"] = $Test.Latency[$tcp]
             # Test.Values[...] is a flattened 1D array of the form: [ first run + RPS, second run + RPS, third run + RPS..... ], ie. if each run has 8 values + RPS, then the array has 27 elements (8*3 + 3)
             for ($offset = 0; $offset -lt $Test.Values[$tcp].Length; $offset += 9) {
                 $SQL += @"
@@ -325,10 +326,10 @@ Write-Host "Tests complete!"
     } catch { }
 
     # Save the test results (sql and json).
-    Write-Host "Writing test-results-$environment-$os-$arch-$tls-$io.sql"
-    $SQL | Set-Content -Path "test-results-$environment-$os-$arch-$tls-$io.sql"
-    Write-Host "Writing json-test-results-$environment-$os-$arch-$tls-$io.json"
-    $json | ConvertTo-Json | Set-Content -Path "json-test-results-$environment-$os-$arch-$tls-$io.json"
+    Write-Host "`Writing test-results-$environment-$plat-$os-$arch-$tls-$io.sql"
+    $SQL | Set-Content -Path "test-results-$environment-$plat-$os-$arch-$tls-$io.sql"
+    Write-Host "`Writing json-test-results-$environment-$plat-$os-$arch-$tls-$io.json"
+    $json | ConvertTo-Json | Set-Content -Path "json-test-results-$environment-$plat-$os-$arch-$tls-$io.json"
 }
 
 # Clear out any exit codes from previous commands.
