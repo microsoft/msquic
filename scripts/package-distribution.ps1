1cb9a4c4fe7e09425f3214ab49e61a74767a1014
Nick Banks <nibanks@microsoft.com>
2024-08-05 08:30:10 -0400

    Increment Patch Version

--- scripts/package-distribution.ps1
+++ scripts/package-distribution.ps1
@@ -19,7 +19,7 @@ $ArtifactsBinDir = Join-Path $BaseArtifactsDir "bin"
 # All direct subfolders are OS's
 $Platforms = Get-ChildItem -Path $ArtifactsBinDir
 
-$Version = "2.4.0"
+$Version = "2.4.1"
 
 $WindowsBuilds = @()
 $AllBuilds = @()
