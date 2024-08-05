f96015560399d60cbdd8608b6fa2120560118500
Nick Banks <nibanks@microsoft.com>
2024-08-05 13:11:50 -0400

    Multiple Receive API Support (#4182)
    * WIP
    
    * Fixes
    
    * Simplify
    
    * fix
    
    * Fix
    
    * Fix more bugs
    
    * Fix clog and .net
    
    * Fixes
    
    * Improvements
    
    * Undo a merge issue
    
    * Another merge issue
    
    * Simple test
    
    * remove fprintf
    
    * fix signature to upto 255. add Large send case
    
    * 1G Multi receive
    
    * fix drain bug
    
    * kernel test
    
    * fix
    
    * fix
    
    * unused variable
    
    * compare data
    
    * tmp
    
    * test cases
    
    * 95% works
    
    * remove fprintf in core
    
    * Fix Range copy
    
    * fix type mimatch
    
    * cleanup
    
    * Fix stall issue
    
    * rollback
    
    * retry if length is 0
    
    * remove continue to reset Readpending in QuicStreamReceiveComplete
    
    * stop enablling send with pending data (race condition exists)
    
    * fix QuicRecvBufferHasUnreadData
    
    * rollback for recv_buff lock bug then return earlier not to indicate FIN bit
    
    * fix
    
    * fix
    
    * reduce test buffer size for CI speed
    
    * update document
    
    * add back buffer count caution
    
    * Update docs/api/StreamReceiveComplete.md
    
    Co-authored-by: Nick Banks <nibanks@microsoft.com>
    
    * Update docs/api/StreamReceiveComplete.md
    
    Co-authored-by: Nick Banks <nibanks@microsoft.com>
    
    * logical conflicts
    
    ---------
    
    Co-authored-by: ami-GS <1991.daiki@gmail.com>

--- src/inc/msquic.h
+++ src/inc/msquic.h
@@ -685,7 +685,8 @@ typedef struct QUIC_SETTINGS {
             uint64_t ReliableResetEnabled                   : 1;
             uint64_t OneWayDelayEnabled                     : 1;
             uint64_t NetStatsEventEnabled                   : 1;
-            uint64_t RESERVED                               : 22;
+            uint64_t StreamMultiReceiveEnabled              : 1;
+            uint64_t RESERVED                               : 21;
 #else
             uint64_t RESERVED                               : 26;
 #endif
@@ -735,7 +736,8 @@ typedef struct QUIC_SETTINGS {
             uint64_t ReliableResetEnabled      : 1;
             uint64_t OneWayDelayEnabled        : 1;
             uint64_t NetStatsEventEnabled      : 1;
-            uint64_t ReservedFlags             : 59;
+            uint64_t StreamMultiReceiveEnabled : 1;
+            uint64_t ReservedFlags             : 58;
 #else
             uint64_t ReservedFlags             : 63;
 #endif
