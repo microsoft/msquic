2f4c82722dfa2843b44dfe8ee82af1ab06bd81b3
Ahmet Ibrahim Aksoy <aaksoy@microsoft.com>
2024-08-18 16:13:49 +0200

    Change some docker images and active commented out distros

--- scripts/docker-script.sh
+++ scripts/docker-script.sh
@@ -1,40 +1,57 @@
 #! /bin/bash
-set -e
+
+if [[ $(id -u) -ne 0 ]]; 
+then
+    #Beware of how you compose the command
+    echo "This script must be run as root. Running the script with sudo..."
+    printf -v cmd_str '%q ' "$0" "$@"
+    exec sudo su -c "$cmd_str"
+    exit $?
+fi
+
 cd /main
-OS=$(echo $(awk -F= '$1=="ID" { print $2 ;}' /etc/os-release) | xargs)
-VERSION=$(echo $(awk -F= '$1=="VERSION_ID" { print $2 ;}' /etc/os-release) | xargs)
+. /etc/os-release
+OS=$(echo $ID | xargs)
+VERSION=$(echo $VERSION_ID | xargs)
 
 echo "${OS} ${VERSION} is detected."
 
 install_dependencies_apt()
 {
-    sudo apt-get update
-    sudo apt-get install --fix-broken
-    sudo apt-get install -y wget git gzip tar
-    # Remove the existing libmsquic package and dependencies for Ubuntu
-    sudo apt-get remove -y libmsquic libnuma1
-    if [[ "$OS" == "ubuntu" ]] && [[ "$VERSION" == "24.04" ]]; then
-        sudo apt-get remove -y libxdp1 libnl-route-3-200
+    apt-get update
+    if ! [ -f /usr/bin/dotnet ]; then
+        apt-get install -y wget gzip tar
     fi
-    sudo apt-get install -y ./artifacts/libmsquic_*.deb
+    apt-get install -y ./artifacts/libmsquic_*.deb
 }
 
 install_dependencies_rpm()
 {
-    sudo yum update -y
-    sudo yum install -y wget git gzip tar libicu
-    # Remove the existing libmsquic package and dependencies for Ubuntu
-    sudo yum remove -y libmsquic numactl-libs
-    sudo find -name "libmsquic*.rpm" -exec yum localinstall -y {} \;
+    yum update -y
+    if ! [ -f /usr/bin/dotnet ]; then
+        yum install -y wget gzip tar # .NET installing requirements
+        yum install -y libicu # .NET dependencies
+    fi
+    find -name "libmsquic*.rpm" -exec yum localinstall -y {} \;
 }
 
 install_dependencies_opensuse()
 {
-    sudo zypper ref
-    sudo zypper install -y wget git gzip
-    # Remove the existing libmsquic package and dependencies for Ubuntu
-    sudo zypper remove -y libmsquic libnuma1
-    sudo find -name "libmsquic*.rpm" -exec zypper install --allow-unsigned-rpm -y {} \;
+    zypper ref
+    if ! [ -f /usr/bin/dotnet ]; then
+        zypper install -y wget gzip
+    fi
+    find -name "libmsquic*.rpm" -exec zypper install --allow-unsigned-rpm -y {} \;
+}
+
+# .NET is installed already on Azure Linux and Mariner images
+install_libmsquic_azure_linux()
+{
+    if ! [ -f /usr/bin/dotnet ]; then
+        tdnf install -y wget gzip tar
+    fi
+    tdnf update
+    find -name "libmsquic*.rpm" -exec tdnf install -y {} \;
 }
 
 if [[ "$OS" == "ubuntu" ]] || [[ "$OS" == "debian" ]]; then
@@ -43,24 +60,25 @@ elif [[ "$OS" == "centos" ]] || [[ "$OS" == "almalinux" ]] || [[ "$OS" == "rhel"
     install_dependencies_rpm
 elif [[ "$OS" == 'opensuse-leap' ]]; then
     install_dependencies_opensuse
+elif [[ "$OS" == 'azurelinux' ]] || [[ "$OS" == 'mariner' ]]; then
+    install_libmsquic_azure_linux
 else
     echo "Unsupported OS: ${OS}"
     exit 1
 fi
 
-sudo chmod +x artifacts/bin/linux/${1}_${2}_${3}/msquictest
-sudo artifacts/bin/linux/${1}_${2}_${3}/msquictest --gtest_filter=ParameterValidation.ValidateApi
-if [ $? -ne 0 ]; then
-    exit $?
+set -e
+chmod +x artifacts/bin/linux/${1}_${2}_${3}/msquictest
+artifacts/bin/linux/${1}_${2}_${3}/msquictest --gtest_filter=ParameterValidation.ValidateApi
+
+# Install .NET if it is not installed
+if ! [ -f /usr/bin/dotnet ]; then
+    wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
+    chmod +x dotnet-install.sh
+    ./dotnet-install.sh --channel $4 --shared-runtime
+
+    export PATH=$PATH:$HOME/.dotnet
+    export DOTNET_ROOT=$HOME/.dotnet
 fi
 
-cd /main
-
-sudo wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
-sudo chmod +x dotnet-install.sh
-sudo ./dotnet-install.sh --channel $4
-
-export DOTNET_ROOT=/root/.dotnet
-export PATH=$PATH:/root/.dotnet
-
-sudo /root/.dotnet/dotnet src/cs/QuicSimpleTest/artifacts/net$4/QuicHello.dll
+dotnet /main/src/cs/QuicSimpleTest/artifacts/net$4/QuicHello.net$4.dll
