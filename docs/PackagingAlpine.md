56698c5ea5c69f45f7cfa1f95ab32f60b4eef62f
Ahmet Ibrahim Aksoy <aaksoy@microsoft.com>
2024-09-22 13:17:54 +0200

    Add Alpine packaging and testing github action workflow

---
+++ docs/PackagingAlpine.md
@@ -0,0 +1,12 @@
+# Packaging MsQuic for Alpine
+
+1. Create release and create tag with exact version number (e.g. v2.5.0).
+2. Run `generate-alpine-packaging-file.ps1` script to create `APKBUILD` file for the release.
+3. If you don't have account for [AlpineLinux GitLab](https://gitlab.alpinelinux.org). Create an account and [configure your SSH](https://docs.gitlab.com/ee/user/ssh.html).
+4. If you didn't fork `aports` repository yet, Fork `https://gitlab.alpinelinux.org/alpine/aports`.
+5. Clone `https://gitlab.alpinelinux.org/<your_username>/aports` repository.
+6. Navigate to `aports/testing/libmsquic` folder.
+7. Replace the `APKBUILD` file with newly created `APKBUILD` file.
+8. Create a commit using `testing/libmsquic: upgrade to <version_number>` (version_number e.g. 2.5.0 or 2.4.4).
+9. Create a merge request using `testing/libmsquic: upgrade to <version_number>` (version_number e.g. 2.5.0 or 2.4.4).
+10. Owners of the `aports` repository will respond to the PR or merge it in couple of days/hours.
---
+++ scripts/generate-alpine-packaging-file.ps1
@@ -0,0 +1,36 @@
+class Version
+{
+    [string]$Major
+    [string]$Minor
+    [string]$Patch
+}
+
+$submodules = git submodule
+
+$processedSubmodules = @("clog", "openssl3", "googletest")
+$placeholderVariables = @{
+    "clog" = "CLOG_COMMIT_HASH"
+    "openssl3" = "OPENSSL3_COMMIT_HASH"
+    "googletest" = "GOOGLETEST_COMMIT_HASH"
+}
+$versionPlaceholder = "VERSION_PLACEHOLDER"
+$alpinePackagingFile = ((Get-Content "$PSScriptRoot/templates/APKBUILD.template") -join "`n") + "`n"
+
+foreach ($submodule in $submodules)
+{
+    $submoduleInfo = $submodule.Trim().Split(" ")
+    $submoduleName = $submoduleInfo[1].Replace("submodules/", "")
+    if ($processedSubmodules -contains $submoduleName)
+    {
+        $alpinePackagingFile = $alpinePackagingFile -replace $placeholderVariables[$submoduleName], $submoduleInfo[0]
+    }
+}
+
+$version = [Version](Get-Content 'version.json' | Out-String | ConvertFrom-Json)
+$alpinePackagingFile = $alpinePackagingFile -replace $versionPlaceholder, "$($version.Major).$($version.Minor).$($version.Patch)"
+Write-Output $alpinePackagingFile | Out-File APKBUILD -NoNewline
+Write-Output "APKBUILD file for msquic v$($version.Major).$($version.Minor).$($version.Patch) has been generated successfully."
+Write-Output "Starting to add file hashes into APKBUILD file..."
+
+docker run -v .:/msquic -w /msquic alpine:latest /msquic/scripts/alpine-generate-hash.sh
+Write-Output "File hashes have been added successfully."
