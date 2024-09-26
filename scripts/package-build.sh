431bc226ebeae0a39e8893fcc1597f293a723c00
Ahmet Ibrahim Aksoy <aaksoy@microsoft.com>
2024-09-26 21:56:28 +0200

    Fix script

--- scripts/package-build.sh
+++ scripts/package-build.sh
@@ -1,12 +1,29 @@
 #! /bin/sh
 
-./alpine-packaging-prepare-script.sh
+set -e
+apk add --upgrade --no-cache sudo alpine-sdk
+
+git config --global user.name "Microsoft QUIC Team"
+git config --global user.email "quicdev@microsoft.com"
+
+# Add the packaging user to the abuild group
+adduser -D packaging -G abuild
+
+# Give the packaging user sudo access
+echo "packaging ALL=(ALL)       NOPASSWD: ALL" > /etc/sudoers.d/packaging
+
+mkdir -p /var/cache/distfiles
+chmod a+w /var/cache/distfiles
+
+mkdir -p /home/packaging/github-actions/packages/
+chown -R packaging:abuild /home/packaging/github-actions/packages/
+
 mkdir -p /home/packaging/tools
 cp APKBUILD /home/packaging/tools
 chown -R packaging:abuild /home/packaging/tools
 
 su packaging -c "abuild-keygen -n"
-find /home/packaging/.abuild -name '*.rsa' -exec ./alpine-configure-packaging-key.sh {} \;
+find /home/packaging/.abuild -name '*.rsa' -exec /msquic/scripts/alpine-configure-packaging-key.sh {} \;
 
 # msquic is using submodules and we need to get them inside
 cd /home/packaging/tools
