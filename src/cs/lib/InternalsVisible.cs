91abe699dffcef803b9e491fe96563f0a5da7691
ami-GS <1991.daiki@gmail.com>
2024-11-18 15:06:49 -0800

    Revert "delete MsQuicTool"
    This reverts commit dfa4d7288c6829652d629bf65fc5aea1c4e0368d.

---
+++ src/cs/lib/InternalsVisible.cs
@@ -0,0 +1,10 @@
+#pragma warning disable IDE0073
+//
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+//
+#pragma warning restore IDE0073
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("MsQuicTool")]
