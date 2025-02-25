f10e72ef2191b6fc8c1635e8004d50e51f077540
Gaurav Singh <gaurasingh@microsoft.com>
2025-02-25 18:20:44 +0530

    Win32 apicall (#4851)
    * Win32 calls will be stdcall
    
    * added quic_api to test and tools
    
    * added QUIC_API to multiple callbacks
    
    * minor change
    
    * added more quic_api

--- src/test/lib/HandshakeTest.cpp
+++ src/test/lib/HandshakeTest.cpp
@@ -105,6 +105,7 @@ QuicTestPrimeResumption(
 _Function_class_(NEW_CONNECTION_CALLBACK)
 static
 bool
+QUIC_API
 ListenerAcceptConnection(
     _In_ TestListener* Listener,
     _In_ HQUIC ConnectionHandle
@@ -2256,6 +2257,7 @@ QuicTestConnectBadSni(
 _Function_class_(NEW_CONNECTION_CALLBACK)
 static
 bool
+QUIC_API
 ListenerRejectConnection(
     _In_ TestListener* /*  Listener */,
     _In_ HQUIC ConnectionHandle
@@ -3703,6 +3705,7 @@ QuicTestChangeAlpn(
 _Function_class_(NEW_CONNECTION_CALLBACK)
 static
 bool
+QUIC_API
 ListenerAcceptConnectionTestTP(
     _In_ TestListener* Listener,
     _In_ HQUIC ConnectionHandle
