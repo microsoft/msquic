7715556cc87158064f6103da9735453959121d1f
Jack He <jackhe@microsoft.com>
2025-03-05 14:57:08 -0800

    qtip tests should pass this one bro

--- src/test/lib/DataTest.cpp
+++ src/test/lib/DataTest.cpp
@@ -317,7 +317,7 @@ NewPingConnection(
 
     if (SendUdpOverQtip) {
         // UseQTIP is implicitly true here. We are alternating between QTIP and QUIC to ping the same listener.
-        Connection->SetQtipPreferences(UseQTIP ? 1 : 0);
+        Connection->SetQtipPreferences(0);
     } else if (UseQTIP) {
         // We hit this case either when we are alternating between QTIP and QUIC to ping the same listener, or SendUdpOverQtip is always false.
         Connection->SetQtipPreferences(1);
