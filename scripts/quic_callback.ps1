53a2d80704177f7207e9982bb0f5c5f944965051
Jack He <jackhe@microsoft.com>
2025-03-12 12:02:45 -0700

    even more observability

--- scripts/quic_callback.ps1
+++ scripts/quic_callback.ps1
@@ -117,6 +117,8 @@ if ($Command.Contains("/home/secnetperf/_work/quic/artifacts/bin/linux/x64_Relea
     # if artifacts don't exist, make it
     New-Item -ItemType Directory "artifacts/logs/$artifactName/server" -ErrorAction Ignore | Out-Null
     ls
+    Write-Host "inspecting artifacts/logs"
+    ls artifacts/logs
     .\scripts\log.ps1 -Stop -OutputPath "artifacts/logs/$artifactName/server" -RawLogOnly
 } else {
     throw "Invalid command: $Command"
