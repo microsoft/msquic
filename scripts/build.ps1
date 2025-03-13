fa3ff847c7c9def10706aeec317a8f050bc708b0
vgmahajanshetty <vgmahajanshetty@gmail.com>
2025-03-13 20:07:12 +0530

    Changes for issue 4856

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -507,16 +507,22 @@ function CMake-Generate {
         $Arguments += " -DQUIC_HIGH_RES_TIMERS=on"
     }
     if ($Platform -eq "android") {
-        $NDK = $env:ANDROID_NDK_LATEST_HOME -replace '26\.\d+\.\d+', '25.2.9519653' # Temporary work around. Use RegEx to replace newer version.
+        $NDK = $env:ANDROID_NDK_LATEST_HOME -replace '26\.\d+\.\d+', '28.0.13004108' # Temporary work around. Use RegEx to replace newer version.
         $env:PATH = "$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin:$env:PATH"
+        $env:ANDROID_NDK_HOME = $NDK
+        $env:ANDROID_NDK_ROOT = $NDK  # Set ANDROID_NDK_ROOT to ensure consistency
+        Write-Host "Set ANDROID_NDK_HOME: $env:ANDROID_NDK_HOME"
+        Write-Host "Set ANDROID_NDK_ROOT: $env:ANDROID_NDK_ROOT"
+
         switch ($Arch) {
             "x86"   { $Arguments += " -DANDROID_ABI=x86"}
             "x64"   { $Arguments += " -DANDROID_ABI=x86_64" }
             "arm"   { $Arguments += " -DANDROID_ABI=armeabi-v7a" }
             "arm64" { $Arguments += " -DANDROID_ABI=arm64-v8a" }
         }
+        $env:CC = "x86_64-linux-android29-clang"
+        $env:CXX = "x86_64-linux-android29-clang++"
         $Arguments += " -DANDROID_PLATFORM=android-29"
-        $env:ANDROID_NDK_HOME = $NDK
         $NdkToolchainFile = "$NDK/build/cmake/android.toolchain.cmake"
         $Arguments += " -DANDROID_NDK=""$NDK"""
         $Arguments += " -DCMAKE_TOOLCHAIN_FILE=""$NdkToolchainFile"""
