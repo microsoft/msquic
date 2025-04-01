f914dc63fdb02aa84720b2ef7fa4b52dc37adfc2
Neil Horman <nhorman@openssl.org>
2025-03-31 22:21:17 -0400

    updating alpine packging script to include openssl
    Also fix the quictls url, as its pointing to the wrong repo at the
    moment

--- scripts/generate-alpine-packaging-file.ps1
+++ scripts/generate-alpine-packaging-file.ps1
@@ -15,10 +15,11 @@ class Version
 
 $submodules = git submodule
 
-$processedSubmodules = @("clog", "quictls3", "googletest")
+$processedSubmodules = @("clog", "quictls3", "openssl", "googletest")
 $placeholderVariables = @{
     "clog" = "CLOG_COMMIT_HASH"
-    "quictls3" = "OPENSSL3_COMMIT_HASH"
+    "quictls3" = "QUICTLS3_COMMIT_HASH"
+    "openssl" = "OPENSSL_COMMIT_HASH"
     "googletest" = "GOOGLETEST_COMMIT_HASH"
 }
 $versionPlaceholder = "VERSION_PLACEHOLDER"
