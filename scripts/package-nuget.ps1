87c7ebebe8a5a4ccd1ecf412b24d384b4af4b71b
Nick Banks <nibanks@microsoft.com>
2025-04-22 10:06:42 -0400

    Fix the QUIC_HANDSHAKE_INFO Test to Allow for Future Versions to Grow… (#5035)

--- scripts/package-nuget.ps1
+++ scripts/package-nuget.ps1
@@ -153,7 +153,7 @@ $DistDir = Join-Path $BaseArtifactsDir "dist"
 $CurrentCommitHash = Get-GitHash -RepoDir $RootDir
 $RepoRemote = Get-GitRemote -RepoDir $RootDir
 
-$Version = "2.4.8"
+$Version = "2.4.9"
 
 $BuildId = $env:BUILD_BUILDID
 if ($null -ne $BuildId) {
