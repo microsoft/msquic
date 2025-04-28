5eade7b906d33ed47dd307aa8dbb0de395d1c687
Yi Huang <huanyi@microsoft.com>
2025-04-28 14:50:15 -0700

    Backport ETW silo fix

--- scripts/package-distribution.ps1
+++ scripts/package-distribution.ps1
@@ -19,7 +19,7 @@ $ArtifactsBinDir = Join-Path $BaseArtifactsDir "bin"
 # All direct subfolders are OS's
 $Platforms = Get-ChildItem -Path $ArtifactsBinDir
 
-$Version = "2.3.10"
+$Version = "2.3.11"
 
 $WindowsBuilds = @()
 $AllBuilds = @()
