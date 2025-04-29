e9d854e7a977305bc6ea461ab72feaab96a19dfc
Yi Huang <huanyi@microsoft.com>
2025-04-29 03:52:30 -0700

    Backport ETW silo fix to v2.3 (#5057)

--- scripts/package-nuget.ps1
+++ scripts/package-nuget.ps1
@@ -153,7 +153,7 @@ $DistDir = Join-Path $BaseArtifactsDir "dist"
 $CurrentCommitHash = Get-GitHash -RepoDir $RootDir
 $RepoRemote = Get-GitRemote -RepoDir $RootDir
 
-$Version = "2.3.10"
+$Version = "2.3.11"
 
 $BuildId = $env:BUILD_BUILDID
 if ($null -ne $BuildId) {
