eaa1fcb630a3b53dfce5584fe369ea15de1a6de4
Neil Horman <nhorman@openssl.org>
2025-05-01 14:25:56 -0400

    Remove incorrect assertion on determining Key for Control frames
    We're seeing periodic failures here:
    https://github.com/microsoft/msquic/actions/runs/14776565585/job/41488589785?pr=4959
    
    Due to an attempt to ACK packets for which we have a read key, but not a
    write key.
    
    However, based on discussion here:
    https://github.com/microsoft/msquic/pull/4959#issuecomment-2845296376
    
    I believe its a possibly expected scenario (based on the TLS backend)
    for this to be ok.  Given that, and the fact that callers of
    QuicPacketBuilderGetPacketTypeAndKeyForControlFrames are written to
    expect a failure case gracefully, that its ok to remove this assertion

--- src/core/packet_builder.c
+++ src/core/packet_builder.c
@@ -585,7 +585,6 @@ QuicPacketBuilderGetPacketTypeAndKeyForControlFrames(
         Builder->Connection,
         "Failed to get packet type for control frames, 0x%x",
         SendFlags);
-    CXPLAT_DBG_ASSERT(CxPlatIsRandomMemoryFailureEnabled()); // This shouldn't have been called then!
 
     return FALSE;
 }
