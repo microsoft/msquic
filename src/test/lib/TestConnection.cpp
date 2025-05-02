f1d0b12e8b333008227ad42f879c6422fd20cee4
Nick Banks <nibanks@microsoft.com>
2025-05-02 08:25:46 -0400

    Increase Retry Limit for Key/CID Update Tests

--- src/test/lib/TestConnection.cpp
+++ src/test/lib/TestConnection.cpp
@@ -263,7 +263,7 @@ TestConnection::ForceKeyUpdate()
                 0,
                 nullptr);
 
-    } while (Status == QUIC_STATUS_INVALID_STATE && ++Try <= 20);
+    } while (Status == QUIC_STATUS_INVALID_STATE && ++Try <= 50);
 
     return Status;
 }
@@ -291,7 +291,7 @@ TestConnection::ForceCidUpdate()
                 0,
                 nullptr);
 
-    } while (Status == QUIC_STATUS_INVALID_STATE && ++Try <= 20);
+    } while (Status == QUIC_STATUS_INVALID_STATE && ++Try <= 50);
 
     return Status;
 }
