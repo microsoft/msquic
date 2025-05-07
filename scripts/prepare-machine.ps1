8206597049326ae98ddd43dd8c66653e8beae1bc
Michael Friesen <3517159+mtfriesen@users.noreply.github.com>
2025-05-07 16:48:39 -0400

    install liburing on test machines

--- scripts/prepare-machine.ps1
+++ scripts/prepare-machine.ps1
@@ -571,6 +571,7 @@ if ($IsLinux) {
         sudo apt-get install -y lttng-tools
         sudo apt-get install -y liblttng-ust-dev
         sudo apt-get install -y gdb
+        sudo apt-get install -y liburing2
         if ($UseXdp) {
             if (!$IsUbuntu2404) {
                 sudo apt-add-repository "deb http://mirrors.kernel.org/ubuntu noble main" -y
