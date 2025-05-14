7a12b7ea41ceee000173ce62a9841364302c6a08
HHN <harihara.sn@gmail.com>
2025-05-14 15:29:19 +0530

    COPIED_TO_FRAME event is now optional and depends on StreamSend flag
    Signed-off-by: HHN <harihara.sn@gmail.com>

--- src/inc/msquic.h
+++ src/inc/msquic.h
@@ -240,15 +240,16 @@ typedef enum QUIC_RECEIVE_FLAGS {
 DEFINE_ENUM_FLAG_OPERATORS(QUIC_RECEIVE_FLAGS)
 
 typedef enum QUIC_SEND_FLAGS {
-    QUIC_SEND_FLAG_NONE                     = 0x0000,
-    QUIC_SEND_FLAG_ALLOW_0_RTT              = 0x0001,   // Allows the use of encrypting with 0-RTT key.
-    QUIC_SEND_FLAG_START                    = 0x0002,   // Asynchronously starts the stream with the sent data.
-    QUIC_SEND_FLAG_FIN                      = 0x0004,   // Indicates the request is the one last sent on the stream.
-    QUIC_SEND_FLAG_DGRAM_PRIORITY           = 0x0008,   // Indicates the datagram is higher priority than others.
-    QUIC_SEND_FLAG_DELAY_SEND               = 0x0010,   // Indicates the send should be delayed because more will be queued soon.
-    QUIC_SEND_FLAG_CANCEL_ON_LOSS           = 0x0020,   // Indicates that a stream is to be cancelled when packet loss is detected.
-    QUIC_SEND_FLAG_PRIORITY_WORK            = 0x0040,   // Higher priority than other connection work.
-    QUIC_SEND_FLAG_CANCEL_ON_BLOCKED        = 0x0080,   // Indicates that a frame should be dropped when it can't be sent immediately.
+    QUIC_SEND_FLAG_NONE                                = 0x0000,
+    QUIC_SEND_FLAG_ALLOW_0_RTT                         = 0x0001,   // Allows the use of encrypting with 0-RTT key.
+    QUIC_SEND_FLAG_START                               = 0x0002,   // Asynchronously starts the stream with the sent data.
+    QUIC_SEND_FLAG_FIN                                 = 0x0004,   // Indicates the request is the one last sent on the stream.
+    QUIC_SEND_FLAG_DGRAM_PRIORITY                      = 0x0008,   // Indicates the datagram is higher priority than others.
+    QUIC_SEND_FLAG_DELAY_SEND                          = 0x0010,   // Indicates the send should be delayed because more will be queued soon.
+    QUIC_SEND_FLAG_CANCEL_ON_LOSS                      = 0x0020,   // Indicates that a stream is to be cancelled when packet loss is detected.
+    QUIC_SEND_FLAG_PRIORITY_WORK                       = 0x0040,   // Higher priority than other connection work.
+    QUIC_SEND_FLAG_CANCEL_ON_BLOCKED                   = 0x0080,   // Indicates that a frame should be dropped when it can't be sent immediately.
+    QUIC_SEND_FLAG_EVENT_ON_FIRST_COPY_TO_FRAME        = 0x0100,   // Event is signaled when data from the send request is copied to a frame for the first time
 } QUIC_SEND_FLAGS;
 
 DEFINE_ENUM_FLAG_OPERATORS(QUIC_SEND_FLAGS)
