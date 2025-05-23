dcaa7232d58f71815aa100bde64020dbb70c53a3
Nick Banks <nibanks@microsoft.com>
2025-05-23 11:51:19 -0400

    Update Version to 2.5.1

--- scripts/write-versions.ps1
+++ scripts/write-versions.ps1
@@ -26,7 +26,7 @@ $ArtifactsDir = $BuildConfig.ArtifactsDir
 $SourceVersion = $env:BUILD_SOURCEVERSION;
 $SourceBranch = $env:BUILD_SOURCEBRANCH;
 $BuildId = $env:BUILD_BUILDID;
-$VersionNumber = "2.5.0";
+$VersionNumber = "2.5.1";
 
 class BuildData {
     [string]$SourceVersion;
