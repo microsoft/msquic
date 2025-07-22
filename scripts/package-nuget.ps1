d49bf5bca627d5695653f6c4db6e6267f1e8e660
Yi Huang <huanyi@microsoft.com>
2025-07-22 19:23:44 +0000

    Bump version to 2.5.2

--- scripts/package-nuget.ps1
+++ scripts/package-nuget.ps1
@@ -153,7 +153,7 @@ $DistDir = Join-Path $BaseArtifactsDir "dist"
 $CurrentCommitHash = Get-GitHash -RepoDir $RootDir
 $RepoRemote = Get-GitRemote -RepoDir $RootDir
 
-$Version = "2.5.1"
+$Version = "2.5.2"
 
 $BuildId = $env:BUILD_BUILDID
 if ($null -ne $BuildId) {
