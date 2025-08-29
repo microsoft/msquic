624dd5c7601759439aa08c852fe2bfcb6720dd32
Michael Friesen <3517159+mtfriesen@users.noreply.github.com>
2025-08-29 19:41:22 +0000

    fix winkernel yet again

--- src/test/bin/winkernel/control.cpp
+++ src/test/bin/winkernel/control.cpp
@@ -1543,9 +1543,6 @@ QuicTestCtlEvtIoDeviceControl(
         QuicTestCtlRun(QuicTestValidatePartition());
         break;
 
-    case IOCTL_QUIC_RUN_VALIDATE_EXECUTION_CONTEXT:
-        QuicTestCtlRun(QuicTestValidateExecutionContext());
-        break;
 #endif
 
     case IOCTL_QUIC_RUN_TEST_KEY_UPDATE_DURING_HANDSHAKE:
