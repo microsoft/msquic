7449ba3942bf0a147435f4f7f07ec1b43bd7caee
Lei Peng <plhitsz@outlook.com>
2025-08-30 00:53:29 +0800

    Fix incorrect cmake option name in build.ps1 and cmakepreset (#5368)

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -440,9 +440,6 @@ function CMake-Generate {
     $Arguments += " -DQUIC_TLS_LIB=" + $Tls
     $Arguments += " -DQUIC_OUTPUT_DIR=""$ArtifactsDir"""
 
-    if ($IsLinux) {
-        $Arguments += " -DQUIC_LINUX_LOG_ENCODER=lttng"
-    }
     if (!$DisableLogs) {
         $Arguments += " -DQUIC_ENABLE_LOGGING=on"
     }
