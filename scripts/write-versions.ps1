b3945bb0c9e44463c93dac13e40975a7c3a526ca
Guillaume Hetier <guhetier@microsoft.com>
2025-08-29 09:52:42 -0700

    Bump version to 2.5.5 (#5396)

--- scripts/write-versions.ps1
+++ scripts/write-versions.ps1
@@ -26,7 +26,7 @@ $ArtifactsDir = $BuildConfig.ArtifactsDir
 $SourceVersion = $env:BUILD_SOURCEVERSION;
 $SourceBranch = $env:BUILD_SOURCEBRANCH;
 $BuildId = $env:BUILD_BUILDID;
-$VersionNumber = "2.5.4";
+$VersionNumber = "2.5.5";
 
 class BuildData {
     [string]$SourceVersion;
