449fd815e6be9fc83ca990d2fc7dfb2227b7d654
Jack He <jackhe@microsoft.com>
2025-09-30 14:55:07 -0700

    Do not always enable DSCP on Recv (#5467)
    * work-around to add recv dscp
    
    * posix
    
    * wip
    
    * linux packages?c
    
    * posix
    
    * proper init
    
    * respond to feedback
    
    * more nit feedback, remove hard-coded TRUE
    
    * comments
    
    * fix build
    
    * fix winkernel build
    
    * wip
    
    * wip
    
    * address comments

--- src/core/library.h
+++ src/core/library.h
@@ -65,6 +65,13 @@ typedef struct QUIC_LIBRARY {
     //
     BOOLEAN CustomPartitions : 1;
 
+    //
+    // Whether the datapath will be initialized with support for DSCP on receive.
+    // As of Windows 26100, requesting DSCP on the receive path causes packets to fall out of
+    // the Windows fast path causing a large performance regression.
+    //
+    BOOLEAN EnableDscpOnRecv : 1;
+
 #ifdef CxPlatVerifierEnabled
     //
     // The app or driver verifier is globally enabled.
