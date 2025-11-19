689ea41038f5ad45953f0c7222c5fcdc253cc8f9
Guillaume Hetier <guhetier@microsoft.com>
2025-11-18 16:28:42 -0800

    Refactor async registration closing test

--- src/test/bin/winkernel/control.cpp
+++ src/test/bin/winkernel/control.cpp
@@ -1557,6 +1557,9 @@ QuicTestCtlEvtIoDeviceControl(
         QuicTestCtlRun(QuicTestValidatePartition());
         break;
 
+    case IOCTL_QUIC_RUN_REGISTRATION_OPEN_CLOSE:
+        QuicTestCtlRun(QuicTestRegistrationOpenClose());
+        break;
 #endif
 
     case IOCTL_QUIC_RUN_TEST_KEY_UPDATE_DURING_HANDSHAKE:
