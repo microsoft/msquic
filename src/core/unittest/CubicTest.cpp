c41da3ff57687dee99bf60105e9cccd0f77f0bec
Agency <saikatc@microsoft.com>
2025-12-09 11:48:43 -0800

    Initial skeleton for cubic test

---
+++ src/core/unittest/CubicTest.cpp
@@ -0,0 +1,16 @@
+/*++
+
+    Copyright (c) Microsoft Corporation.
+    Licensed under the MIT License.
+
+Abstract:
+
+    Unit test for CUBIC congestion control initialization logic.
+
+--*/
+
+#include "main.h"
+#ifdef QUIC_CLOG
+#include "CubicTest.cpp.clog.h"
+#endif
+
