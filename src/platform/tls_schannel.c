51b89f6c2de13940fd43d306dd8e7f84e9d19cb0
Guillaume Hetier <guhetier@microsoft.com>
2026-01-26 09:36:45 -0800

    Add comment and normalize error

--- src/platform/tls_schannel.c
+++ src/platform/tls_schannel.c
@@ -2311,10 +2311,14 @@ CxPlatTlsWriteDataToSchannel(
             if ((SecStatus == SEC_E_NO_CREDENTIALS || SecStatus == SEC_E_INTERNAL_ERROR) &&
                 (TlsContext->SecConfig->Flags & QUIC_CREDENTIAL_FLAG_DEFER_CERTIFICATE_VALIDATION)) {
                 //
-                // Ignore this case.
+                // Certificate validation is being deferred to the application:
+                // indicate no certificate is present but let the application decide if this is a
+                // failure.
+                // SEC_E_INTERNAL_ERROR can be returned on SECPKG_ATTR_REMOTE_CERT_CONTEXT if no
+                // certificate is found, normalize to SEC_E_NO_CREDENTIALS.
                 //
                 PeerCertBlob.Type = QUIC_CERT_BLOB_NONE;
-                CertValidationResult.hrVerifyChainStatus = SecStatus;
+                CertValidationResult.hrVerifyChainStatus = SEC_E_NO_CREDENTIALS;
             } else if (SecStatus == SEC_E_OK &&
                 !(TlsContext->SecConfig->Flags & QUIC_CREDENTIAL_FLAG_NO_CERTIFICATE_VALIDATION) &&
                 (TlsContext->SecConfig->Flags & QUIC_CREDENTIAL_FLAG_REQUIRE_CLIENT_AUTHENTICATION ||
