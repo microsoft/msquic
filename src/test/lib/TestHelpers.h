78e34b1747b23f06c88775cfae50bfbbe8a6fa62
Guillaume Hetier <guhetier@microsoft.com>
2026-02-04 09:47:10 -0800

    Comment style

--- src/test/lib/TestHelpers.h
+++ src/test/lib/TestHelpers.h
@@ -1068,8 +1068,10 @@ _IRQL_requires_max_(PASSIVE_LEVEL)
 QUIC_INLINE
 void
 DrainConnectionWorkQueue(HQUIC Connection) {
+    //
     // Call the GetParam API: it schedules a work item on the connection and waits for its completion,
     // ensuring all prior work items have completed.
+    //
     QUIC_STATISTICS_V2 Stats{};
     uint32_t StatsSize = sizeof(Stats);
     (void)MsQuic->GetParam(Connection, QUIC_PARAM_CONN_STATISTICS_V2, &StatsSize, &Stats);
