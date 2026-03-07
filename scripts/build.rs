807eadc601fa05827973735839dc8f5f80383dfd
Masahiro Kozuka <masa.koz@kozuka.jp>
2026-03-07 19:34:54 +0900

    Improve support for custom OpenSSL selection

--- scripts/build.rs
+++ scripts/build.rs
@@ -29,9 +29,7 @@ fn cmake_build() {
     let target = env::var("TARGET").unwrap().replace("\\", "/");
     let out_dir = env::var("OUT_DIR").unwrap().replace("\\", "/");
     // The output directory for the native MsQuic library.
-    let libdir = "/lib";
-    let full_out_dir = [out_dir, libdir.to_string()].join("");
-    let quic_output_dir = Path::new(&full_out_dir);
+    let quic_output_dir = Path::new(&out_dir).join("artifacts");
 
     // Builds the native MsQuic and installs it into $OUT_DIR.
     let mut config = Config::new(".");
@@ -50,6 +48,20 @@ fn cmake_build() {
         config.define("QUIC_TLS_LIB", "quictls");
     } else if cfg!(feature = "openssl") {
         config.define("QUIC_TLS_LIB", "openssl");
+    } else if cfg!(feature = "openssl-external") {
+        config
+            .define("QUIC_TLS_LIB", "openssl")
+            .define("QUIC_USE_EXTERNAL_OPENSSL", "on");
+        if let Ok(openssl_dir) = std::env::var("OPENSSL_DIR") {
+            config.define("QUIC_OPENSSL_DIR", openssl_dir);
+        } else {
+            if let Ok(openssl_include_dir) = std::env::var("OPENSSL_INCLUDE_DIR") {
+                config.define("QUIC_OPENSSL_INCLUDE_DIR", openssl_include_dir);
+            }
+            if let Ok(openssl_lib_dir) = std::env::var("OPENSSL_LIB_DIR") {
+                config.define("QUIC_OPENSSL_LIB_DIR", openssl_lib_dir);
+            }
+        }
     } else if cfg!(windows) {
         config.define("QUIC_TLS_LIB", "schannel");
     } else {
