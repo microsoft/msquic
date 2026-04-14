684d2aa4802c098cc541c624642fb46107504dd4
Guillaume Hetier <guhetier@microsoft.com>
2026-04-14 16:49:58 -0700

    Bump the version number to 2.4.19 (#5947)
    ## Description
    
    Bump the version number to 2.4.19
    
    ## Testing
    
    CI
    
    ## Documentation
    
    N/A

--- scripts/write-versions.ps1
+++ scripts/write-versions.ps1
@@ -26,7 +26,7 @@ $ArtifactsDir = $BuildConfig.ArtifactsDir
 $SourceVersion = $env:BUILD_SOURCEVERSION;
 $SourceBranch = $env:BUILD_SOURCEBRANCH;
 $BuildId = $env:BUILD_BUILDID;
-$VersionNumber = "2.4.18";
+$VersionNumber = "2.4.19";
 
 class BuildData {
     [string]$SourceVersion;
