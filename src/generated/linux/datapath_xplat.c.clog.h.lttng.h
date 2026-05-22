c9e7572b5db5de892565ef48e50690a9d97e4018
Jack He <jackhe@microsoft.com>
2026-05-22 15:39:51 -0700

    CLOG

--- src/generated/linux/datapath_xplat.c.clog.h.lttng.h
+++ src/generated/linux/datapath_xplat.c.clog.h.lttng.h
@@ -52,25 +52,6 @@ TRACEPOINT_EVENT(CLOG_DATAPATH_XPLAT_C, DatapathRawInitFailMapMode,
 
 
 /*----------------------------------------------------------
-// Decoder Ring for DatapathRawMapInsertFail
-// [  dp] XDP map mode: failed to insert XSK sockets into map, status:%d
-// QuicTraceLogVerbose(
-                DatapathRawMapInsertFail,
-                "[  dp] XDP map mode: failed to insert XSK sockets into map, status:%d",
-                Status);
-// arg2 = arg2 = Status = arg2
-----------------------------------------------------------*/
-TRACEPOINT_EVENT(CLOG_DATAPATH_XPLAT_C, DatapathRawMapInsertFail,
-    TP_ARGS(
-        int, arg2), 
-    TP_FIELDS(
-        ctf_integer(int, arg2, arg2)
-    )
-)
-
-
-
-/*----------------------------------------------------------
 // Decoder Ring for DatapathInitFail
 // [  dp] Failed to initialize datapath, status:%d
 // QuicTraceLogVerbose(
@@ -110,8 +91,8 @@ TRACEPOINT_EVENT(CLOG_DATAPATH_XPLAT_C, SockCreateFail,
 // Decoder Ring for RawSockCreateFail
 // [sock] Failed to create raw socket, status:%d
 // QuicTraceLogVerbose(
-                RawSockCreateFail,
-                "[sock] Failed to create raw socket, status:%d", Status);
+                    RawSockCreateFail,
+                    "[sock] Failed to create raw socket, status:%d", Status);
 // arg2 = arg2 = Status = arg2
 ----------------------------------------------------------*/
 TRACEPOINT_EVENT(CLOG_DATAPATH_XPLAT_C, RawSockCreateFail,
