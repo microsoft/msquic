b5dc214761868c212a3e116f1b5b444fea9270a7
Gaurav Singh <gaurasingh@microsoft.com>
2026-08-13 01:07:14 +0530

    removed MaxAckDelay stop loss

--- src/tools/spin/spinquic.cpp
+++ src/tools/spin/spinquic.cpp
@@ -723,7 +723,7 @@ void SpinQuicRandomizeSettings(QUIC_SETTINGS& Settings, uint16_t ThreadID)
         Settings.IsSet.InitialRttMs = TRUE;
         break;
     case 14:
-        Settings.MaxAckDelayMs = GetRandom(UINT32_MAX, ThreadID) | 1;
+        Settings.MaxAckDelayMs = GetRandom(UINT32_MAX, ThreadID);
         Settings.IsSet.MaxAckDelayMs = TRUE;
         break;
     case 15:
