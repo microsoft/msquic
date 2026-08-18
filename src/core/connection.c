3f42675a2758edd4c52961386567ab8a5dd380d7
Gaurav Singh <gaurasingh@microsoft.com>
2026-08-18 18:33:50 +0530

    Deepcopy client alpn list (#6171)
    ## Description
    
    This PR deepCopies the Client Alpn List instead of keeping a pointer.
    Fixes:: https://microsoft.visualstudio.com/OS/_workitems/edit/62840948/
    
    ## Testing
    
    _Do any existing tests cover this change? Are new tests needed?_
    
    ## Documentation
    
    _Is there any documentation impact for this change?_

--- src/core/connection.c
+++ src/core/connection.c
@@ -2585,8 +2585,6 @@ QuicConnSetConfiguration(
         if (QUIC_FAILED(Status)) {
             goto Cleanup;
         }
-        Connection->Crypto.TlsState.ClientAlpnList = NULL;
-        Connection->Crypto.TlsState.ClientAlpnListLength = 0;
     }
 
     Status = QuicConnGenerateLocalTransportParameters(Connection, &LocalTP);
